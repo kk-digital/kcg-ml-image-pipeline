e9de96351cefea357e70d80f78113a637bba553a
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-09-26 15:41:07 +0100

    update

--- worker/image_generation/scripts/inpaint_A1111.py
+++ worker/image_generation/scripts/inpaint_A1111.py
@@ -203,6 +203,7 @@ class StableDiffusionProcessing:
     def prompt_embedding_vectors(self, prompt_array):
         embedded_prompts = []
         for prompt in prompt_array:
+            prompt = prompt.to(device=self.device)
             prompt_embedding = self.clip_text_embedder.forward(prompt)
             embedded_prompts.append(prompt_embedding)
 
