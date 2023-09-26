8cee42fcac459699456079324edb9d74d7c3f222
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-09-26 09:22:31 +0100

    change folder structure

--- worker/worker.py
+++ worker/worker.py
@@ -169,7 +169,7 @@ def main():
 
         else:
             # If there was no job, go to sleep for a while
-            sleep_time_in_seconds = 1
+            sleep_time_in_seconds = 10
             print("Did not find job, going to sleep for ", sleep_time_in_seconds, " seconds")
             time.sleep(sleep_time_in_seconds)
 
