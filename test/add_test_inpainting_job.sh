a340c09ff1e5dd3cdc4ed4d4348cc4cb5cfbc7e2
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-09-27 09:34:48 +0100

    update

--- test/add_test_inpainting_job.sh
+++ test/add_test_inpainting_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -X 'POST' http://192.168.3.1:8111/add-job -H "Content-Type: application/json" -d '{
-        'task_type': 'inpainting_generation_task'
+curl -X 'POST' http://192.168.3.1:8111/add-job -H "Content-Type: application/json" -d "{
+        "task_type': 'inpainting_generation_task'
     }"
