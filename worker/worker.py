b356e5bb62a0a9d47ca1a971f32756bdb90cee8a
kenje4090 <kenje4090@gmail.com>
2023-10-04 16:19:24 +0800

    Add automatically adding clip calculation task after uploading image data

--- worker/worker.py
+++ worker/worker.py
@@ -245,6 +245,16 @@ def upload_image_data_and_update_job_status(worker_state, job, generation_task, 
     # update status
     request.http_update_job_completed(job)
 
+    # add clip calculation task
+    clip_calculation_job = {"task_type": "clip_calculation_task",
+                            "task_input_dict": {
+                                "input_file_path": output_file_path,
+                                "input_file_hash": output_file_hash
+                                },
+                            }
+
+    request.http_add_job(clip_calculation_job)
+
 
 def process_jobs(worker_state):
     thread_state = ThreadState(1, "Job Processor")
