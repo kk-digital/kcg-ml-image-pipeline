0bc8132eba681136052299b9e4752600f7a91b82
unknown <mahlledz@gmail.com>
2023-10-04 09:22:38 +0100

    update

--- orchestration/api/data.py
+++ orchestration/api/data.py
@@ -106,7 +106,7 @@ def get_images_metadata(request: Request, dataset: str = None, limit: int = 20, 
     print('begin')
     jobs = request.app.completed_jobs_collection.find().skip(offset).limit(limit)
     print(jobs)
-    print(jobs['task_input_dict']['dataset'])
+    #print(jobs['task_input_dict']['dataset'])
     for job in jobs:
         job.pop('_id', None)
 
