2e731be42720c15c3583f9424dc291da3e9547e0
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-10-09 09:16:58 +0100

    minor change

--- job_coordinator/job_coordinator.py
+++ job_coordinator/job_coordinator.py
@@ -20,24 +20,24 @@ def main():
     args = parse_args()
 
     csv_dataset_path = 'input/civitai_phrases_database_v6.csv'
-    prompt_count = 5,
-    base_prompts_csv_path = 'input/base-prompts/icon/base_prompts_icon_1.csv',
-    dataset_name = 'icons',
-    csv_phrase_limit = 0,
-    positive_prefix = "",
-    init_img_path = "./test/test_inpainting/white_512x512.jpg",
+    prompt_count = 5
+    base_prompts_csv_path = 'input/base-prompts/icon/base_prompts_icon_1.csv'
+    dataset_name = 'icons'
+    csv_phrase_limit = 0
+    positive_prefix = ""
+    init_img_path = "./test/test_inpainting/white_512x512.jpg"
     mask_path = "./test/test_inpainting/icon_mask.png"
 
 
     generate_inpainting_generation_jobs_using_generated_prompts_and_base_prompts(
-        csv_dataset_path,
-        prompt_count,
-        base_prompts_csv_path,
-        dataset_name,
-        csv_phrase_limit,
-        positive_prefix,
-        init_img_path,
-        mask_path
+        csv_dataset_path=csv_dataset_path,
+        prompt_count=prompt_count,
+        base_prompts_csv_path=base_prompts_csv_path,
+        dataset_name=dataset_name,
+        csv_phrase_limit=csv_phrase_limit,
+        positive_prefix=positive_prefix,
+        init_img_path=init_img_path,
+        mask_path=mask_path
     )
 
 
