c0e67eb94f3311ef70d5ef175735763089438713
kenje4090 <kenje4090@gmail.com>
2023-10-10 17:05:32 +0800

    Add relevancy add selection datapoint"

--- orchestration/api/api_ranking.py
+++ orchestration/api/api_ranking.py
@@ -30,3 +30,24 @@ def add_selection_datapoint(request: Request, dataset: str, selection: Selection
     cmd.upload_data(request.app.minio_client, "datasets", full_path, data)
 
     return True
+
+
+@router.post("/relevancy/add-selection-datapoint/{dataset}")
+def add_relevancy_selection_datapoint(request: Request, dataset: str, selection: Selection):
+    time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    selection.datetime = time
+
+    # prepare path
+    file_name = "{}-{}.json".format(time, selection.username)
+    path = "data/ranking/relevancy"
+    full_path = os.path.join(dataset, path, file_name)
+
+    # convert to bytes
+    dict_data = selection.to_dict()
+    json_data = json.dumps(dict_data, indent=4).encode('utf-8')
+    data = BytesIO(json_data)
+
+    # upload
+    cmd.upload_data(request.app.minio_client, "datasets", full_path, data)
+
+    return True
