db75d2c32a4e20858538332e0f9ff08faf4a5ecb
ssss <sdevdariani9@gmail.com>
2023-10-10 13:29:55 +0400

    update api function

--- orchestration/api/api_model.py
+++ orchestration/api/api_model.py
@@ -4,13 +4,13 @@ import json
 
 router = APIRouter()
 
-@router.get("/datasets/{dataset}/models")
-def get_dataset_models(request: Request, dataset: str):
+@router.get("/models/list-relevancy/{dataset}")
+def get_relevancy_models(request: Request, dataset: str):
     # Bucket name
     bucket_name = "datasets"
     
-    # Prefix/path inside the bucket where models for the dataset are stored in MinIO
-    prefix = f"{dataset}/models/ab_ranking_efficient_net"
+    # Prefix/path inside the bucket where relevancy models for the dataset are stored in MinIO
+    prefix = f"{dataset}/models/relevancy/ab_ranking_efficient_net"
 
     # Fetch list of model objects from MinIO
     model_objects = cmd.get_list_of_objects_with_prefix(request.app.minio_client, bucket_name, prefix)
@@ -33,6 +33,32 @@ def get_dataset_models(request: Request, dataset: str):
             }
             
             # Append the rearranged content of the JSON file to the models_list
+            models_list.append(arranged_content)
+
+    return models_list
+
+
+@router.get("/models/list-ranking/{dataset}")
+def get_ranking_models(request: Request, dataset: str):
+    # Bucket name
+    bucket_name = "datasets"
+    
+    # Prefix/path inside the bucket where ranking models for the dataset are stored in MinIO
+    prefix = f"{dataset}/models/ranking/ab_ranking_efficient_net"
+
+    # Similar to the previous function, fetch, parse and rearrange the model information
+    model_objects = cmd.get_list_of_objects_with_prefix(request.app.minio_client, bucket_name, prefix)
+
+    models_list = []
+    for obj in model_objects:
+        if obj.endswith('.json'):
+            data = cmd.get_file_from_minio(request.app.minio_client, bucket_name, obj)
+            model_content = json.loads(data.read().decode('utf-8'))
+            model_name = obj.split('/')[-1].split('.')[0] + '.pth'
+            arranged_content = {
+                'model_name': model_name,
+                **model_content
+            }
             models_list.append(arranged_content)
 
     return models_list
