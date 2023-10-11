d1bb49dcb050dbc75f9a943c3fb96399bb778f7b
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-10-11 14:20:16 +0100

    http get requests for dataset rate & dataset generation policy

--- prompt_job_generator/http_requests/request.py
+++ prompt_job_generator/http_requests/request.py
@@ -3,6 +3,7 @@ import requests
 SERVER_ADRESS = 'http://192.168.3.1:8111'
 #SERVER_ADRESS = 'http://127.0.0.1:8000'
 
+
 def http_get_completed_jobs_count(dataset_name: str):
     url = SERVER_ADRESS + "/job/count-completed?dataset=" + dataset_name
 
@@ -15,7 +16,6 @@ def http_get_completed_jobs_count(dataset_name: str):
 
     except Exception as e:
         print('request exception ', e)
-
 
     return None
 
@@ -33,7 +33,6 @@ def http_get_pending_jobs_count(dataset_name: str):
     except Exception as e:
         print('request exception ', e)
 
-
     return None
 
 
@@ -49,7 +48,6 @@ def http_get_in_progress_jobs_count(dataset_name: str):
 
     except Exception as e:
         print('request exception ', e)
-
 
     return None
 
@@ -82,8 +80,8 @@ def http_get_dataset_rate(dataset_name: str):
     except Exception as e:
         print('request exception ', e)
 
-
     return None
+
 
 def http_get_all_dataset_rate():
     url = SERVER_ADRESS + f"/dataset/get-all-dataset-rate"
@@ -98,6 +96,68 @@ def http_get_all_dataset_rate():
     except Exception as e:
         print('request exception ', e)
 
+    return None
+
+
+def http_get_dataset_generation_policy(dataset : str):
+    url = SERVER_ADRESS + f"/dataset/get-generation-policy?dataset={dataset}"
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            job_json = response.json()
+            return job_json
+
+    except Exception as e:
+        print('request exception ', e)
 
     return None
 
+
+def http_get_all_dataset_generation_policy(dataset : str):
+    url = SERVER_ADRESS + f"/dataset/get-all-dataset-generation-policy?dataset={dataset}"
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            job_json = response.json()
+            return job_json
+
+    except Exception as e:
+        print('request exception ', e)
+
+    return None
+
+
+def http_get_dataset_top_k_value(dataset : str):
+    url = SERVER_ADRESS + f"/dataset/get-top-k?dataset={dataset}"
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            job_json = response.json()
+            return job_json
+
+    except Exception as e:
+        print('request exception ', e)
+
+    return None
+
+
+def http_get_dataset_job_per_second(dataset : str, sample_size : int):
+    url = SERVER_ADRESS + f"/dataset/get-dataset-job-per-second?dataset={dataset}&sample_size={sample_size}"
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            job_json = response.json()
+            return job_json
+
+    except Exception as e:
+        print('request exception ', e)
+
+    return None
