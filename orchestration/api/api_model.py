5fdd747e0c49366823e7deee496dc73681599a61
sandrodevdariani <73473507+sandrodevdariani@users.noreply.github.com>
2023-10-13 18:56:16 +0400

    Update api_model.py

--- orchestration/api/api_model.py
+++ orchestration/api/api_model.py
@@ -9,11 +9,14 @@ def get_relevancy_models(request: Request, dataset: str = Query(...)):
     # Bucket name
     bucket_name = "datasets"
     
-    # Prefix/path inside the bucket where relevancy models for the dataset are stored in MinIO
-    prefix = f"{dataset}/models/relevancy/ab_ranking_efficient_net"
-
-    # Fetch list of model objects from MinIO
-    model_objects = cmd.get_list_of_objects_with_prefix(request.app.minio_client, bucket_name, prefix)
+    # Base path where relevancy models for the dataset are stored in MinIO
+    base_path = f"{dataset}/models/relevancy"
+    
+    # Fetch list of model objects from MinIO for the base path, recursively
+    model_objects = []
+    objects = request.app.minio_client.list_objects(bucket_name, prefix=base_path, recursive=True)
+    for obj in objects:
+        model_objects.append(obj.object_name)
 
     # Parse models list from model_objects
     models_list = []
@@ -23,12 +26,16 @@ def get_relevancy_models(request: Request, dataset: str = Query(...)):
             data = cmd.get_file_from_minio(request.app.minio_client, bucket_name, obj)
             model_content = json.loads(data.read().decode('utf-8'))
             
-            # Extract model name from the JSON file name (like '2023-10-09.json') and append .pth
-            model_name = obj.split('/')[-1].split('.')[0] 
+            # Extract model name from the JSON file name (like '2023-10-09.json')
+            model_name = obj.split('/')[-1].split('.')[0]
             
-            # Construct a new dictionary with model_name at the top
+            # Extract model architecture from the object path (like 'ab_ranking_linear' or 'ab_ranking_efficient_net')
+            model_architecture = obj.split('/')[-2]
+            
+            # Construct a new dictionary with model_name and model_architecture at the top
             arranged_content = {
                 'model_name': model_name,
+                'model_architecture': model_architecture,
                 **model_content
             }
             
@@ -38,27 +45,44 @@ def get_relevancy_models(request: Request, dataset: str = Query(...)):
     return models_list
 
 
+
+
 @router.get("/models/rank-embedding/list-models")
 def get_ranking_models(request: Request, dataset: str = Query(...)):
     # Bucket name
     bucket_name = "datasets"
     
-    # Prefix/path inside the bucket where ranking models for the dataset are stored in MinIO
-    prefix = f"{dataset}/models/ranking/ab_ranking_efficient_net"
+    # Base path where ranking models for the dataset are stored in MinIO
+    base_path = f"{dataset}/models/ranking"
+    
+    # Fetch list of model objects from MinIO for the base path, recursively
+    model_objects = []
+    objects = request.app.minio_client.list_objects(bucket_name, prefix=base_path, recursive=True)
+    for obj in objects:
+        model_objects.append(obj.object_name)
 
-    # Similar to the previous function, fetch, parse and rearrange the model information
-    model_objects = cmd.get_list_of_objects_with_prefix(request.app.minio_client, bucket_name, prefix)
-
+    # Parse models list from model_objects
     models_list = []
     for obj in model_objects:
+        # Filter out only the .json files for processing
         if obj.endswith('.json'):
             data = cmd.get_file_from_minio(request.app.minio_client, bucket_name, obj)
             model_content = json.loads(data.read().decode('utf-8'))
-            model_name = obj.split('/')[-1].split('.')[0] 
+            
+            # Extract model name from the JSON file name (like '2023-10-09.json')
+            model_name = obj.split('/')[-1].split('.')[0]
+            
+            # Extract model architecture from the object path (like 'ab_ranking_linear' or 'ab_ranking_efficient_net')
+            model_architecture = obj.split('/')[-2]
+            
+            # Construct a new dictionary with model_name and model_architecture at the top
             arranged_content = {
                 'model_name': model_name,
+                'model_architecture': model_architecture,
                 **model_content
             }
+            
+            # Append the rearranged content of the JSON file to the models_list
             models_list.append(arranged_content)
 
     return models_list
