73c3f3ed041242283193f1a21e1035c3b10135fb
kenje4090 <kenje4090@gmail.com>
2023-10-16 21:48:14 +0800

    Add export cuda visible edvices

--- worker/run_worker.sh
+++ worker/run_worker.sh
@@ -2,4 +2,4 @@
 
 # pass the first arg given as device
 export CUDA_VISIBLE_DEVICES=0,1,2
-CUDA_VISIBLE_DEVICES=0,1,2 python3 ./worker/worker.py --device $1 --minio-access-key $2 --minio-secret-key $3 --worker-type $4
+python3 ./worker/worker.py --device $1 --minio-access-key $2 --minio-secret-key $3 --worker-type $4
