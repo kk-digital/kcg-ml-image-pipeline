a121a14f7a76429646791767b5e095dbc493f69c
kenje4090 <kenje4090@gmail.com>
2023-10-16 20:17:39 +0800

    Add set cuda visible devices

--- worker/run_worker.sh
+++ worker/run_worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # pass the first arg given as device
-python3 ./worker/worker.py --device $1 --minio-access-key $2 --minio-secret-key $3 --worker-type $4
+CUDA_VISIBLE_DEVICES=0,1,2 python3 ./worker/worker.py --device $1 --minio-access-key $2 --minio-secret-key $3 --worker-type $4
