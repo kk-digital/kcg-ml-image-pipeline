d4391b53d97cbbeffcd294da65851aecb536b92e
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-10-25 16:11:50 +0100

    update

--- orchestration/api/api_job.py
+++ orchestration/api/api_job.py
@@ -101,7 +101,7 @@ def get_jobs_count_last_n_hour(request: Request, dataset, hours: int):
     # Query the collection to count the documents created in the last hour
     pending_query = {"task_input_dict.dataset": dataset, "task_creation_time": {"$gte": time_ago}}
     in_progress_query = {"task_input_dict.dataset": dataset, "task_creation_time": {"$gte": time_ago}}
-    completed_query = {"task_input_dict.dataset": dataset}
+    completed_query = {"task_input_dict.dataset": dataset, "task_creation_time": {"$gte": time_ago}}
 
     count = 0
 
