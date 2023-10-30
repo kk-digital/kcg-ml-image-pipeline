488eb13903c71d11c18d3f5a51463415370cca70
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-10-30 08:18:58 +0100

    remove unnecessary code

--- prompt_job_generator/prompt_generation_prompt_queue.py
+++ prompt_job_generator/prompt_generation_prompt_queue.py
@@ -82,21 +82,10 @@ class PromptGenerationPromptQueue:
 
         total_prompt_count = int(total_prompt_count)
 
-        #if base_prompts_csv_path is None:
-            #print('base prompt file is not found for dataset ', dataset)
-
         scoring_model = prompt_job_generator_state.get_dataset_scoring_model(dataset)
 
-        #if scoring_model is None:
-            #print('prompt scoring model is not found for dataset ', dataset)
-        #else:
-            #print('prompt scoring model is available for dataset ', dataset)
-
-
-        print('dataset : ', dataset, ' base prompt csv ', base_prompts_csv_path)
         base_prompt_population = load_base_prompts(base_prompts_csv_path)
 
-        #print(f'generating {total_prompt_count} prompts for dataset {dataset}')
         prompts = []
 
         if generation_policy == 'top-k':
