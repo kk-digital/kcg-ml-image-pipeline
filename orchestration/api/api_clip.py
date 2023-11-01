a405b4dd55b83f98c2a2051546bae183f52200c8
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-11-01 09:41:35 +0100

    getting clip vector of a specific phrase

--- orchestration/api/api_clip.py
+++ orchestration/api/api_clip.py
@@ -5,6 +5,8 @@ CLIP_SERVER_ADRESS = 'http://127.0.0.1:8002'
 
 router = APIRouter()
 
+
+# --------- Http requests -------------
 def http_clip_server_add_phrase(phrase: str):
     url = CLIP_SERVER_ADRESS + "/add-phrase?phrase=" + phrase
 
@@ -12,8 +14,26 @@ def http_clip_server_add_phrase(phrase: str):
         response = requests.put(url)
 
         if response.status_code == 200:
-            job_json = response.json()
-            return job_json
+            result_json = response.json()
+            return result_json
+
+    except Exception as e:
+        print('request exception ', e)
+
+    return None
+
+
+
+
+def http_clip_server_clip_vector_from_phrase(phrase: str):
+    url = CLIP_SERVER_ADRESS + "/clip-vector?phrase=" + phrase
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            result_json = response.json()
+            return result_json
 
     except Exception as e:
         print('request exception ', e)
@@ -26,4 +46,9 @@ def add_phrase(request: Request,
 
     return http_clip_server_add_phrase(phrase)
 
-    return True
+
+@router.get("/clip/clip-vector", description="Gets a clip vector of a specific phrase")
+def add_phrase(request: Request,
+               phrase : str):
+
+    return http_clip_server_clip_vector_from_phrase(phrase)
