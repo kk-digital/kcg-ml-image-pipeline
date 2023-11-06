c1b973a25d259e36d519c849752c8bf2270bc517
kenje4090 <kenje4090@gmail.com>
2023-11-06 09:03:20 +0000

    Add concurrent uploading of scores and residuals, add uploading of residual percentile

--- training_worker/http/request.py
+++ training_worker/http/request.py
@@ -153,6 +153,21 @@ def http_add_percentile(percentile_data):
     return None
 
 
+def http_add_residual_percentile(residual_percentile_data):
+    url = SERVER_ADRESS + "/residual-percentile/set-image-rank-residual-percentile"
+    headers = {"Content-type": "application/json"}  # Setting content type header to indicate sending JSON data
+
+    try:
+        response = requests.post(url, json=residual_percentile_data, headers=headers)
+
+        if response.status_code != 200:
+            print(f"request failed with status code: {response.status_code}: {str(response.content)}")
+    except Exception as e:
+        print('request exception ', e)
+
+    return None
+
+
 # Get list of all dataset names
 def http_get_dataset_names():
     url = SERVER_ADRESS + "/dataset/list"
