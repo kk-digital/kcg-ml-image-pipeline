afa5a1db212ae569b2de49d1132f2fe041f9de2e
kenje4090 <kenje4090@gmail.com>
2023-11-10 09:14:34 +0000

    Update graph report, add performance graph for hyperparam

--- training_worker/ab_ranking/script/hyperparam_search_xgboost_ranking_pairwise.py
+++ training_worker/ab_ranking/script/hyperparam_search_xgboost_ranking_pairwise.py
@@ -8,10 +8,15 @@ import sys
 from ray import train, tune
 from ray.tune.integration.xgboost import TuneReportCheckpointCallback
 from ray.tune.search.hyperopt import HyperOptSearch
+from ray.air import session
 import numpy as np
 from datetime import datetime
 from pytz import timezone
+import time
 from io import BytesIO
+import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
+
 base_directory = os.getcwd()
 sys.path.insert(0, base_directory)
 
@@ -23,6 +28,42 @@ from utility.minio import cmd
 from training_worker.ab_ranking.model import constants
 from training_worker.ab_ranking.model.reports import upload_score_residual
 from training_worker.ab_ranking.model.reports.graph_report_ab_ranking_linear import get_graph_report
+
+def get_performance_graph_report(results):
+    # Initialize all graphs/subplots
+    plt.figure(figsize=(22, 20))
+    figure_shape = (2, 2)
+    performance = plt.subplot2grid(figure_shape, (0, 0), rowspan=2, colspan=2)
+    # ----------------------------------------------------------------------------------------------------------------#
+    # performance
+    loss_values = []
+    for res in results:
+        loss_values.append(res.metrics["validation-error"])
+
+    x_axis_values = [i for i in range(len(loss_values))]
+    performance.plot(x_axis_values,
+                     loss_values,
+                     label="xgboost",
+                     c="#281ad9")
+
+    performance.set_xlabel("Iterations")
+    performance.set_ylabel("Loss")
+    performance.set_title("Loss vs Iterations")
+    performance.legend()
+
+    performance.autoscale(enable=True, axis='both')
+
+    # Save figure
+    # graph_path = os.path.join(model_output_path, graph_name)
+    # plt.subplots_adjust(left=0.15, hspace=0.5)
+    # plt.savefig(graph_path)
+    # plt.show()
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+
+    return buf
+
 
 def train_ranking(config: dict, training_data, training_targets, validation_data, validation_targets):
     # Create regression matrices
@@ -40,11 +81,14 @@ def train_ranking(config: dict, training_data, training_targets, validation_data
     dtest_reg.set_group(validation_group)
 
     evals = [(dtest_reg, "validation")]
+    evals_result = {}
+
     xgb.train(config,
               dtrain_reg,
               evals=evals,
               verbose_eval=False,
               callbacks=[TuneReportCheckpointCallback(filename="model.xgb")],
+              evals_result=evals_result
               )
 
 
@@ -120,7 +164,7 @@ def tune_xgboost(training_data,
         max_t=50, grace_period=1, reduction_factor=2  # 10 training iterations
     )
 
-    hyper_opt = HyperOptSearch()
+    hyper_opt = HyperOptSearch(metric="validation-error", mode="min")
     trainable_with_cpu = tune.with_resources(train_ranking, {"cpu": 2})
     tuner = tune.Tuner(
         tune.with_parameters(trainable_with_cpu,
@@ -133,14 +177,13 @@ def tune_xgboost(training_data,
             mode="min",
             search_alg=hyper_opt,
             scheduler=scheduler,
-            num_samples=1000,
+            num_samples=100,
         ),
         param_space=search_space,
     )
     results = tuner.fit()
 
     return results
-
 
 def do_hyperparameter_search(dataset_name="environmental",
                              minio_ip_addr=None,  # will use defualt if none is given
@@ -173,15 +216,6 @@ def do_hyperparameter_search(dataset_name="environmental",
 
     # placeholders for the graph report
     epochs = 1000
-    learning_rate = 0.05
-    training_batch_size = 1
-    weight_decay = 0.00
-    normalize_vectors = True
-    pooling_strategy = constants.AVERAGE_POOLING
-    add_loss_penalty = True
-    target_option = constants.TARGET_1_AND_0
-    duplicate_flip_option = constants.DUPLICATE_AND_FLIP_ALL
-    randomize_data_per_epoch = True
     input_shape = 768
 
     # get final filename
@@ -235,7 +269,6 @@ def do_hyperparameter_search(dataset_name="environmental",
         prob = forward_bradley_terry(validation_x_pred_scores[i], validation_y_pred_scores[i])
         validation_pred_prob.append(prob)
 
-    ab_model = ABRankingModel(inputs_shape=768)
     training_predicted_score_images_x = train_x_pred_scores
     training_predicted_score_images_y = train_y_pred_scores
     training_predicted_probabilities = train_pred_prob
@@ -246,6 +279,8 @@ def do_hyperparameter_search(dataset_name="environmental",
     validation_target_probabilities = validation_targets
     training_loss_per_epoch = [0] * epochs
     validation_loss_per_epoch = [0] * epochs
+    training_loss = 0.0
+    validation_loss = 0.0
 
     # data for chronological score graph
     training_shuffled_indices_origin = []
@@ -289,57 +324,55 @@ def do_hyperparameter_search(dataset_name="environmental",
     graph_name = "{}.png".format(filename)
     graph_output_path = os.path.join(output_path, graph_name)
 
-    graph_buffer = get_graph_report(ab_model,
-                                    training_predicted_probabilities,
-                                    training_target_probabilities,
-                                    validation_predicted_probabilities,
-                                    validation_target_probabilities,
-                                    training_predicted_score_images_x,
-                                    training_predicted_score_images_y,
-                                    validation_predicted_score_images_x,
-                                    validation_predicted_score_images_y,
-                                    training_total_size,
-                                    validation_total_size,
-                                    training_loss_per_epoch,
-                                    validation_loss_per_epoch,
-                                    epochs,
-                                    learning_rate,
-                                    training_batch_size,
-                                    weight_decay,
-                                    date_now,
-                                    network_type,
-                                    input_type,
-                                    input_shape,
-                                    output_type,
-                                    train_sum_correct,
-                                    validation_sum_correct,
-                                    "",
-                                    dataset_name,
-                                    pooling_strategy,
-                                    normalize_vectors,
-                                    -1,
-                                    add_loss_penalty,
-                                    target_option,
-                                    duplicate_flip_option,
-                                    randomize_data_per_epoch,
-                                    -1,
-                                    training_shuffled_indices_origin,
-                                    validation_shuffled_indices_origin,
-                                    dataset_loader.total_selection_datapoints)
+    graph_buffer = get_graph_report(training_loss=training_loss,
+                                    validation_loss=validation_loss,
+                                    train_prob_predictions=training_predicted_probabilities,
+                                    training_targets=training_target_probabilities,
+                                    validation_prob_predictions=validation_predicted_probabilities,
+                                    validation_targets=validation_target_probabilities,
+                                    training_pred_scores_img_x=training_predicted_score_images_x,
+                                    training_pred_scores_img_y=training_predicted_score_images_y,
+                                    validation_pred_scores_img_x=validation_predicted_score_images_x,
+                                    validation_pred_scores_img_y=validation_predicted_score_images_y,
+                                    training_total_size=training_total_size,
+                                    validation_total_size=validation_total_size,
+                                    training_losses=training_loss_per_epoch,
+                                    validation_losses=validation_loss_per_epoch,
+                                    epochs=epochs,
+                                    date=date_now,
+                                    network_type=network_type,
+                                    input_type=input_type,
+                                    input_shape=input_shape,
+                                    output_type=output_type,
+                                    train_sum_correct=train_sum_correct,
+                                    validation_sum_correct=validation_sum_correct,
+                                    loss_func="",
+                                    dataset_name=dataset_name,
+                                    training_shuffled_indices_origin=training_shuffled_indices_origin,
+                                    validation_shuffled_indices_origin=validation_shuffled_indices_origin,
+                                    total_selection_datapoints=dataset_loader.total_selection_datapoints,
+                                    )
 
     # upload the graph report
     cmd.upload_data(dataset_loader.minio_client, bucket_name, graph_output_path, graph_buffer)
 
+    # performance
+    performance_graph = get_performance_graph_report(results)
+    graph_name = "{}_performance.png".format(filename)
+    graph_output_path = os.path.join(output_path, graph_name)
+    cmd.upload_data(dataset_loader.minio_client, bucket_name, graph_output_path, performance_graph)
+
 if __name__ == "__main__":
     start_time = time.time()
 
-    do_hyperparameter_search(dataset_name="environmental",
+    do_hyperparameter_search(dataset_name="test-generations",
                              minio_ip_addr=None,  # will use default if none is given
                              minio_access_key="nkjYl5jO4QnpxQU0k0M1",
                              minio_secret_key="MYtmJ9jhdlyYx3T1McYy4Z0HB3FkxjmITXLEPKA1",
-                             input_type="embedding",
+                             input_type="clip",
                              )
 
     time_elapsed = time.time() - start_time
     print("Time elapsed: {0}s".format(format(time_elapsed, ".2f")))
-
+    # while not eval_results_queue.empty():
+    #     print(eval_results_queue.get())
