53325aa262351e206472c9fd1d86cdb9a28c39f2
Quincy <quincy_lim@protonmail.com>
2023-11-16 19:09:47 +0800

    remove embeddings in csv

--- scripts/generate_prompt_mutator_dataset.py
+++ scripts/generate_prompt_mutator_dataset.py
@@ -235,6 +235,13 @@ def main(
         if ((i + 1) % 10000) == 0:
             df_data_removed = pd.DataFrame(df_data_removed)
             df_data_add = pd.DataFrame(df_data_add)
+
+            df_data_removed = df_data_removed.drop(
+                ['original_embedding', 'removed_embedding', 'positional_encoding'], axis=1
+            )
+            df_data_add = df_data_add.drop(
+                ['original_embedding', 'add_embedding', 'positional_encoding'], axis=1
+            )
             removed_path = os.path.join(
                 minio_upload_path,
                 str(folder).zfill(6),
