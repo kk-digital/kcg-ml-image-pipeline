04dc89e87e596fc2679fd14a80beb613c286bc2d
rachid2198 <rachid9821@gmail.com>
2023-11-19 15:53:21 +0100

    update

--- scripts/test_prompt_mutator.py
+++ scripts/test_prompt_mutator.py
@@ -78,9 +78,10 @@ def get_mean_pooled_embedding(embedding):
 
     return embedding.cpu().numpy()
 
-def get_best_substitution_choice(xgboost_model ,prompt_str, 
-                  prompt_score, prompt_embedding, 
-                  phrase_embeddings):
+def get_best_substitution_choice(sigma_model, 
+                                 prompt_str, 
+                                 prompt_score, prompt_embedding, 
+                                 phrase_embeddings):
     
     # get mean pooled embedding of prompt for xgboost model
     pooled_prompt_embedding= get_mean_pooled_embedding(prompt_embedding)
@@ -89,7 +90,7 @@ def get_best_substitution_choice(xgboost_model ,prompt_str,
     prompt_list = prompt_str.split(',')
     token_number= len(prompt_list)
     # list of delta scores for each substitution
-    delta_scores=[]
+    sigma_scores=[]
 
     # Randomly select a phrase from the dataset and get an embedding
     for token in range(token_number):
@@ -98,19 +99,17 @@ def get_best_substitution_choice(xgboost_model ,prompt_str,
         # get full input for xgboost model
         substituted_embedding= get_mean_pooled_embedding(substituted_embedding)
         substitution_input= np.concatenate([pooled_prompt_embedding, substituted_embedding, [token], [prompt_score]])
-        # add delta score to the list of scores
-        delta_score=xgboost_model.predict([substitution_input])[0]
-        if delta_score>0:
-            delta_scores.append(-delta_score)
+        # add sigma score to the list of scores
+        sigma_score=sigma_model.predict([substitution_input])[0]
+        sigma_scores.append(-sigma_score)
     
-    tokens_to_substitute=np.argsort(delta_scores)
-
+    tokens_to_substitute=np.argsort(sigma_scores)
     return tokens_to_substitute
 
 def mutate_prompt(device, embedding_model,
-                  xgboost_model, scoring_model, 
+                  sigma_model, scoring_model, 
                   prompt_str, phrase_list, 
-                  max_iterations=1000, early_stopping=10):
+                  max_iterations=1000, early_stopping=3):
     
     # calculate prompt embedding and score
     prompt_embedding=get_prompt_embedding(device, embedding_model, prompt_str)
@@ -123,40 +122,44 @@ def mutate_prompt(device, embedding_model,
     # run mutation process iteratively untill score converges
     for i in range(max_iterations):
         print(f"iteration {i}")
-        substitution_tokens=get_best_substitution_choice(xgboost_model, 
+        tokens=get_best_substitution_choice(sigma_model, 
                                                         prompt_str,
                                                         prompt_score,
                                                         prompt_embedding, 
                                                         phrase_embeddings)
         
-        for token in substitution_tokens:
+        if len(tokens)==0:
+            break
+
+        for token in tokens:
             #Create a modified prompt with the substitution
             prompt_list = prompt_str.split(',')
+            substituted_phrase= prompt_list[token]
             prompt_list[token] = random.choice(phrase_list)
-            phrase_embeddings[token]= get_prompt_embedding(device, embedding_model, prompt_list[token])
             modified_prompt_str = ",".join(prompt_list)
 
             #calculate modified prompt embedding and score
             modified_prompt_embedding=get_prompt_embedding(device, embedding_model, modified_prompt_str)
             modified_prompt_score= get_prompt_score(scoring_model, modified_prompt_embedding)
 
-            if prompt_score < modified_prompt_score:
-                prompt_str=modified_prompt_str
-                prompt_embedding = modified_prompt_embedding
+            # check if score improves
+            if(prompt_score< modified_prompt_score):
+                prompt_str= modified_prompt_str
+                prompt_embedding= modified_prompt_embedding
+                phrase_embeddings[token]= get_prompt_embedding(device, embedding_model, substituted_phrase)
                 break
 
         # check if score increased
-        if prompt_score > modified_prompt_score:
+        if prompt_score >= modified_prompt_score:
             early_stopping-=1
         else:
-            early_stopping=10
+            early_stopping=3
         
         prompt_score= modified_prompt_score
         print(f"----mutated prompt str: {prompt_str}")
         print(f"----resulting score: {prompt_score}")
         if early_stopping==0:
             break
-
 
 def main():
     args = parse_args()
@@ -181,8 +184,12 @@ def main():
     elm_model= load_model(768, minio_client, device)
 
     # load the xgboost model
-    prompt_mutator= PromptMutator(minio_client=minio_client, output_type="delta_score")
-    prompt_mutator.load_model()
+    sigma_model= PromptMutator(minio_client=minio_client, output_type="sigma_score")
+    sigma_model.load_model()
+
+    # load the classification model
+    # binary_model= MulticlassPromptMutator(minio_client=minio_client)
+    # binary_model.load_model('environmental/output/prompt_mutator/binary_prompt_mutator.json')
 
     # prompt and phrases
     prompt_str="environmental, pixel art, concept art, side scrolling, video game, neo city, (1 girl), white box, puffy lips, cinematic lighting, colorful, steampunk, partially submerged, original, 1girl, night, ribbon choker, see through top, black tissues, a masterpiece, high heel, hand on own crotch"
@@ -192,7 +199,7 @@ def main():
     mutate_prompt(device, 
                   embedding_model=clip, 
                   scoring_model=elm_model, 
-                  xgboost_model=prompt_mutator,
+                  sigma_model=sigma_model,
                   prompt_str=prompt_str,
                   phrase_list=phrases_list)
     
