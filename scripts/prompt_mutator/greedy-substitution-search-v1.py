f34eef323a1f413074445696e4f10624fd98ef7a
rachid2198 <rachid9821@gmail.com>
2023-11-23 16:43:43 +0100

    testing

--- scripts/prompt_mutator/greedy-substitution-search-v1.py
+++ scripts/prompt_mutator/greedy-substitution-search-v1.py
@@ -148,7 +148,7 @@ def rank_substitution_choices(device,
         substitution_input= np.concatenate([pooled_prompt_embedding, substituted_embedding, substitute_embedding, [token], [prompt_score]])
         # add sigma score to the list of scores
         pred=binary_model.predict_probs([substitution_input])[0]
-        if pred["decrease"]>0.66:
+        if pred["increase"]>0.6:
             sigma_score=sigma_model.predict([substitution_input])[0]
             sigma_scores.append(-sigma_score)
             sub_phrases.append(substitute_phrase)
