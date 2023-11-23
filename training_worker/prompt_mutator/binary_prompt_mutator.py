5cd5fdd2320f3e7d36cf59e2faf7e0d826c5d6f4
rachid2198 <rachid9821@gmail.com>
2023-11-23 15:05:44 +0100

    testing

--- training_worker/prompt_mutator/binary_prompt_mutator.py
+++ training_worker/prompt_mutator/binary_prompt_mutator.py
@@ -174,7 +174,6 @@ class BinaryPromptMutator:
         # Load the model from the temporary file into XGBClassifier
         self.model = xgb.XGBClassifier()
         self.model.load_model(temp_file.name)
-        self.model.set_param({"device": "cuda"})
 
         # Remove the temporary file
         os.remove(temp_file.name)
