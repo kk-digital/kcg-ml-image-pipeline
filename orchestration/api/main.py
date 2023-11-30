0885a8de9dd7b6df313bb706c741841bd8e1b71d
ssss <sdevdariani9@gmail.com>
2023-11-30 09:14:33 +0400

    add active learning router in main.py

--- orchestration/api/main.py
+++ orchestration/api/main.py
@@ -21,6 +21,7 @@ from orchestration.api.api_percentile import router as percentile_router
 from orchestration.api.api_residual_percentile import router as residual_percentile_router
 from orchestration.api.api_image_by_rank import router as image_by_rank_router
 from orchestration.api.api_queue_ranking import router as queue_ranking
+from orchestration.api.api_active_learning import router as active_learning 
 from utility.minio import cmd
 
 config = dotenv_values("./orchestration/api/.env")
@@ -52,6 +53,7 @@ app.include_router(residual_router)
 app.include_router(percentile_router)
 app.include_router(residual_percentile_router)
 app.include_router(queue_ranking)
+app.include_router(active_learning)
 
 
 def get_minio_client(minio_access_key, minio_secret_key):
