981305a450d06a89dee10914b9a149a28eafdb53
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-12-01 11:50:51 +0100

    independent approx v1 class improvements

--- prompt_job_generator/independent_approx_v1/independent_approx_v1.py
+++ prompt_job_generator/independent_approx_v1/independent_approx_v1.py
@@ -7,7 +7,9 @@ base_directory = "./"
 sys.path.insert(0, base_directory)
 
 from scripts.prompt_job_generator.prompt_generator_using_independent_approx_v1_csv import (PhraseScoresLoader,
-                                                                                           get_cumulative_probability_arr)
+                                                                                           get_cumulative_probability_arr,
+                                                                                           generate_prompts_array)
+
 
 # Helper class that generates prompts using
 # Indenpendent approx v1
@@ -26,6 +28,8 @@ class IndependentApproxV1:
         self.negative_cumulative_probability_arr = None
         self.positive_phrase_scores_csv = None
         self.negative_phrase_scores_csv = None
+
+        self.csv_loaded = False
 
     def load_csv(self, minio_client, positive_phrase_scores_csv, negative_phrase_scores_csv):
         dataset_name = self.dataset_name
@@ -69,3 +73,25 @@ class IndependentApproxV1:
         self.negative_phrase_scores_loader = negative_phrase_scores_loader
         self.negative_phrase_origin_indexes = negative_phrase_origin_indexes
         self.negative_cumulative_probability_arr = negative_cumulative_probability_arr
+        self.csv_loaded = True
+
+    def generate_prompts(self, prompt_count):
+
+        # making sure the csv was loaded from minio
+        if self.csv_loaded is False:
+            return []
+
+        prompt_list = generate_prompts_array(positive_phrase_scores_loader=self.positive_phrase_scores_loader,
+                                             positive_phrase_origin_indexes=self.positive_phrase_origin_indexes,
+                                             positive_cumulative_probability_arr=self.positive_cumulative_probability_arr,
+                                             negative_phrase_scores_loader=self.negative_phrase_scores_loader,
+                                             negative_phrase_origin_indexes=self.negative_phrase_origin_indexes,
+                                             negative_cumulative_probability_arr=self.negative_cumulative_probability_arr,
+                                             prompt_count=prompt_count)
+
+        # prompt list item is a dictionary type
+        # {
+        #   "positive_prompt": positive_prompt,
+        #   "negative_prompt": negative_prompt
+        # }
+        return prompt_list
