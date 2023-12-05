193bdd0b23c6d00a8ad8c7dcfc53bf12c0e0c322
rachid2198 <rachid9821@gmail.com>
2023-12-05 21:55:22 +0100

    getting datapoints with highest delta

--- scripts/prompt_mutator/greedy-substitution-search-v1.py
+++ scripts/prompt_mutator/greedy-substitution-search-v1.py
@@ -266,7 +266,7 @@ class PromptSubstitutionGenerator:
                 original_embeddings.append(phrase_embeddings[token])
             
         # substitutions are sorted from highest sigma score to lowest
-        token_order= np.argsort(-sigma_scores)
+        token_order= np.argsort(-1 * sigma_scores)
         tokens=[tokens[token_pos] for token_pos in token_order]
         sub_phrases=[sub_phrases[token_pos] for token_pos in token_order]
         sub_embeddings=[sub_embeddings[token_pos] for token_pos in token_order]
