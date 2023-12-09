36e105664c72fbb9950eadb12090b104c62e687c
rachid2198 <rachid9821@gmail.com>
2023-12-09 03:20:12 +0100

    testing

--- scripts/prompt_mutator/greedy-substitution-search-v1.py
+++ scripts/prompt_mutator/greedy-substitution-search-v1.py
@@ -373,7 +373,7 @@ class PromptSubstitutionGenerator:
             # test every choice and take the first choice that increases score
             for substitution in substitution_choices:
                 # get substitution data
-                print(substitution)
+                print(substitution['position'])
                 position=substitution['position'],
                 substitute_phrase=substitution['substitute_phrase'],
                 substitute_embedding=substitution['substitute_embedding'],
