77ab436a58fcf0792cc4582c420108b28f8eb8b1
rachid2198 <rachid9821@gmail.com>
2023-12-19 00:21:51 +0100

    changing function for checking token length

--- scripts/prompt_mutator/greedy-substitution-search-v1.py
+++ scripts/prompt_mutator/greedy-substitution-search-v1.py
@@ -225,7 +225,7 @@ class PromptSubstitutionGenerator:
         return embedding.detach().cpu().numpy()
 
     # function to get a random phrase from civitai with a max token size for substitutions
-    def choose_random_phrase(self, prompt, phrase, position, max_token_length=77):
+    def choose_random_phrase(self, prompt, position, max_token_length=77):
         prompt_token_length = max_token_length + 1 
         while(prompt_token_length > max_token_length):
             random_index=random.randrange(0, len(self.phrase_list))
@@ -261,7 +261,7 @@ class PromptSubstitutionGenerator:
             # get the substituted phrase embedding
             substituted_embedding = phrase_embeddings[phrase_position]
             # get a random phrase from civitai to substitute with
-            phrase_index, random_phrase= self.choose_random_phrase(prompt_list, substituted_phrase, phrase_position)
+            phrase_index, random_phrase= self.choose_random_phrase(prompt_list, phrase_position)
             # get phrase string
             substitute_phrase = random_phrase
             # get phrase embedding by its index
