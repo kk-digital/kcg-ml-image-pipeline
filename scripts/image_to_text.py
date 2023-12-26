0bba954f54f7a13f33c9a255b4b96fb25a170ac7
rachid2198 <rachid9821@gmail.com>
2023-12-26 21:57:16 +0100

    using same tokenization as the clip text embedder to avoid errors

--- scripts/image_to_text.py
+++ scripts/image_to_text.py
@@ -131,7 +131,7 @@ def truncate_prompts(embedder, prompt, max_length=77):
     
     # Tokenize the prompt
     batch_encoding = tokenizer(prompt, truncation=False, max_length=max_length, return_length=True,
-                                        return_overflowing_tokens=False, padding="max_length", return_tensors="pt")
+                                        return_overflowing_tokens=False, padding="max_length", return_tensors="pt")[0]
     
     input_ids = batch_encoding['input_ids']
     # Truncate to the desired length
