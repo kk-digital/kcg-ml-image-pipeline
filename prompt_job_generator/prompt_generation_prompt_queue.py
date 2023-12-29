00523753db1a4d075fa4fbfcaeca78804dea5dd3
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-12-29 14:13:45 +0100

    a

--- prompt_job_generator/prompt_generation_prompt_queue.py
+++ prompt_job_generator/prompt_generation_prompt_queue.py
@@ -161,7 +161,7 @@ class PromptGenerationPromptQueue:
             prompts = prompt_list
 
         elif generation_policy == 'independent-approx-v1-top-k':
-            boltzman_temperature = random.randint(16, 32)
+            boltzman_temperature = random.randint(3, 8)
             boltzman_k = 1
 
             prompt_generator = self.get_independent_approx_v1_generator(dataset)
