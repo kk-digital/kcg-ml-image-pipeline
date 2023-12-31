dda75a541f95905e6edc2ef28ad9249dc8c7d699
ssss <sdevdariani9@gmail.com>
2023-12-31 12:38:57 +0400

    implemented reposnse handler in aips

--- orchestration/api/api_active_learning.py
+++ orchestration/api/api_active_learning.py
@@ -5,7 +5,7 @@ import random
 import pymongo
 from utility.minio import cmd
 from orchestration.api.mongo_schemas import  ActiveLearningPolicy, ActiveLearningQueuePair
-from .api_utils import PrettyJSONResponse
+from .api_utils import PrettyJSONResponse, ApiResponseHandler
 import os
 from fastapi.responses import JSONResponse
 from pymongo.collection import Collection
@@ -310,70 +310,79 @@ def get_ranking_comparison(
 
 @router.post("/active-learning-queue/queue-pair/add", response_class=PrettyJSONResponse)
 def add_queue_pair(request: Request, queue_pair: ActiveLearningQueuePair):
-    # Validate and retrieve the active learning policy using the active_learning_policy_id
-    policy = request.app.active_learning_policies_collection.find_one(
-        {"active_learning_policy_id": queue_pair.active_learning_policy_id}
-    )
-    if not policy:
-        raise HTTPException(status_code=404, detail=f"Active learning policy with ID {queue_pair.active_learning_policy_id} not found")
+    response_handler = ApiResponseHandler("/active-learning-queue/queue-pair/add")  # Initialize ApiResponseHandler
 
-    # Function to extract job details
-    def extract_job_details(job_uuid, suffix):
-        job = request.app.completed_jobs_collection.find_one({"uuid": job_uuid})
-        if not job:
-            raise HTTPException(status_code=422, detail=f"Job {job_uuid} not found")
+    try:
+        # Validate and retrieve the active learning policy using the active_learning_policy_id
+        policy = request.app.active_learning_policies_collection.find_one(
+            {"active_learning_policy_id": queue_pair.active_learning_policy_id}
+        )
+        if not policy:
+            return response_handler.create_error_response(
+                404, f"Active learning policy with ID {queue_pair.active_learning_policy_id} not found")
 
-        output_file_path = job["task_output_file_dict"]["output_file_path"]
-        task_creation_time = job["task_creation_time"]
-        path_parts = output_file_path.split('/')
-        if len(path_parts) < 4:
-            raise HTTPException(status_code=500, detail="Invalid output file path format")
+        def extract_job_details(job_uuid, suffix):
+            job = request.app.completed_jobs_collection.find_one({"uuid": job_uuid})
+            if not job:
+                raise HTTPException(status_code=422, detail=f"Job {job_uuid} not found")
 
-        return {
-            f"job_uuid_{suffix}": job_uuid,
-            f"file_name_{suffix}": path_parts[-1],
-            f"image_path_{suffix}": output_file_path,
-            f"image_hash_{suffix}": job["task_output_file_dict"]["output_file_hash"],
-            f"job_creation_time_{suffix}": task_creation_time,
+            output_file_path = job["task_output_file_dict"]["output_file_path"]
+            task_creation_time = job["task_creation_time"]
+            path_parts = output_file_path.split('/')
+            if len(path_parts) < 4:
+                raise HTTPException(status_code=500, detail="Invalid output file path format")
+
+            return {
+                f"job_uuid_{suffix}": job_uuid,
+                f"file_name_{suffix}": path_parts[-1],
+                f"image_path_{suffix}": output_file_path,
+                f"image_hash_{suffix}": job["task_output_file_dict"]["output_file_hash"],
+                f"job_creation_time_{suffix}": task_creation_time,
+            }
+        
+        # Extract job details for both jobs
+        job_details_1 = extract_job_details(queue_pair.image1_job_uuid, "1")
+        job_details_2 = extract_job_details(queue_pair.image2_job_uuid, "2")
+
+            # Prepare the document to insert into the active learning queue pairs collection
+        combined_job_details = {
+            "active_learning_policy_id": queue_pair.active_learning_policy_id,
+            "active_learning_policy": policy["active_learning_policy"],
+            "dataset": job_details_1['image_path_1'].split('/')[1],
+            "metadata": queue_pair.metadata,
+            "generator_string": queue_pair.generator_string,
+            "creation_date": datetime.utcnow().isoformat(),
+            "images": [
+                {
+                    "job_uuid_1": job_details_1["job_uuid_1"],
+                    "file_name_1": job_details_1["file_name_1"],
+                    "image_path_1": job_details_1["image_path_1"],
+                    "image_hash_1": job_details_1["image_hash_1"],
+                    "job_creation_time_1": job_details_1["job_creation_time_1"],
+                },
+                {
+                    "job_uuid_2": job_details_2["job_uuid_2"],
+                    "file_name_2": job_details_2["file_name_2"],
+                    "image_path_2": job_details_2["image_path_2"],
+                    "image_hash_2": job_details_2["image_hash_2"],
+                    "job_creation_time_2": job_details_2["job_creation_time_2"],
+                }
+            ]
         }
 
-    # Extract job details for both jobs
-    job_details_1 = extract_job_details(queue_pair.image1_job_uuid, "1")
-    job_details_2 = extract_job_details(queue_pair.image2_job_uuid, "2")
+        # Insert the combined job details into MongoDB collection
+        insert_result = request.app.active_learning_queue_pairs_collection.insert_one(combined_job_details)
 
-    # Prepare the document to insert into the active learning queue pairs collection
-    combined_job_details = {
-        "active_learning_policy_id": queue_pair.active_learning_policy_id,
-        "active_learning_policy": policy["active_learning_policy"],  # Retrieved from the policies collection
-        "dataset": job_details_1['image_path_1'].split('/')[1],
-        "metadata": queue_pair.metadata,
-        "generator_string": queue_pair.generator_string,
-        "creation_date": datetime.utcnow().isoformat(),
-        "images": [
-            {
-                "job_uuid_1": job_details_1["job_uuid_1"],
-                "file_name_1": job_details_1["file_name_1"],
-                "image_path_1": job_details_1["image_path_1"],
-                "image_hash_1": job_details_1["image_hash_1"],
-                "job_creation_time_1": job_details_1["job_creation_time_1"],
-            },
-            {
-                "job_uuid_2": job_details_2["job_uuid_2"],
-                "file_name_2": job_details_2["file_name_2"],
-                "image_path_2": job_details_2["image_path_2"],
-                "image_hash_2": job_details_2["image_hash_2"],
-                "job_creation_time_2": job_details_2["job_creation_time_2"],
-            }
-        ]
-    }
+        # Convert ObjectId to string for JSON serialization
+        combined_job_details['_id'] = str(insert_result.inserted_id)
 
-    # Insert the combined job details into MongoDB collection
-    insert_result = request.app.active_learning_queue_pairs_collection.insert_one(combined_job_details)
+        return response_handler.create_success_response(combined_job_details)
 
-    # Convert ObjectId to string for JSON serialization
-    combined_job_details['_id'] = str(insert_result.inserted_id)
-
-    return combined_job_details
+    except HTTPException as http_ex:
+        return response_handler.create_error_response(http_ex.status_code, http_ex.detail)
+    except Exception as e:
+        return response_handler.create_error_response(
+            500, str(e))
 
 @router.get("/active-learning-queue/queue-pair/list", response_class=PrettyJSONResponse)
 def list_queue_pairs(request: Request, dataset: Optional[str] = None, limit: int = 10, offset: int = 0):
@@ -395,27 +404,34 @@ def list_queue_pairs(request: Request, dataset: Optional[str] = None, limit: int
 
 @router.get("/active-learning-queue/queue-pair/random", response_class=PrettyJSONResponse)
 def random_queue_pair(request: Request, size: int = 1, dataset: Optional[str] = None):
-    # Define the aggregation pipeline
-    pipeline = []
+    response_handler = ApiResponseHandler("/active-learning-queue/queue-pair/random")  # Initialize ApiResponseHandler
 
-    # If a dataset is provided, add a match stage to the pipeline
-    if dataset:
-        pipeline.append({"$match": {"dataset": dataset}})
+    try:
+        pipeline = []
 
-    # Add the random sampling stage to the pipeline
-    pipeline.append({"$sample": {"size": size}})
+        # If a dataset is provided, add a match stage to the pipeline
+        if dataset:
+            pipeline.append({"$match": {"dataset": dataset}})
 
-    # Use MongoDB's aggregation framework to randomly select documents
-    random_pairs_cursor = request.app.active_learning_queue_pairs_collection.aggregate(pipeline)
+        # Add the random sampling stage to the pipeline    
+        pipeline.append({"$sample": {"size": size}})
 
-    # Convert the cursor to a list of dictionaries
-    random_pairs = []
-    for pair in random_pairs_cursor:
-        # Convert _id ObjectId to string
-        pair['_id'] = str(pair['_id'])
-        random_pairs.append(pair)
+        # Use MongoDB's aggregation framework to randomly select documents
+        random_pairs_cursor = request.app.active_learning_queue_pairs_collection.aggregate(pipeline)
 
-    return {"random_pairs": random_pairs}
+        # Convert the cursor to a list of dictionaries
+        random_pairs = []
+        for pair in random_pairs_cursor:
+            # Convert _id ObjectId to string
+            pair['_id'] = str(pair['_id'])
+            random_pairs.append(pair)
+
+        return response_handler.create_success_response({"random_pairs": random_pairs})
+
+    except Exception as e:
+        # Log the error here, if needed
+        return response_handler.create_error_response(
+            500, str(e))
 
 
 @router.delete("/active-learning-queue/queue-pair/delete")
