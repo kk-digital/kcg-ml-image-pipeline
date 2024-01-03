95ff3e239637f62833e3baf3c10340d4539d08f3
rachid2198 <rachid9821@gmail.com>
2024-01-03 21:10:30 +0100

    adding a test print

--- scripts/prompt_mutator/greedy-substitution-search-v1.py
+++ scripts/prompt_mutator/greedy-substitution-search-v1.py
@@ -360,6 +360,7 @@ class PromptSubstitutionGenerator:
     # get linear or elm positive score of an embedding
     def get_positive_score(self, embedding):
         embedding= torch.from_numpy(embedding).to(self.device)
+        print(embedding.shape)
 
         with torch.no_grad():
             prompt_score=self.positive_scorer.predict_positive_or_negative_only_pooled(embedding)
@@ -371,6 +372,8 @@ class PromptSubstitutionGenerator:
         # convert embeddings to tensors
         positive_embedding= torch.from_numpy(positive_embedding).to(self.device)
         negative_embedding= torch.from_numpy(negative_embedding).to(self.device)
+
+        print(positive_embedding.shape)
 
         with torch.no_grad():
             prompt_score=self.positive_scorer.predict_pooled_embeddings(positive_embedding, negative_embedding)
