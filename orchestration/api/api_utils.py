8ea4fc6900848262ec8e2bf2b70fe4c3548721b9
ssss <sdevdariani9@gmail.com>
2024-01-07 16:56:09 +0400

    update apiresponsehandler

--- orchestration/api/api_utils.py
+++ orchestration/api/api_utils.py
@@ -33,7 +33,7 @@ class ApiResponseHandler:
     def _elapsed_time(self) -> float:
         return time.time() - self.start_time
 
-    def create_success_response(self, response_data: dict, http_status_code: int, headers: dict = None):
+    def create_success_response(self, response_data: dict, http_status_code: int, headers: dict = {"Cache-Control": "no-store"}):
         # Validate the provided HTTP status code
         if not 200 <= http_status_code < 300:
             raise ValueError("Invalid HTTP status code for a success response. Must be between 200 and 299.")
@@ -43,11 +43,8 @@ class ApiResponseHandler:
             "duration": self._elapsed_time(),
             "response": response_data
         }
-        if headers:
-            return PrettyJSONResponse(status_code=http_status_code, content=response_content, headers=headers)
-        else:
-            return PrettyJSONResponse(status_code=http_status_code, content=response_content)
-
+        return PrettyJSONResponse(status_code=http_status_code, content=response_content, headers=headers)
+    
     def create_success_delete_response(self, reachable: bool):
         return PrettyJSONResponse(
             status_code=200,
