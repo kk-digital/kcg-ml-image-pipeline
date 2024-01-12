3074ea1af98eb90a30a407c19e6a38150c2ae777
rachid2198 <rachid9821@gmail.com>
2024-01-12 21:35:53 +0100

    fixing a bug

--- scripts/prompt_mutator/Iterative_painting.py
+++ scripts/prompt_mutator/Iterative_painting.py
@@ -68,7 +68,7 @@ class IterativePainter:
         new_cx1, new_cy1, new_cx2, new_cy2 = new_center
         for center in self.painted_centers:
             cx1, cy1, cx2, cy2 = center
-            if not (new_cx2 < cx1 or new_cy2 < cy1 or new_cx1 > cx2 or new_cy1 > cy2):
+            if not (new_cx1== cx1 and new_cx2==cx2):
                 return True
         return False
 
@@ -118,7 +118,7 @@ class IterativePainter:
         # Generate the image
         output_file_path, output_file_hash, img_byte_arr, seed, subseed = img2img(
             prompt=generated_prompt, negative_prompt='', sampler_name="ddim", batch_size=1, n_iter=1, 
-            steps=self.steps, cfg_scale=7.0, width=128, height=128, mask_blur=0, inpainting_fill=0, 
+            steps=self.steps, cfg_scale=7.0, width=512, height=512, mask_blur=0, inpainting_fill=0, 
             outpath='output', styles=None, init_images=init_images, mask=mask, resize_mode=0, 
             denoising_strength=0.75, image_cfg_scale=None, inpaint_full_res_padding=0, inpainting_mask_invert=0,
             sd=self.sd, clip_text_embedder=self.embedder, model=self.model, device=self.device)
