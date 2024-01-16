da23b3db6db13446b3e63358b8903a79f2572caf
rachid2198 <rachid9821@gmail.com>
2024-01-16 14:54:07 +0100

    testing

--- scripts/prompt_mutator/greedy_substitution_search_v1.py
+++ scripts/prompt_mutator/greedy_substitution_search_v1.py
@@ -86,34 +86,6 @@ class PromptData:
         self.positive_phrase_embeddings= positive_phrase_embeddings
         self.positive_phrase_token_lengths= positive_phrase_token_lengths
 
-    def get_phrases(self):
-        # split prompt phrases by comma
-        phrases = self.positive_prompt.split(', ')
-
-        # merge quoted phrases that have commas within them that were split
-        merged_phrases = []
-        merge_next = False
-
-        for phrase in phrases:
-            if phrase.startswith('"') and not phrase.endswith('"'):
-                # Start of a quoted phrase
-                merge_next = True
-                merged_phrases.append(phrase)
-            elif merge_next and not phrase.endswith('"'):
-                # Middle of a quoted phrase
-                merged_phrases[-1] += ', ' + phrase
-            elif merge_next and phrase.endswith('"'):
-                # End of a quoted phrase
-                merged_phrases[-1] += ', ' + phrase
-                print(merged_phrases[-1])
-                merge_next = False
-            else:
-                # Regular, non-quoted phrase
-                merged_phrases.append(phrase)
-
-        # Merge phrases that were incorrectly split
-        return merged_phrases
-
 class PromptSubstitutionGenerator:
     def __init__(
         self,
@@ -272,7 +244,7 @@ class PromptSubstitutionGenerator:
             # get cumulative probabilities
             self.positive_phrase_origin_indexes, self.positive_cumulative_probability_arr = get_cumulative_probability_arr_without_upload(
             index_phrase_score_data=self.phrase_score_data,
-            boltzman_temperature=boltzman_temperature,
+            boltzman_temperature=16,
             boltzman_k=boltzman_k)
 
             # get min and max
@@ -573,7 +545,7 @@ class PromptSubstitutionGenerator:
                 # get the substituted phrase embedding
                 substituted_embedding = prompt.positive_phrase_embeddings[phrase_position]
                 # get a random phrase from civitai to substitute with
-                phrase_index, random_phrase= self.choose_random_phrase(max_token_length=substituted_phrase_length)   
+                phrase_index, random_phrase= self.selection_policy(max_token_length=substituted_phrase_length)   
                 # get phrase string
                 substitute_phrase = random_phrase
                 # get phrase embedding by its index
@@ -649,7 +621,7 @@ class PromptSubstitutionGenerator:
                     predicted_score=substitution['score']
 
                     #Create a modified prompt with the substitution
-                    prompt_list = prompts[index].get_phrases()
+                    prompt_list = prompts[index].positive_prompt.split()
                     prompt_list[position] = substitute_phrase
                     modified_prompt_str = ", ".join(prompt_list)
 
@@ -858,7 +830,7 @@ class PromptSubstitutionGenerator:
                     substitute_embedding=substitution['substitute_embedding']
 
                     #Create a modified prompt with the substitution
-                    prompt_list = prompts[index].get_phrases()
+                    prompt_list = prompts[index].positive_prompt.split()
                     prompt_list[position] = substitute_phrase
                     modified_prompt_str = ", ".join(prompt_list)
 
@@ -1091,7 +1063,7 @@ class PromptSubstitutionGenerator:
         # Calculate phrase embeddings and token lengths for chosen prompts
         print("Calculating phrase embeddings and token lengths for each phrase in each prompt")
         for prompt in tqdm(chosen_scored_prompts):
-            phrases = prompt.get_phrases()
+            phrases = prompt.positive_prompt.split()
             prompt.positive_phrase_embeddings = [self.load_phrase_embedding(phrase) for phrase in phrases]
             prompt.positive_phrase_token_lengths = [self.load_phrase_token_length(phrase) for phrase in phrases] 
 
@@ -1178,7 +1150,7 @@ class PromptSubstitutionGenerator:
         # Calculate phrase embeddings and token lengths for chosen prompts
         print("Calculating phrase embeddings and token lengths for each phrase in each prompt")
         for prompt in tqdm(chosen_scored_prompts):
-            phrases = prompt.get_phrases()
+            phrases = prompt.positive_prompt.split()
             prompt.positive_phrase_embeddings = [self.load_phrase_embedding(phrase) for phrase in phrases]
             prompt.positive_phrase_token_lengths = [self.load_phrase_token_length(phrase) for phrase in phrases] 
 
