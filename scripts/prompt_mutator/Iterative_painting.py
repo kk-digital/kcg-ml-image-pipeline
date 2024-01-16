f1ca0b1ccd94f045d7ad704c87405938dd8c17a8
rachid2198 <rachid9821@gmail.com>
2024-01-16 20:33:46 +0100

    experimenting with different parameters

--- scripts/prompt_mutator/Iterative_painting.py
+++ scripts/prompt_mutator/Iterative_painting.py
@@ -233,8 +233,7 @@ class IterativePainter:
         return prompt_str
     
     def generate_image(self, context_image, generated_prompt):
-        # Draw a blank rectangle over the center area
-        # Use the background color
+        # Use the context image as an initial image
         init_images = [context_image]
 
         # Create mask
@@ -245,9 +244,9 @@ class IterativePainter:
         # Generate the image
         output_file_path, output_file_hash, img_byte_arr, seed, subseed = img2img(
             prompt=generated_prompt, negative_prompt='', sampler_name="ddim", batch_size=1, n_iter=1, 
-            steps=20, cfg_scale=1.0, width=self.context_size, height=self.context_size, mask_blur=0, inpainting_fill=0, 
+            steps=20, cfg_scale=7.0, width=self.paint_size, height=self.paint_size, mask_blur=0, inpainting_fill=0, 
             outpath='output', styles=None, init_images=init_images, mask=mask, resize_mode=0, 
-            denoising_strength=0, image_cfg_scale=None, inpaint_full_res_padding=0, inpainting_mask_invert=0,
+            denoising_strength=0.75, image_cfg_scale=None, inpaint_full_res_padding=0, inpainting_mask_invert=0,
             sd=self.sd, clip_text_embedder=self.text_embedder, model=self.model, device=self.device)
         
         img_byte_arr.seek(0)
