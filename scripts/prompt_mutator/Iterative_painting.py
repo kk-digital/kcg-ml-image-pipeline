bf9e491732fa6a0b9004a5a8f0bb61ea886dc38c
rachid2198 <rachid9821@gmail.com>
2024-01-17 11:30:14 +0100

    experimenting with different parameters

--- scripts/prompt_mutator/Iterative_painting.py
+++ scripts/prompt_mutator/Iterative_painting.py
@@ -244,7 +244,7 @@ class IterativePainter:
         # Generate the image
         output_file_path, output_file_hash, img_byte_arr, seed, subseed = img2img(
             prompt=generated_prompt, negative_prompt="", sampler_name="ddim", batch_size=1, n_iter=1, 
-            steps=20, cfg_scale=7.0, width=self.context_size, height=self.context_size, mask_blur=0, inpainting_fill=2, 
+            steps=20, cfg_scale=7.0, width=self.context_size, height=self.context_size, mask_blur=4.0, inpainting_fill=1, 
             outpath='output', styles=None, init_images=init_images, mask=mask, resize_mode=0, 
             denoising_strength=0.75, image_cfg_scale=None, inpaint_full_res_padding=0, inpainting_mask_invert=0,
             sd=self.sd, clip_text_embedder=self.text_embedder, model=self.model, device=self.device)
@@ -256,7 +256,7 @@ class IterativePainter:
         return generated_image
 
     def test(self):
-        prompt= self.generate_prompt()
+        prompt= "side scrolling concept art, surviving in space, level design, steampunk, castle, warhammer , grin, eerie forest, jungle, power lines, cannons, a harbor, tree top, lab equipment, in dungeon, deep rainforest"
 
         white_background= [Image.new("RGB", (512, 512), "white")]
         mask= Image.new("L", (512, 512), 255)
