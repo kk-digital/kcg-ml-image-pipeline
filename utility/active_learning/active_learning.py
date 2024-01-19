dfbbac215cbd52d61b87349108161314dcc89a34
rachid2198 <rachid9821@gmail.com>
2024-01-19 16:57:24 +0100

    updating policy names

--- utility/active_learning/active_learning.py
+++ utility/active_learning/active_learning.py
@@ -261,7 +261,7 @@ class ActiveLearningPipeline:
                 if pair not in merged_list:
                     merged_list.append({
                         "pair": pair,
-                        "policy": f"same_embedding_cluster_48"
+                        "policy": f"same_embedding_cluster_100"
                     })
            
             # merge pairs by sigma score and by cluster
@@ -269,7 +269,7 @@ class ActiveLearningPipeline:
                 if pair not in merged_list:
                     merged_list.append({
                         "pair": pair,
-                        "policy": f"same_embedding_cluster_1024"
+                        "policy": f"same_embedding_cluster_100"
                     })
             
             # merge pairs by sigma score and by cluster
@@ -277,7 +277,7 @@ class ActiveLearningPipeline:
                 if pair not in merged_list:
                     merged_list.append({
                         "pair": pair,
-                        "policy": f"same_embedding_cluster_4096"
+                        "policy": f"same_embedding_cluster_100"
                     })
                 
         return merged_list
