cd56b7c46260ec0cd0f3c29cc6b066534d24d2cf
rachid2198 <rachid9821@gmail.com>
2024-01-22 11:10:29 +0100

    update

--- scripts/prompt_mutator/greedy_substitution_search_v1.py
+++ scripts/prompt_mutator/greedy_substitution_search_v1.py
@@ -260,15 +260,14 @@ class PromptSubstitutionGenerator:
         # get base prompt list
         base_prompts = load_base_prompts(self.csv_base_prompts)
         # create a dictionarry for base prompts
-        self.base_prompt_embeddings={phrase: self.compute_embeddings(phrase) for phrase in base_prompts}
+        self.base_prompt_embeddings={phrase: self.get_mean_pooled_embedding(self.get_prompt_embedding(phrase)) for phrase in base_prompts}
         self.base_prompt_token_lengths={phrase: self.get_token_length(phrase) for phrase in base_prompts}
 
         # load topic tags
         tag_info = json.load(open('input/tagging/environmental.json', 'rt'))
         subtags = sum([i['subtags'] for i in tag_info], start=[])
-        tag_embs= self.compute_embeddings(subtags)
-        #tag_embs = self.get_prompt_embedding(subtags)
-        #tag_embs= [self.get_mean_pooled_embedding(emb.unsqueeze(0)) for emb in tag_embs]
+        tag_embs = self.get_prompt_embedding(subtags)
+        tag_embs= [self.get_mean_pooled_embedding(emb.unsqueeze(0)) for emb in tag_embs]
 
         self.tagger= Tagger(tag_info, tag_embs)
         self.tag_list= self.tagger.tag_names
@@ -442,7 +441,7 @@ class PromptSubstitutionGenerator:
             prompts = [prompts]
 
         with torch.no_grad():
-            embeddings= self.embedder(prompts)
+            embeddings= self.embedder.forward_return_all(prompts)
         
         embeddings= embeddings.unsqueeze(1)
         embeddings=embeddings.to(self.device)
@@ -451,26 +450,6 @@ class PromptSubstitutionGenerator:
             return embeddings[0].unsqueeze(0)
         
         return embeddings
- 
-    def compute_embeddings(self, prompts):
-        # Tokenize the prompts
-        batch_encoding = self.embedder.tokenizer(prompts, truncation=False, max_length=MAX_LENGTH, return_length=True,
-                                        return_overflowing_tokens=False, padding="max_length", return_tensors="pt")
-
-        # Get token ids and move to device
-        tokens = batch_encoding["input_ids"].to(self.device)
-
-        self.transformer = self.embedder.transformer.to(self.device)
-
-        # Get CLIP embeddings
-        clip_output = self.embedder.transformer(input_ids=tokens)
-
-        if not isinstance(prompts, list):
-            pooler_output=clip_output.pooler_output.squeeze(0)
-        else:
-            pooler_output=clip_output.pooler_output
-
-        return pooler_output.detach().cpu().numpy()
 
     # get linear or elm positive score of an embedding
     def get_positive_score(self, embedding):
@@ -607,7 +586,7 @@ class PromptSubstitutionGenerator:
             substituted_embedding=prompts[prompt_index].positive_phrase_embeddings[phrase_position]
             substitute_phrase= sampled_phrases[index]
             substitute_embedding= sampled_embeddings[index]
-            topic= prompts[prompt_index].topic
+            # topic= prompts[prompt_index].topic
 
             # only take substitutions that increase score by more then a set threshold
             # topic_similarity= self.tagger.get_tag_similarity(topic, substitute_embedding)
@@ -666,9 +645,8 @@ class PromptSubstitutionGenerator:
                     modified_prompt_str = ", ".join(prompt_list)
 
                     #calculate modified prompt embedding and sigma score
-                    # modified_prompt_embedding=self.get_prompt_embedding(modified_prompt_str)
-                    # modified_prompt_embedding=self.get_mean_pooled_embedding(modified_prompt_embedding)
-                    modified_prompt_embedding= self.compute_embeddings(modified_prompt_str)
+                    modified_prompt_embedding=self.get_prompt_embedding(modified_prompt_str)
+                    modified_prompt_embedding=self.get_mean_pooled_embedding(modified_prompt_embedding)
                     modified_prompt_score= self.get_positive_score(modified_prompt_embedding)
                     modified_prompt_score= (modified_prompt_score - self.positive_mean) / self.positive_std
 
@@ -1155,8 +1133,8 @@ class PromptSubstitutionGenerator:
 
             # Get embeddings for the batch
             start=time.time()
-            positive_embeddings= self.compute_embeddings(valid_positive_prompts)
-            negative_embeddings= self.compute_embeddings(valid_negative_prompts)
+            positive_embeddings = self.get_prompt_embedding(valid_positive_prompts)
+            negative_embeddings = self.get_prompt_embedding(valid_negative_prompts)
             end= time.time()
 
             clip_time+= end-start
@@ -1164,11 +1142,9 @@ class PromptSubstitutionGenerator:
             # Normalize scores and calculate mean pooled embeddings for the batch
             for i, index in enumerate(valid_positive_indices):
                 # Mean pooling and other processing
-                # positive_embedding = self.get_mean_pooled_embedding(positive_embeddings[i].unsqueeze(0))
-                # negative_embedding = self.get_mean_pooled_embedding(negative_embeddings[i].unsqueeze(0))
-                positive_embedding= positive_embeddings[i]
-                negative_embedding= negative_embeddings[i]
-
+                positive_embedding = self.get_mean_pooled_embedding(positive_embeddings[i].unsqueeze(0))
+                negative_embedding = self.get_mean_pooled_embedding(negative_embeddings[i].unsqueeze(0))
+                
                 # calculate positive score and variance score
                 positive_score=self.get_positive_score(positive_embedding)
                 positive_score = (positive_score - self.positive_mean) / self.positive_std
