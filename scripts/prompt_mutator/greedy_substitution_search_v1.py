0b10c5fe6f5e144152f608da58819bd54213b3bf
rachid2198 <rachid9821@gmail.com>
2024-01-22 22:28:44 +0100

    testing different rejection policy

--- scripts/prompt_mutator/greedy_substitution_search_v1.py
+++ scripts/prompt_mutator/greedy_substitution_search_v1.py
@@ -592,7 +592,7 @@ class PromptSubstitutionGenerator:
             
             topic_similarity= self.tagger.get_tag_similarity(topic, substitute_embedding)
             topic_score= (topic_similarity +1) / 2
-            combined_score= (topic_score * 10) + sigma_score
+            combined_score= topic_score + sigma_score
 
             if combined_score > (prompts[prompt_index].variance_score + self.sigma_threshold):            
                 substitution_data={
@@ -660,7 +660,7 @@ class PromptSubstitutionGenerator:
                     # get topic score
                     topic_similarity= self.tagger.get_tag_similarity(prompts[index].topic, modified_prompt_embedding)
                     topic_score= (topic_similarity +1) / 2
-                    combined_score= (10 * topic_score) + modified_prompt_score
+                    combined_score= topic_score + modified_prompt_score
 
                     if(self.self_training):
                         # collect self training data
