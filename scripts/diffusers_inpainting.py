300a82de97bc3fd5fd68f66e8fa9fced6ff0b196
hanruijiang <hanruijiang@mail.nwpu.edu.cn>
2024-01-24 05:58:49 +0000

    use safetensors for diffusers inpainting

--- scripts/diffusers_inpainting.py
+++ scripts/diffusers_inpainting.py
@@ -12,10 +12,12 @@ parser.add_argument('--device_id', type=int, default=0, help='Device ID for CUDA
 parser.add_argument('--num_inference_steps', type=int, default=40, help='Number of inference steps.')
 parser.add_argument('--strength', type=float, default=0.75, help='Strength of the inpainting.')
 parser.add_argument('--guidance_scale', type=float, default=7.5, help='Guidance scale for the model.')
-parser.add_argument('--local_files_only', action='store_true', help='Use local files only.')
-parser.add_argument('--cache_dir', type=str, default=None, help='Cache directory for the model.')
-parser.add_argument('--model_id', type=str, default='runwayml/stable-diffusion-inpainting', help='Model ID for the pipeline.')
 parser.add_argument('--use_float32', action='store_true', help='Use float32 precision.')
+parser.add_argument('--model_file_path', type=str, required=True, help='Path to the model file.')
+parser.add_argument('--config_file_path', type=str, required=True, help='Path to the config file.')
+parser.add_argument('--tokenizer_path', type=str, required=True, help='Path to the tokenizer.')
+parser.add_argument('--text_model_path', type=str, required=True, help='Path to the text model.')
+
 
 # get args
 args = parser.parse_args()
@@ -24,6 +26,7 @@ args = parser.parse_args()
 os.environ['CUDA_VISIBLE_DEVICES'] = str(args.device_id)
 
 from diffusers import StableDiffusionInpaintPipeline
+from transformers import CLIPTokenizer, CLIPTextModel
 from PIL import Image
 import numpy as np
 import torch
@@ -32,13 +35,18 @@ import torch
 init_image = Image.open(args.image_path).convert("RGB").resize(args.target_size)
 mask = Image.open(args.mask_path).convert("RGB").resize(args.target_size)
 
+# Load tokenizer and text encoder
+tokenizer = CLIPTokenizer.from_pretrained(args.tokenizer_path, local_files_only=True)
+text_encoder = CLIPTextModel.from_pretrained(args.text_model_path, local_files_only=True).cuda().eval()
+
 # load model
 torch_dtype = torch.float32 if args.use_float32 else torch.float16
-pipe = StableDiffusionInpaintPipeline.from_pretrained(
-    args.model_id, 
-    torch_dtype=torch_dtype, 
-    local_files_only=args.local_files_only, 
-    cache_dir=args.cache_dir
+pipe = StableDiffusionInpaintPipeline.from_single_file(
+    pretrained_model_link_or_path=args.model_file_path,
+    config_files={'v1': args.config_file_path},
+    text_encoder=text_encoder,
+    tokenizer=tokenizer,
+    local_files_only=True, use_safetensors=True, load_safety_checker=False
 ).to('cuda')
 
 # do inpainting
