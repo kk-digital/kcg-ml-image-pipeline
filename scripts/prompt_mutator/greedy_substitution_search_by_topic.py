a02fb9f2e6d3612e50e65f082d71a73d097d0269
rachid2198 <rachid9821@gmail.com>
2024-01-28 16:35:36 +0100

    testing

--- scripts/prompt_mutator/greedy_substitution_search_by_topic.py
+++ scripts/prompt_mutator/greedy_substitution_search_by_topic.py
@@ -991,7 +991,7 @@ class PromptSubstitutionGenerator:
             phrases = prompt.positive_prompt.split(', ')
             prompt.positive_phrase_embeddings = [self.load_phrase_embedding(phrase) for phrase in phrases]
             prompt.positive_phrase_token_lengths = [self.load_phrase_token_length(phrase) for phrase in phrases] 
-            prompt.phrase_topic_scores = [self.tagger.get_tag_similarity(prompt.topic, embedding) for embedding in prompt.positive_phrase_embeddings] 
+            prompt.topic_phrase_scores = [self.tagger.get_tag_similarity(prompt.topic, embedding) for embedding in prompt.positive_phrase_embeddings] 
             prompt.topic_score= sum(prompt.phrase_topic_scores)
             prompt.combined_score= prompt.positive_score + prompt.topic_score
 
