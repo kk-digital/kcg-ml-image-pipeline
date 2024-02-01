06eaceae0b491b3aefea9418cb5d4771b7f8e4f7
rachid2198 <rachid9821@gmail.com>
2024-02-01 23:15:04 +0100

    implementing different tasks with kandinsky

--- worker/image_generation/scripts/inpaint_kandinsky.py
+++ worker/image_generation/scripts/inpaint_kandinsky.py
@@ -58,34 +58,52 @@ class KandinskyInpaintingPipeline:
         else:
             self.device = 'cpu'
 
-    def load_models(self, image_encoder_path, unet_path, prior_path, decoder_path, task_type="inpainting"):
+    def load_models(self, image_encoder_path, unet_path, inpaint_unet_path, prior_path, decoder_path, inpaint_decoder_path, task_type="inpainting"):
         with section("Loading Inpainting model"):
             self.image_encoder = CLIPVisionModelWithProjection.from_pretrained(image_encoder_path, 
                                                                                 local_files_only=True,
                                                                                 use_safetensors=True).to(torch.float16).to(self.device)
             
-            self.unet = UNet2DConditionModel.from_pretrained(unet_path, local_files_only=True,
+            if task_type=="inpainting":
+                self.unet = UNet2DConditionModel.from_pretrained(inpaint_unet_path, local_files_only=True,
                                                                         use_safetensors=True).to(torch.float16).to(self.device)
-            
-            self.prior = KandinskyV22PriorPipeline.from_pretrained(prior_path, 
+                
+                self.prior = KandinskyV22PriorPipeline.from_pretrained(prior_path, 
                                                                    local_files_only=True,
                                                                    use_safetensors=True, 
                                                                    image_encoder=self.image_encoder, 
                                                                    torch_dtype=torch.float16).to(self.device)
-            
-            if task_type=="inpainting":
-                self.decoder = KandinskyV22InpaintPipeline.from_pretrained(decoder_path, 
+                
+                self.decoder = KandinskyV22InpaintPipeline.from_pretrained(inpaint_decoder_path, 
                                                                    local_files_only=True,
                                                                    use_safetensors=True, 
                                                                    unet=self.unet, 
                                                                    torch_dtype=torch.float16).to(self.device)
             elif task_type=="img2img":
+                self.unet = UNet2DConditionModel.from_pretrained(unet_path, local_files_only=True,
+                                                                        use_safetensors=True).to(torch.float16).to(self.device)
+                
+                self.prior = KandinskyV22PriorPipeline.from_pretrained(prior_path, 
+                                                                   local_files_only=True,
+                                                                   use_safetensors=True, 
+                                                                   image_encoder=self.image_encoder, 
+                                                                   torch_dtype=torch.float16).to(self.device)
+                
                 self.decoder = KandinskyV22Img2ImgPipeline.from_pretrained(decoder_path, 
                                                                    local_files_only=True,
                                                                    use_safetensors=True, 
                                                                    unet=self.unet, 
                                                                    torch_dtype=torch.float16).to(self.device)
             elif task_type=="text2img":
+                self.unet = UNet2DConditionModel.from_pretrained(unet_path, local_files_only=True,
+                                                                        use_safetensors=True).to(torch.float16).to(self.device)
+                
+                self.prior = KandinskyV22PriorPipeline.from_pretrained(prior_path, 
+                                                                   local_files_only=True,
+                                                                   use_safetensors=True, 
+                                                                   image_encoder=self.image_encoder, 
+                                                                   torch_dtype=torch.float16).to(self.device)
+                
                 self.decoder = KandinskyV22Pipeline.from_pretrained(decoder_path, 
                                                                    local_files_only=True,
                                                                    use_safetensors=True, 
