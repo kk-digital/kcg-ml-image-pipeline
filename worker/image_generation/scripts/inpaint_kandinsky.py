350c87bfd7368aa7dda969b4d7ff1c17cbec4e14
rachid2198 <rachid9821@gmail.com>
2024-02-01 22:51:12 +0100

    implementing different tasks with kandinsky

--- worker/image_generation/scripts/inpaint_kandinsky.py
+++ worker/image_generation/scripts/inpaint_kandinsky.py
@@ -3,7 +3,7 @@ import io
 import os
 import sys
 import torch
-from diffusers import KandinskyV22PriorPipeline, KandinskyV22InpaintPipeline
+from diffusers import KandinskyV22PriorPipeline, KandinskyV22InpaintPipeline, KandinskyV22Img2ImgPipeline, KandinskyV22Pipeline
 from transformers import CLIPVisionModelWithProjection
 from diffusers.models import UNet2DConditionModel
 from transformers import CLIPVisionModelWithProjection
@@ -21,29 +21,36 @@ from utility.minio import cmd
 
 class KandinskyInpaintingPipeline:
     def __init__(self,
-                 denoising_strength=0.75,
-                 guidance_scale=7.5,
-                 steps=150,
                  width=512,
-                 height=512
-                 ):
+                 height=512,
+                 batch_size=1,
+                 decoder_steps=50,
+                 prior_steps=25,
+                 strength=0.4,
+                 decoder_guidance_scale=4,
+                 prior_guidance_scale=4,
+                 negative_prior_prompt="",
+                 negative_decoder_prompt=""
+                ):
         """
-        denoising_strength (float): The strength of denoising applied during inpainting. A higher value enhances denoising effects.
-        guidance_scale (float): The scale of guidance for inpainting. It controls the impact of guidance on the inpainting process.
         steps (int): The number of optimization steps to perform during inpainting. More steps may lead to finer inpainting results.
         width (int): The width of the inpainting canvas. Specifies the horizontal dimension of the image to be inpainted.
         height (int): The height of the inpainting canvas. Specifies the vertical dimension of the image to be inpainted.
         """
 
         # set parameters
-        self.denoising_strength=denoising_strength
-        self.guidance_scale=guidance_scale
-        self.steps=steps
+        self.batch_size= batch_size
+        self.prior_guidance_scale=prior_guidance_scale
+        self.decoder_guidance_scale=decoder_guidance_scale
+        self.strength=strength
+        self.negative_prior_prompt=negative_prior_prompt
+        self.negative_decoder_prompt=negative_decoder_prompt
+        self.prior_steps= prior_steps
+        self.decoder_steps=decoder_steps
         self.width=width
         self.height=height
 
         self.inpainting_model=None
-
 
         # get device
         if torch.cuda.is_available():
@@ -51,7 +58,7 @@ class KandinskyInpaintingPipeline:
         else:
             self.device = 'cpu'
 
-    def load_models(self, image_encoder_path, unet_path, prior_path, decoder_path):
+    def load_models(self, image_encoder_path, unet_path, prior_path, decoder_path, task_type="inpainting"):
         with section("Loading Inpainting model"):
             self.image_encoder = CLIPVisionModelWithProjection.from_pretrained(image_encoder_path, 
                                                                                 local_files_only=True,
@@ -66,47 +73,101 @@ class KandinskyInpaintingPipeline:
                                                                    image_encoder=self.image_encoder, 
                                                                    torch_dtype=torch.float16).to(self.device)
             
-            self.decoder = KandinskyV22InpaintPipeline.from_pretrained(decoder_path, 
+            if task_type=="inpainting":
+                self.decoder = KandinskyV22InpaintPipeline.from_pretrained(decoder_path, 
                                                                    local_files_only=True,
                                                                    use_safetensors=True, 
                                                                    unet=self.unet, 
                                                                    torch_dtype=torch.float16).to(self.device)
+            elif task_type=="img2img":
+                self.decoder = KandinskyV22Img2ImgPipeline.from_pretrained(decoder_path, 
+                                                                   local_files_only=True,
+                                                                   use_safetensors=True, 
+                                                                   unet=self.unet, 
+                                                                   torch_dtype=torch.float16).to(self.device)
+            elif task_type=="text2img":
+                self.decoder = KandinskyV22Pipeline.from_pretrained(decoder_path, 
+                                                                   local_files_only=True,
+                                                                   use_safetensors=True, 
+                                                                   unet=self.unet, 
+                                                                   torch_dtype=torch.float16).to(self.device)
+            
             logger.debug(f"Kandinsky Inpainting model successfully loaded")
    
     def unload_models(self):
         pass
 
+    def get_new_h_w(self, h, w):
+        new_h = h // 64
+        if h % 64 != 0:
+            new_h += 1
+        new_w = w // 64
+        if w % 64 != 0:
+            new_w += 1
+        return new_h * 64, new_w * 64
+
     def generate_inpainting(
         self,
         prompt,
         initial_img,
-        img_mask,
-        batch_size=1,
-        decoder_steps=50,
-        prior_steps=25,
-        decoder_guidance_scale=4,
-        prior_guidance_scale=4,
-        h=512,
-        w=512,
-        negative_prior_prompt="",
-        negative_decoder_prompt="",
+        img_mask
     ):
         
-        img_emb = self.prior(prompt=prompt, num_inference_steps=prior_steps,
-                        num_images_per_prompt=batch_size, guidance_scale=prior_guidance_scale,
-                        negative_prompt=negative_prior_prompt)
-        negative_emb = self.prior(prompt=negative_prior_prompt, num_inference_steps=prior_steps,
-                             num_images_per_prompt=batch_size, guidance_scale=prior_guidance_scale)
-        if negative_decoder_prompt == "":
+        img_emb = self.prior(prompt=prompt, num_inference_steps=self.prior_steps,
+                        num_images_per_prompt=self.batch_size, guidance_scale=self.prior_guidance_scale,
+                        negative_prompt=self.negative_prior_prompt)
+        negative_emb = self.prior(prompt=self.negative_prior_prompt, num_inference_steps=self.prior_steps,
+                             num_images_per_prompt=self.batch_size, guidance_scale=self.prior_guidance_scale)
+        if self.negative_decoder_prompt == "":
             negative_emb = negative_emb.negative_image_embeds
         else:
             negative_emb = negative_emb.image_embeds
         images = self.decoder(image_embeds=img_emb.image_embeds, negative_image_embeds=negative_emb,
-                         num_inference_steps=decoder_steps, height=h,
-                         width=w, guidance_scale=decoder_guidance_scale,
+                         num_inference_steps=self.decoder_steps, height=self.height,
+                         width=self.width, guidance_scale=self.decoder_guidance_scale,
                          image=initial_img, mask_image=img_mask).images
         return images[0]
+    
+    def generate_text2img(
+        self,
+        prompt
+    ):
+        h, w = self.get_new_h_w(h, w)
+        img_emb = self.prior(prompt=prompt, num_inference_steps=self.prior_steps,
+                        num_images_per_prompt=self.batch_size, guidance_scale=self.prior_guidance_scale,
+                        negative_prompt=self.negative_prior_prompt)
+        negative_emb = self.prior(prompt=self.negative_decoder_prompt, num_inference_steps=self.prior_steps,
+                             num_images_per_prompt=self.batch_size, guidance_scale=self.prior_guidance_scale)
+        if self.negative_decoder_prompt == "":
+            negative_emb = negative_emb.negative_image_embeds
+        else:
+            negative_emb = negative_emb.image_embeds
+        images = self.decoder(image_embeds=img_emb.image_embeds, negative_image_embeds=negative_emb,
+                         num_inference_steps=self.decoder_steps, height=h,
+                         width=w, guidance_scale=self.decoder_guidance_scale).images
+        return images
 
+    def generate_img2img(
+        self,
+        prompt,
+        image
+    ):
+        h, w = self.get_new_h_w(h, w)
+        img_emb = self.prior(prompt=prompt, num_inference_steps=self.prior_steps,
+                        num_images_per_prompt=self.batch_size, guidance_scale=self.prior_guidance_scale,
+                        negative_prompt=self.negative_prior_prompt)
+        negative_emb = self.prior(prompt=self.negative_prior_prompt, num_inference_steps=self.prior_steps,
+                             num_images_per_prompt=self.batch_size, guidance_scale=self.prior_guidance_scale)
+        if self.negative_decoder_prompt == "":
+            negative_emb = negative_emb.negative_image_embeds
+        else:
+            negative_emb = negative_emb.image_embeds
+        images = self.decoder(image_embeds=img_emb.image_embeds, negative_image_embeds=negative_emb,
+                         num_inference_steps=self.decoder_steps, height=h,
+                         width=w, guidance_scale=self.decoder_guidance_scale,
+                             strength=self.strength, image=image).images
+        return images
+    
     def convert_image_to_png(self, image):
         # convert image to bytes arr
         img_byte_arr = io.BytesIO()
