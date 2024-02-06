763378ee3170c101691c52f6a649299dbce31e60
Amine-AH <md.amine.ah@gmail.com>
2024-02-06 21:14:15 +0100

    Refactoring trying diffirent architectures

--- scripts/ab_ranking_treeconnect_script.py
+++ scripts/ab_ranking_treeconnect_script.py
@@ -303,7 +303,7 @@ def train_ranking(dataset_name: str,
 
     # add model card
     model_id = score_residual.add_model_card(model_card)
-    model_type = "TC-small"
+    model_type = "TC-small-2"
 
     print("validation loss: ",validation_loss_per_epoch)
     print("training loss: ",training_loss_per_epoch)
