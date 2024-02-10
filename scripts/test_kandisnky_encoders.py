f3c1a5f4f7c6dbfc0c6bafa3226b2b413c45fa2b
rachid2198 <rachid9821@gmail.com>
2024-02-10 16:34:30 +0100

    testing script

---
+++ scripts/test_kandisnky_encoders.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+sys.path.insert(0, os.getcwd())
+from kandinsky.models.clip_text_encoder.clip_text_encoder import KandinskyCLIPTextEmbedder
+
+def main():
+    text_encoder=KandinskyCLIPTextEmbedder()
+
+    texts = [
+    'Three blind horses listening to Mozart.',
+    'Älgen är skogens konung!',
+    'Wie leben Eisbären in der Antarktis?',
+    'Вы знали, что все белые медведи левши?'
+    ]
+
+    prompt_embeds, text_encoder_hidden_states, text_mask= text_encoder.forward(texts)
+
+    print(f"prompt embedding: {prompt_embeds.shape}")
+    print(f"hidden state {text_encoder_hidden_states.shape}")
+    print(f"mask {text_mask.shape}")
