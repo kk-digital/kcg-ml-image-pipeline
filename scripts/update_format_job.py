b06df76b98673f1b641897f56060042ff59c1c4d
ssss <sdevdariani9@gmail.com>
2024-02-13 17:36:39 +0400

    add and update apis according new data format

--- scripts/update_format_job.py
+++ scripts/update_format_job.py
@@ -8,25 +8,31 @@ db = client['orchestration-job-db']
 # Access the specific collection
 collection = db["completed-jobs"]
 
-# Fetch documents that need updating
-documents = collection.find({})
+uuid_to_update = "796a0c9e-9ffd-4f89-aff0-d26e924f2ef3"
 
-for doc in documents:
+# Fetch the document that matches the specified UUID
+document = collection.find_one({"uuid": uuid_to_update})
+
+if document:
     update_fields = {}
-    prompt_generation_data = doc.get('prompt_generation_data', {})
+    prompt_generation_data = document.get('prompt_generation_data', {})
     
     # Handling top_k from task_input_dict
-    if 'top_k' in doc.get('task_input_dict', {}):  # Correctly access top_k within task_input_dict
-        top_k_value = doc['task_input_dict']['top_k']  # Retrieve the top_k value
-        prompt_generation_data['top-k'] = top_k_value  # Move it inside prompt_generation_data
-
+    if 'top_k' in document.get('task_input_dict', {}):
+        top_k_value = document['task_input_dict']['top_k']
+        prompt_generation_data['top-k'] = top_k_value
+        # Prepare to remove the original top_k field from task_input_dict
+        update_fields['$unset'] = {'task_input_dict.top_k': ""}
+    
     # Update prompt_generation_data with new values or structure
     update_fields['$set'] = {
         'prompt_generation_data': prompt_generation_data
     }
-
-    # Perform the update
-    collection.update_one({'_id': doc['_id']}, update_fields)
-
-print("Documents updated successfully.")
+    
+    # Perform the update on the document with the specified UUID
+    collection.update_one({'uuid': uuid_to_update}, update_fields)
+    
+    print(f"Document with UUID {uuid_to_update} updated successfully.")
+else:
+    print(f"No document found with UUID {uuid_to_update}.")
 
