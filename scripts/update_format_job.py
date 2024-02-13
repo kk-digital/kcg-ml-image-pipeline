505a17709adf4d03f12bf9b42518ab064efca465
ssss <sdevdariani9@gmail.com>
2024-02-13 23:02:59 +0400

    add and update apis according new data format

--- scripts/update_format_job.py
+++ scripts/update_format_job.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import pymongo
+from tqdm import tqdm
 
 # Connect to MongoDB
 client = MongoClient('mongodb://192.168.3.1:32017/')
@@ -8,24 +9,17 @@ db = client['orchestration-job-db']
 # Access the specific collection
 collection = db["completed-jobs"]
 
-# Fetch documents that need updating
-documents = collection.find({})
+# Fetch documents that need updating and convert cursor to a list to use with tqdm
+documents = list(collection.find({}))
 
-for document in documents:
-    update_fields = {}
-    prompt_generation_data = document.get('prompt_generation_data', {})
-    
-    # Handling top_k from task_input_dict
+# Use tqdm to wrap the documents list for progress tracking
+for document in tqdm(documents, desc="Processing documents"):
+    # Check if 'top_k' exists within 'task_input_dict'
     if 'top_k' in document.get('task_input_dict', {}):
-        top_k_value = document['task_input_dict']['top_k']
-        prompt_generation_data['top-k'] = top_k_value  # Add top-k to prompt_generation_data
-        # Prepare to remove the original top_k field from task_input_dict
+        # Prepare to remove the 'top_k' field from 'task_input_dict'
+        update_fields = {'$unset': {'task_input_dict.top_k': ""}}
         
-    
-    # Update prompt_generation_data with new values or structure
-    update_fields['$set'] = {'prompt_generation_data': prompt_generation_data}
-    
-    # Perform the update on the current document
-    collection.update_one({'_id': document['_id']}, update_fields)
+        # Perform the update on the current document
+        collection.update_one({'_id': document['_id']}, update_fields)
 
-print("Completed updating documents.")
+print("Completed removing top_k from task_input_dict in documents.")
