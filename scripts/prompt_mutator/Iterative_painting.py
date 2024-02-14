8879d37c351d4dda44606d1807cfb4e60792a77d
rachid2198 <rachid9821@gmail.com>
2024-02-14 20:28:54 +0100

    testing img2img generation

--- scripts/prompt_mutator/Iterative_painting.py
+++ scripts/prompt_mutator/Iterative_painting.py
@@ -286,11 +286,10 @@ class IterativePainter:
     def test_img2img(self):
         init_image= Image.open("input/test_image.jpg").convert("RGB").resize((512,512))
 
-        image_emb, latent=self.image_embedder.get_image_features(init_image)
+        image_emb, latent=self.image_embedder.get_image_features(init_image, return_latents=True)
 
         self.pipeline.unload_models()
         self.pipeline.load_models(task_type="img2img")
-
 
         result_image= self.pipeline.generate_img2img(image_embeds=image_emb, image=latent)
 
