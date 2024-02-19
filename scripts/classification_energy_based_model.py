973f0affa1a425f61208d164559bdd0aa226e43b
Amine-AH <md.amine.ah@gmail.com>
2024-02-19 19:12:47 +0100

    Refactoring the EBM code

--- scripts/classification_energy_based_model.py
+++ scripts/classification_energy_based_model.py
@@ -719,7 +719,7 @@ def train_model(**kwargs):
     trainer = pl.Trainer(default_root_dir=os.path.join(CHECKPOINT_PATH, "MNIST"),
                          accelerator="gpu" if str(device).startswith("cuda") else "cpu",
                          devices=1,
-                         max_epochs=3,
+                         max_epochs=15,
                          gradient_clip_val=0.1,
                          callbacks=[ModelCheckpoint(save_weights_only=True, mode="min", monitor='val_contrastive_divergence'),
                                     GenerateCallback(every_n_epochs=5),
@@ -853,16 +853,40 @@ for i in range(imgs_per_step.shape[1]):
     imgs_to_plot = torch.cat([imgs_per_step[0:1,i],imgs_to_plot], dim=0)
     grid = torchvision.utils.make_grid(imgs_to_plot, nrow=imgs_to_plot.shape[0], normalize=True, pad_value=0.5, padding=2)
     grid = grid.permute(1, 2, 0)
+
+    
     axes[i].imshow(grid)
     axes[i].xlabel("Generation iteration")
     axes[i].xticks([(imgs_per_step.shape[-1]+2)*(0.5+j) for j in range(callback.vis_steps+1)],
                labels=[1] + list(range(step_size,imgs_per_step.shape[0]+1,step_size)))
-plt.yticks([])
+    axes[i].yticks([])
+    
 buf = io.BytesIO()
 plt.savefig(buf, format='png')
 buf.seek(0)
 
 minio_path_i = "environmental/output/my_test/images_generation_sample_" + str(i) +"_" +date_now+".png"
 cmd.upload_data(minio_client, 'datasets', minio_path_i, buf)
+
+
+# for i in range(imgs_per_step.shape[1]):
+#     step_size = callback.num_steps // callback.vis_steps
+#     imgs_to_plot = imgs_per_step[step_size-1::step_size,i]
+#     imgs_to_plot = torch.cat([imgs_per_step[0:1,i],imgs_to_plot], dim=0)
+#     grid = torchvision.utils.make_grid(imgs_to_plot, nrow=imgs_to_plot.shape[0], normalize=True, pad_value=0.5, padding=2)
+#     grid = grid.permute(1, 2, 0)
+    
+#     plt.figure(figsize=(8,8))
+#     plt.imshow(grid)
+#     plt.xlabel("Generation iteration")
+#     plt.xticks([(imgs_per_step.shape[-1]+2)*(0.5+j) for j in range(callback.vis_steps+1)],
+#                labels=[1] + list(range(step_size,imgs_per_step.shape[0]+1,step_size)))
+#     plt.yticks([])
+#     buf = io.BytesIO()
+#     plt.savefig(buf, format='png')
+#     buf.seek(0)
+
+#     minio_path_i = "environmental/output/my_test/images_generation_sample_" + str(i) +"_" +date_now+".png"
+#     cmd.upload_data(minio_client, 'datasets', minio_path_i, buf)
 
 
