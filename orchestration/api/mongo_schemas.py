4b2ffb3b90cc0b737c358c5501f7f3a505644ac0
rachid2198 <rachid9821@gmail.com>
2024-02-22 03:05:23 +0100

    bug fix

--- orchestration/api/mongo_schemas.py
+++ orchestration/api/mongo_schemas.py
@@ -41,7 +41,7 @@ class Task(BaseModel):
 class KandinskyTask(BaseModel):
     job: Task # task data
     positive_embedding: List[List[float]]
-    negative_embedding: Optional[List[list[float]]] = None
+    negative_embedding: Optional[List[List[float]]] = None
 
 class SequentialID:
     dataset_name: str
