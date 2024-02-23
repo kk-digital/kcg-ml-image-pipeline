4b94649e0fd7af7ef72fbe5fbae2f805010646b4
rachid2198 <rachid9821@gmail.com>
2024-02-23 13:44:04 +0100

    bug fix

--- scripts/pez_optimisation_kandinsky.py
+++ scripts/pez_optimisation_kandinsky.py
@@ -100,7 +100,7 @@ class KandinskyImageGenerator:
     
     def get_zero_embed(self, batch_size=1):
         zero_img = torch.zeros(1, 3, self.image_encoder.config.image_size, self.image_encoder.config.image_size).to(
-            device=self.device
+            device=self.device, dtype=torch.float16
         )
         zero_image_emb = self.image_encoder(zero_img)["image_embeds"]
         zero_image_emb = zero_image_emb.repeat(batch_size, 1)
