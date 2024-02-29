7cf17c4657d7fe7a9c56f1920b494c782d41194b
rachid2198 <rachid9821@gmail.com>
2024-02-29 16:35:59 +0100

    adding top-k generation

--- scripts/pez_optimisation_kandinsky.py
+++ scripts/pez_optimisation_kandinsky.py
@@ -196,9 +196,9 @@ class KandinskyImageGenerator:
         return features
 
     def generate_latent(self):
-        # features_data = get_object(self.minio_client, "environmental/0435/434997_clip_kandinsky.msgpack")
-        # features_vector = msgpack.unpackb(features_data)["clip-feature-vector"]
-        # image_embedding= torch.tensor(features_vector).to(device=self.device, dtype=torch.float32)
+        features_data = get_object(self.minio_client, "environmental/0435/434997_clip_kandinsky.msgpack")
+        features_vector = msgpack.unpackb(features_data)["clip-feature-vector"]
+        image_embedding= torch.tensor(features_vector).to(device=self.device, dtype=torch.float32)
         
         random.seed(time.time())
         seed = random.randint(0, 2 ** 24 - 1)
@@ -206,7 +206,7 @@ class KandinskyImageGenerator:
         init_image = Image.open("./test/test_inpainting/white_512x512.jpg")
         df_data=[]
         sampled_embedding= self.sample_embedding()
-        optimized_embedding = sampled_embedding.clone().detach().requires_grad_(True)
+        optimized_embedding = image_embedding.clone().detach().requires_grad_(True)
 
         # Setup the optimizer
         optimizer = optim.Adam([optimized_embedding], lr=self.learning_rate)
