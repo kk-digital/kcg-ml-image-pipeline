3fc306f3f7a077455c814e5d3ce76f13f74998b8
rachid2198 <rachid9821@gmail.com>
2024-03-02 21:10:21 +0100

    bug fix

--- training_worker/latent_generator/train_fc_ab_ranking.py
+++ training_worker/latent_generator/train_fc_ab_ranking.py
@@ -190,7 +190,8 @@ class ABRankingFcTrainingPipeline:
                 image_score = self.scoring_model.predict_clip(clip_vector.unsqueeze(0)).item()
                 input_clip_score= (input_clip_score - self.mean) / self.std
                 image_score= (image_score - self.mean) / self.std
-                cosine_sim =cosine_similarity(clip_vector.squeeze(), latent.squeeze()).item()
+                print(clip_vector.shape, latent.shape)
+                cosine_sim =cosine_similarity(clip_vector, latent.squeeze(0)).item()
 
                 data = {
                     'input_clip': latent.detach().cpu().numpy().tolist(),
