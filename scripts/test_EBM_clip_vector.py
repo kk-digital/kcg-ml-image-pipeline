96de8ab067d1e5c6cb520d69fe0c3a8c31be96f9
Amine-AH <md.amine.ah@gmail.com>
2024-03-05 18:18:26 +0100

    Refactoring the EBM code

--- scripts/test_EBM_clip_vector.py
+++ scripts/test_EBM_clip_vector.py
@@ -1418,7 +1418,7 @@ for i in range(1,len(id_classes_ood)):
 
 
 
-for i in range (16):
+for i in range (5):
     energy_evaluation_with_pictures_clip(images_paths_in[i],images_paths_ood[i])
     
 #energy_evaluation_with_pictures_clip(images_paths_in,images_paths_ood)
