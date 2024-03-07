3a45de82aeb73b5d23bb00b60e2ecf138ee9a3a1
Amine-AH <md.amine.ah@gmail.com>
2024-03-07 14:35:24 +0100

    Refactoring the EBM code

--- scripts/test_EBM_clip_vector.py
+++ scripts/test_EBM_clip_vector.py
@@ -1033,7 +1033,7 @@ def get_clip_embeddings_by_tag(id_classes,label_value):
 
 
 def get_clip_embeddings_by_path(images_paths,label_value):
-    
+    ocult_clips = []
     ocult_clips = get_clip_vectors(images_paths)
 
 
@@ -1466,108 +1466,105 @@ images_paths_ood = get_file_paths("environmental",30000)
 
 images_paths_characters = get_file_paths("character",1500)
 
-print("firs image : ", images_paths_characters[0] )
 
-print("character dataset is lenght is ",len(images_paths_characters))
+train_loader_clip_characters, val_loader_clip_characters= get_clip_embeddings_by_path(images_paths_characters,0)
 
-# train_loader_clip_characters, val_loader_clip_characters= get_clip_embeddings_by_path(images_paths_characters,0)
-
-# train_loader_clip_cyber, val_loader_clip_cyber = get_clip_embeddings_by_tag([7,8,9,15,20,21,22],0)
+train_loader_clip_cyber, val_loader_clip_cyber = get_clip_embeddings_by_tag([7,8,9,15,20,21,22],0)
 
 
-# # Set loaders
-# train_loader = train_loader_clip_characters
-# val_loader = val_loader_clip_characters
-# adv_loader = train_loader_clip_cyber
+# Set loaders
+train_loader = train_loader_clip_characters
+val_loader = val_loader_clip_characters
+adv_loader = train_loader_clip_cyber
 
 
 
 
-# ##################################### Train
-# model = train_model(img_shape=(1,1280),
-#                     batch_size=train_loader.batch_size,
-#                     lr=0.001,
-#                     beta1=0.0)
+##################################### Train
+model = train_model(img_shape=(1,1280),
+                    batch_size=train_loader.batch_size,
+                    lr=0.001,
+                    beta1=0.0)
 
 
 
-# # Plot
+# Plot
 
-# save_model(model,'characters','temp_model.pth')
-
-
-# epochs = range(1, len(total_losses) + 1)  
+save_model(model,'characters','temp_model.pth')
 
 
-# # Create subplots grid (3 rows, 1 column)
-# fig, axes = plt.subplots(4, 1, figsize=(10, 24))
-
-# # Plot each loss on its own subplot
-# axes[0].plot(epochs, total_losses, label='Total Loss')
-# axes[0].set_xlabel('Steps')
-# axes[0].set_ylabel('Loss')
-# axes[0].set_title('Total Loss')
-# axes[0].legend()
-# axes[0].grid(True)
-
-# axes[1].plot(epochs, cdiv_losses, label='Contrastive Divergence Loss')
-# axes[1].set_xlabel('Steps')
-# axes[1].set_ylabel('Loss')
-# axes[1].set_title('Contrastive Divergence Loss')
-# axes[1].legend()
-# axes[1].grid(True)
+epochs = range(1, len(total_losses) + 1)  
 
 
-# axes[2].plot(epochs, reg_losses , label='Regression Loss')
-# axes[2].set_xlabel('Steps')
-# axes[2].set_ylabel('Loss')
-# axes[2].set_title('Regression Loss')
-# axes[2].legend()
-# axes[2].grid(True)
+# Create subplots grid (3 rows, 1 column)
+fig, axes = plt.subplots(4, 1, figsize=(10, 24))
 
-# # Plot real and fake scores on the fourth subplot
-# axes[3].plot(epochs, real_scores_s, label='Real Scores')
-# axes[3].plot(epochs, fake_scores_s, label='Fake Scores')
-# axes[3].set_xlabel('Steps')
-# axes[3].set_ylabel('Score')  # Adjust label if scores represent a different metric
-# axes[3].set_title('Real vs. Fake Scores')
-# axes[3].legend()
-# axes[3].grid(True)
+# Plot each loss on its own subplot
+axes[0].plot(epochs, total_losses, label='Total Loss')
+axes[0].set_xlabel('Steps')
+axes[0].set_ylabel('Loss')
+axes[0].set_title('Total Loss')
+axes[0].legend()
+axes[0].grid(True)
 
-# # Adjust spacing between subplots for better visualization
-# plt.tight_layout()
-
-# plt.savefig("output/loss_tracking_per_step.png")
-
-# # Save the figure to a file
-# buf = io.BytesIO()
-# plt.savefig(buf, format='png')
-# buf.seek(0)
-
-# # upload the graph report
-# minio_path= minio_path + "/loss_tracking_per_step_1_cd_p2_regloss_characters_training" +date_now+".png"
-# cmd.upload_data(minio_client, 'datasets', minio_path, buf)
-# # Remove the temporary file
-# os.remove("output/loss_tracking_per_step.png")
-# # Clear the current figure
-# plt.clf()
+axes[1].plot(epochs, cdiv_losses, label='Contrastive Divergence Loss')
+axes[1].set_xlabel('Steps')
+axes[1].set_ylabel('Loss')
+axes[1].set_title('Contrastive Divergence Loss')
+axes[1].legend()
+axes[1].grid(True)
 
 
-# #load model
-# model5 = DeepEnergyModel(img_shape=(1280,))
-# load_model(model5,'characters')
-# model = model5
+axes[2].plot(epochs, reg_losses , label='Regression Loss')
+axes[2].set_xlabel('Steps')
+axes[2].set_ylabel('Loss')
+axes[2].set_title('Regression Loss')
+axes[2].legend()
+axes[2].grid(True)
+
+# Plot real and fake scores on the fourth subplot
+axes[3].plot(epochs, real_scores_s, label='Real Scores')
+axes[3].plot(epochs, fake_scores_s, label='Fake Scores')
+axes[3].set_xlabel('Steps')
+axes[3].set_ylabel('Score')  # Adjust label if scores represent a different metric
+axes[3].set_title('Real vs. Fake Scores')
+axes[3].legend()
+axes[3].grid(True)
+
+# Adjust spacing between subplots for better visualization
+plt.tight_layout()
+
+plt.savefig("output/loss_tracking_per_step.png")
+
+# Save the figure to a file
+buf = io.BytesIO()
+plt.savefig(buf, format='png')
+buf.seek(0)
+
+# upload the graph report
+minio_path= minio_path + "/loss_tracking_per_step_1_cd_p2_regloss_characters_training" +date_now+".png"
+cmd.upload_data(minio_client, 'datasets', minio_path, buf)
+# Remove the temporary file
+os.remove("output/loss_tracking_per_step.png")
+# Clear the current figure
+plt.clf()
+
+
+#load model
+model5 = DeepEnergyModel(img_shape=(1280,))
+load_model(model5,'characters')
+model = model5
 
 
 
-# #     
-# print("yep it's here")
-# sorted_comic_book = process_and_sort_dataset(images_paths_ood, model)
-# selected_structure_first_52 = sorted_comic_book[:52]
-# selected_structure_second_52 = sorted_comic_book[52:103]
+#     
+print("yep it's here")
+sorted_comic_book = process_and_sort_dataset(images_paths_ood, model)
+selected_structure_first_52 = sorted_comic_book[:52]
+selected_structure_second_52 = sorted_comic_book[52:103]
 
-# plot_images_with_scores(selected_structure_first_52,"Top_first_52_characters_env")
-# plot_images_with_scores(selected_structure_second_52,"Top_second_52_characters_env")
+plot_images_with_scores(selected_structure_first_52,"Top_first_52_characters_env")
+plot_images_with_scores(selected_structure_second_52,"Top_second_52_characters_env")
 
 
 
