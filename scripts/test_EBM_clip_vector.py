d19ce7d8ead52d66699754abe55ff32d5b0a1816
Amine-AH <md.amine.ah@gmail.com>
2024-03-07 11:14:54 +0100

    Refactoring the EBM code

--- scripts/test_EBM_clip_vector.py
+++ scripts/test_EBM_clip_vector.py
@@ -1265,9 +1265,9 @@ adv_loader = train_loader_clip_ood
 # # Clear the current figure
 # plt.clf()
 
-def get_file_paths(self):
+def get_file_paths(dataset,num_samples):
         print('Loading image file paths')
-        response = requests.get(f'{API_URL}/queue/image-generation/list-by-dataset?dataset={self.dataset}&size={self.num_samples}')
+        response = requests.get(f'{API_URL}/queue/image-generation/list-by-dataset?dataset={dataset}&size={num_samples}')
         
         jobs = json.loads(response.content)
 
@@ -1292,15 +1292,16 @@ for i in range(1,len(id_classes_in)):
 
 #id_classes_ood = [7,8,9,15,20,21,22]
 # VS comic book
-id_classes_ood = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,45,46,47,48,49,50]
+# id_classes_ood = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,45,46,47,48,49,50]
+
+# images_paths_ood  = get_tag_jobs(id_classes_ood [0])
+# i = 1
+# for i in range(1,len(id_classes_ood)):
+#     images_paths_ood  = images_paths_ood  + get_tag_jobs(id_classes_ood [i])
 
 
 
-images_paths_ood  = get_tag_jobs(id_classes_ood [0])
-i = 1
-for i in range(1,len(id_classes_ood)):
-    images_paths_ood  = images_paths_ood  + get_tag_jobs(id_classes_ood [i])
-
+images_paths_ood = get_file_paths("environmental",1000)
 
 # # Test on some pcitures
 # for i in range (len(images_paths_in)):
