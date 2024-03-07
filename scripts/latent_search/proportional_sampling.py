a166e1684c2fd892173f1b3442538b712b039e14
rachid2198 <rachid9821@gmail.com>
2024-03-07 19:16:36 +0100

    function for balancing generation bins

--- scripts/latent_search/proportional_sampling.py
+++ scripts/latent_search/proportional_sampling.py
@@ -21,11 +21,9 @@ def parse_args():
         parser.add_argument('--minio-access-key', type=str, help='Minio access key')
         parser.add_argument('--minio-secret-key', type=str, help='Minio secret key')
         parser.add_argument('--dataset', type=str, help='Name of the dataset', default="environmental")
-        parser.add_argument('--min-sigma', type=float, help='Minimal sigma score', default=-5)
-        parser.add_argument('--max-sigma', type=float, help='Maximal sigma score', default=5)
-        parser.add_argument('--max-generations', type=int, help='Maximum number of generated vectors', default=500000)
         parser.add_argument('--num-bins', type=int, help='Number of bins', default=10)
-        parser.add_argument('--num-images', type=int, help='Number of images to generate', default=50)
+        parser.add_argument('--num-images', type=int, help='Number of images to generate', default=1000)
+        parser.add_argument('--top-k', type=float, help='Portion of samples to generate images with', default=0.1)
         parser.add_argument('--send-job', action='store_true', default=False)
         parser.add_argument('--save-csv', action='store_true', default=False)
 
@@ -36,10 +34,8 @@ class KandinskyImageGenerator:
                  minio_access_key,
                  minio_secret_key,
                  dataset,
-                 min_sigma,
-                 max_sigma,
-                 max_generations,
                  num_bins,
+                 top_k,
                  send_job=False,
                  save_csv=False
                 ):
@@ -47,10 +43,8 @@ class KandinskyImageGenerator:
         self.dataset= dataset
         self.send_job= send_job
         self.save_csv= save_csv
-        self.min_sigma= min_sigma
-        self.max_sigma= max_sigma
-        self.max_generations= max_generations
         self.num_bins= num_bins
+        self.top_k= top_k
 
         # get minio client
         self.minio_client = cmd.get_minio_client(minio_access_key=minio_access_key,
@@ -80,45 +74,36 @@ class KandinskyImageGenerator:
 
         return mean_vector, std_vector, max_vector, min_vector
     
-    def generate_sample(self):
-        """Generates a single embedding."""
-        return torch.normal(mean=self.clip_mean, std=self.clip_std).unsqueeze(0)
-    
-    def get_score(self, embedding):
-        """Scores a given embedding."""
-        with torch.no_grad():
-            score= self.scoring_model.model(embedding).item()
-
-        return score
-
     def proportional_sampling(self, num_samples):
         num_bins = self.num_bins
         embeddings_per_bin = num_samples // self.num_bins
 
-        # Define bins
-        bins = np.linspace(self.min_sigma, self.max_sigma, num_bins+1)
+        # Generate a large batch of embeddings
+        embeddings, scores = self.sample_embeddings(num_samples / self.top_k)
+
+        # Determine min and max scores
+        min_score, max_score = min(scores), max(scores)
+
+        # Define bins based on the range of scores
+        bins = np.linspace(min_score, max_score, num_bins + 1)
 
         # Initialize bins
         binned_vectors = {i: [] for i in range(num_bins)}
         bins_full = 0
 
-        for _ in range(self.max_generations):
+        for embedding, score in zip(embeddings, scores):
             if bins_full == num_bins:
                 break
-
-            # Generate a sample embedding and score it
-            embedding = self.generate_sample()
-            score = self.get_score(embedding)
-
+            
             # Determine the appropriate bin for the current score
             bin_index = np.digitize(score, bins) - 1
             # Adjust bin index to fit within the range of defined bins
-            bin_index = max(0, min(bin_index, num_bins-1))
-
+            bin_index = max(0, min(bin_index, num_bins - 1))
+            
             # Add embedding to the bin if not full
             if len(binned_vectors[bin_index]) < embeddings_per_bin:
                 binned_vectors[bin_index].append(embedding)
-
+                
                 # Check if the bin is now full
                 if len(binned_vectors[bin_index]) == embeddings_per_bin:
                     bins_full += 1
@@ -130,19 +115,24 @@ class KandinskyImageGenerator:
         for bin_index, embeddings in binned_vectors.items():
             print(f"Bin {bin_index}: {len(embeddings)} embeddings")
 
-    def sample_embeddings(self, num_samples=1000):
+    def sample_embeddings(self, num_samples):
         sampled_embeddings = torch.normal(mean=self.clip_mean.repeat(num_samples, 1),
                                         std=self.clip_std.repeat(num_samples, 1))
-        
+
         # Score each sampled embedding
         scores=[]
         embeddings=[]
-        for embed in sampled_embeddings:
-            embeddings.append(embed.unsqueeze(0))
-            score = self.scoring_model.model(embed.unsqueeze(0)).item() 
-            scores.append(score)
+        # Perform prediction in batches
+        with torch.no_grad():
+            for i in range(0, len(sampled_embeddings), self.batch_size):
+                batch = sampled_embeddings[i:i + self.batch_size]  # Extract a batch
+                embeddings.extend(batch)
+                outputs = self.scoring_model.model(batch)  # Get predictions for this batch
+                # Concatenate all scores and convert to a list
+                outputs = torch.cat(outputs, dim=0).cpu().numpy().list()
+                scores.extend(outputs)
         
-        return scores
+        return embeddings, scores
     
 def main():
     args= parse_args()
@@ -150,10 +140,8 @@ def main():
     generator= KandinskyImageGenerator(minio_access_key=args.minio_access_key,
                                        minio_secret_key=args.minio_secret_key,
                                        dataset=args.dataset,
-                                       min_sigma=args.min_sigma,
-                                       max_sigma=args.max_sigma,
-                                       max_generations=args.max_generations,
                                        num_bins=args.num_bins,
+                                       top_k= args.top_k,
                                        send_job= args.send_job,
                                        save_csv= args.save_csv)
     
