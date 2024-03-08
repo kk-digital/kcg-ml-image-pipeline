d8d1b0495d4eeab1de159d7d7ce1bf57e57b972d
Amine-AH <md.amine.ah@gmail.com>
2024-03-08 15:45:59 +0100

    Refactoring the EBM code

--- scripts/test_EBM_clip_vector.py
+++ scripts/test_EBM_clip_vector.py
@@ -1427,7 +1427,7 @@ id_classes_ood = [42]
 # Add top 200 images to the train set
 
 #load original model
-images_paths_ood = get_file_paths("environmental",500)
+images_paths_ood = get_file_paths("environmental",30000)
 cyber_model = DeepEnergyModel(img_shape=(1280,))
 load_model(cyber_model,'cyber')
 
@@ -1439,94 +1439,93 @@ selected_structure_first_500 = sorted_comic_book[:500]
 # selected_structure_first_500 = selected_structure_first_500[0,:]
 selected_structure_first_500 = [item[0] for item in selected_structure_first_500]
 
-for path in selected_structure_first_500:
-    print ("dat path is : ", path)
-#train_loader_automated, val_loader_automated = get_clip_embeddings_by_path(selected_structure_first_500,1)
+
+train_loader_automated, val_loader_automated = get_clip_embeddings_by_path(selected_structure_first_500,1)
 
 
-# #Get real images
-# train_loader_clip_cyber, val_loader_clip_cyber = get_clip_embeddings_by_tag([35],1)
-# train_loader_clip_ood, val_loader_clip_ood = get_clip_embeddings_by_tag([7,8,9,15,20,21,22],0)
-
-
-
-
-# print("val loader lenght: ", len(val_loader_clip_cyber))
-
-# train_loader = train_loader_automated
-# val_loader = val_loader_automated
-# adv_loader = train_loader_clip_ood
-
-
-# # Train
-# model2 = train_model(img_shape=(1,1280),
-#                     batch_size=train_loader.batch_size,
-#                     lr=0.001,
-#                     beta1=0.0)
-# save_model(model2,'cyber','temp_model.pth')
-# modelsave = model
-# model = model2
-# # Plot
+#Get real images
+train_loader_clip_cyber, val_loader_clip_cyber = get_clip_embeddings_by_tag([35],1)
+train_loader_clip_ood, val_loader_clip_ood = get_clip_embeddings_by_tag([7,8,9,15,20,21,22],0)
 
 
 
 
-# ############### Plot graph
-# epochs = range(1, len(total_losses) + 1)  
+print("val loader lenght: ", len(val_loader_clip_cyber))
 
-# # Create subplots grid (3 rows, 1 column)
-# fig, axes = plt.subplots(4, 1, figsize=(10, 24))
-
-# # Plot each loss on its own subplot
-# axes[0].plot(epochs, total_losses, label='Total Loss')
-# axes[0].set_xlabel('Steps')
-# axes[0].set_ylabel('Loss')
-# axes[0].set_title('Total Loss')
-# axes[0].legend()
-# axes[0].grid(True)
-
-# axes[1].plot(epochs, cdiv_losses, label='Contrastive Divergence Loss')
-# axes[1].set_xlabel('Steps')
-# axes[1].set_ylabel('Loss')
-# axes[1].set_title('Contrastive Divergence Loss')
-# axes[1].legend()
-# axes[1].grid(True)
+train_loader = train_loader_automated
+val_loader = val_loader_automated
+adv_loader = train_loader_clip_ood
 
 
-# axes[2].plot(epochs, reg_losses , label='Regression Loss')
-# axes[2].set_xlabel('Steps')
-# axes[2].set_ylabel('Loss')
-# axes[2].set_title('Regression Loss')
-# axes[2].legend()
-# axes[2].grid(True)
+# Train
+model2 = train_model(img_shape=(1,1280),
+                    batch_size=train_loader.batch_size,
+                    lr=0.001,
+                    beta1=0.0)
+save_model(model2,'cyber','temp_model.pth')
+modelsave = model
+model = model2
+# Plot
 
-# # Plot real and fake scores on the fourth subplot
-# axes[3].plot(epochs, real_scores_s, label='Real Scores')
-# axes[3].plot(epochs, fake_scores_s, label='Fake Scores')
-# axes[3].set_xlabel('Steps')
-# axes[3].set_ylabel('Score')  # Adjust label if scores represent a different metric
-# axes[3].set_title('Real vs. Fake Scores')
-# axes[3].legend()
-# axes[3].grid(True)
 
-# # Adjust spacing between subplots for better visualization
-# plt.tight_layout()
 
-# plt.savefig("output/loss_tracking_per_step.png")
 
-# # Save the figure to a file
-# buf = io.BytesIO()
-# plt.savefig(buf, format='png')
-# buf.seek(0)
+############### Plot graph
+epochs = range(1, len(total_losses) + 1)  
 
-# # upload the graph report
-# minio_path="environmental/output/my_tests"
-# minio_path= minio_path + "/loss_tracking_per_step_1_cd_p2_regloss_cyber_training" +date_now+".png"
-# cmd.upload_data(minio_client, 'datasets', minio_path, buf)
-# # Remove the temporary file
-# os.remove("output/loss_tracking_per_step.png")
-# # Clear the current figure
-# plt.clf()
+# Create subplots grid (3 rows, 1 column)
+fig, axes = plt.subplots(4, 1, figsize=(10, 24))
+
+# Plot each loss on its own subplot
+axes[0].plot(epochs, total_losses, label='Total Loss')
+axes[0].set_xlabel('Steps')
+axes[0].set_ylabel('Loss')
+axes[0].set_title('Total Loss')
+axes[0].legend()
+axes[0].grid(True)
+
+axes[1].plot(epochs, cdiv_losses, label='Contrastive Divergence Loss')
+axes[1].set_xlabel('Steps')
+axes[1].set_ylabel('Loss')
+axes[1].set_title('Contrastive Divergence Loss')
+axes[1].legend()
+axes[1].grid(True)
+
+
+axes[2].plot(epochs, reg_losses , label='Regression Loss')
+axes[2].set_xlabel('Steps')
+axes[2].set_ylabel('Loss')
+axes[2].set_title('Regression Loss')
+axes[2].legend()
+axes[2].grid(True)
+
+# Plot real and fake scores on the fourth subplot
+axes[3].plot(epochs, real_scores_s, label='Real Scores')
+axes[3].plot(epochs, fake_scores_s, label='Fake Scores')
+axes[3].set_xlabel('Steps')
+axes[3].set_ylabel('Score')  # Adjust label if scores represent a different metric
+axes[3].set_title('Real vs. Fake Scores')
+axes[3].legend()
+axes[3].grid(True)
+
+# Adjust spacing between subplots for better visualization
+plt.tight_layout()
+
+plt.savefig("output/loss_tracking_per_step.png")
+
+# Save the figure to a file
+buf = io.BytesIO()
+plt.savefig(buf, format='png')
+buf.seek(0)
+
+# upload the graph report
+minio_path="environmental/output/my_tests"
+minio_path= minio_path + "/loss_tracking_per_step_1_cd_p2_regloss_cyber_training" +date_now+".png"
+cmd.upload_data(minio_client, 'datasets', minio_path, buf)
+# Remove the temporary file
+os.remove("output/loss_tracking_per_step.png")
+# Clear the current figure
+plt.clf()
 
 
 
@@ -1556,19 +1555,19 @@ for path in selected_structure_first_500:
 
 
 
-# #automated model
-# #toodoo
-# #go create something
-# print("yep it's here")
-# sorted_comic_book = process_and_sort_dataset(images_paths_ood, model)
-# get_structure_csv_content(sorted_comic_book,"cyber_on_env_30000_sample")
-# selected_structure_first_52 = sorted_comic_book[:52]
-# selected_structure_second_52 = sorted_comic_book[52:103]
-# selected_structure_third_52 = sorted_comic_book[103:154]
+#automated model
+#toodoo
+#go create something
+print("yep it's here")
+sorted_comic_book = process_and_sort_dataset(images_paths_ood, model)
+get_structure_csv_content(sorted_comic_book,"cyber_on_env_30000_sample")
+selected_structure_first_52 = sorted_comic_book[:52]
+selected_structure_second_52 = sorted_comic_book[52:103]
+selected_structure_third_52 = sorted_comic_book[103:154]
 
-# plot_images_with_scores(selected_structure_first_52,"Top_first_52_cyber_env_30000_sample")
-# plot_images_with_scores(selected_structure_second_52,"Top_second_52_cyber_env_30000_sample")
-# plot_images_with_scores(selected_structure_third_52,"Top_third_52_cyber_env_30000_sample")
+plot_images_with_scores(selected_structure_first_52,"Top_first_52_cyber_env_30000_sample")
+plot_images_with_scores(selected_structure_second_52,"Top_second_52_cyber_env_30000_sample")
+plot_images_with_scores(selected_structure_third_52,"Top_third_52_cyber_env_30000_sample")
 
 
 
