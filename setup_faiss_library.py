d76f9d7e2565b6ae7fe6b81b4b5efd9243e5a3aa
rachid2198 <rachid9821@gmail.com>
2024-03-20 16:38:56 +0100

    bug fix

--- setup_faiss_library.py
+++ setup_faiss_library.py
@@ -10,11 +10,18 @@ def run_command(command):
         print(f"Error executing command: {command}\nExit code: {e.returncode}\nOutput: {e.output.decode()}")
 
 def main():
-    # Clone the Faiss repository
-    if not os.path.exists("input/faiss"):
-        run_command("git clone https://github.com/facebookresearch/faiss.git input/faiss")
+    # Ensure the input/faiss directory structure exists
+    faiss_dir_path = "input/faiss"
+    os.makedirs(faiss_dir_path, exist_ok=True)
     
-    os.chdir("input/faiss")
+    # Change directory to input
+    os.chdir("input")
+
+    # Clone the Faiss repository
+    if not os.path.exists("faiss"):
+        run_command("git clone https://github.com/facebookresearch/faiss.git faiss")
+    
+    os.chdir("faiss")
     
     # Create a build directory
     if not os.path.exists("build"):
