c0aaf8a97066e490df200e2b10e294d58578a219
rachid2198 <rachid9821@gmail.com>
2024-03-21 05:59:12 +0100

    setting default datapoints per sphere number to 16

--- training_worker/sampling/scripts/train_uniform_sampling_models.py
+++ training_worker/sampling/scripts/train_uniform_sampling_models.py
@@ -16,12 +16,12 @@ def parse_args():
     parser.add_argument('--minio-secret-key', type=str, help='Minio secret key')
     parser.add_argument('--dataset', type=str, help='Name of the dataset', default="environmental")
     parser.add_argument('--target-avg-points', type=int, help='Target average of datapoints per sphere', 
-                        default=5)
+                        default=16)
     parser.add_argument('--n-spheres', type=int, help='Number of spheres', default=100000)
     parser.add_argument('--training-batch-size', type=int, default=64)
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--learning-rate', type=float, default=0.001)
-    parser.add_argument('--output-type', type=str, help="output type, variance, mean_sigma_score or score_distribution", default="mean_sigma_score")
+    parser.add_argument('--output-type', type=str, help="variance, mean_sigma_score or score_distribution", default="mean_sigma_score")
     parser.add_argument('--output-size', type=int, default=8)
     parser.add_argument('--bin-size', type=int, default=1)
 
