fa28939eed56178b017522d5a853fbdb7bd0ca95
Amine-AH <md.amine.ah@gmail.com>
2024-03-22 00:26:16 +0100

    add new script class for training the EBM, and testing automated labeling, changing the saving format

--- scripts/train_ebm_on_kandinsky_clip_embeddings.py
+++ scripts/train_ebm_on_kandinsky_clip_embeddings.py
@@ -1624,7 +1624,7 @@ load_model_to_minio(reloaded_model,model_name)
 
 
 # Load the environmental dataset     tooodoooo
-images_paths_ood, hashes = get_file_paths_and_hashes("environmental",5000)
+images_paths_ood, hashes = get_file_paths_and_hashes("environmental",40000)
 
 # #go create something
 # print("yep it's here")
