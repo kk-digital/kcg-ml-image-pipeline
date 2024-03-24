4dcd52d2faadc3ff36b538a202167b52834db660
charles_lee <lucky.goldragon@gmail.com>
2024-03-24 14:27:54 -0400

    fix bug

--- orchestration/api/api_classifier_score.py
+++ orchestration/api/api_classifier_score.py
@@ -254,11 +254,15 @@ async def list_images_by_classifier_scores(
     cursor = request.app.image_classifier_scores_collection.find(query).limit(limit)
     scores_data = list(cursor)
 
+    # Remove _id in response data
+    for score in scores_data:
+        score.pop('_id', None)
+
     # Prepare the data for the response
-    images_data = [ListClassifierScore(**doc).dict() for doc in scores_data]
+    images_data = ListClassifierScore(images=[ClassifierScore(**doc).to_dict() for doc in scores_data]).dict()
 
     # Return the fetched data with a success response
     return response_handler.create_success_response_v1(
-        response_data={"images":images_data}, 
+        response_data=images_data, 
         http_status_code=200
     )
