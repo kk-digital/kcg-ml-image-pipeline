77c787afd58f1ed17530ec94431ef03177b14920
Amine-AH <md.amine.ah@gmail.com>
2024-03-25 22:19:23 +0100

    create new file for the sole purpose of training the models

--- training_worker/classifiers/models/ebm_single_class.py
+++ training_worker/classifiers/models/ebm_single_class.py
@@ -333,8 +333,10 @@ class EBM_Single_Class_Trainer:
             # Upload the model to MinIO
 
             cmd.is_object_exists(minio_client, bucket_name,
-                                      os.path.join(output_path, filename + ".safetensors"),BytesIO(model_bytes))
+                                      os.path.join(output_path, filename + ".safetensors"))
             
+            # upload model
+            cmd.upload_data(minio_client, bucket_name, filename, BytesIO(model_bytes))
             # get model card and upload
             classifier_name="{}-{}-{}-{}".format(self.class_id, output_type, network_type, input_type)
             model_card_name = "{}.json".format(filename)
