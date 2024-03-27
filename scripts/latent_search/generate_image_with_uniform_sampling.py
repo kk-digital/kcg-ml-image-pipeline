4d28f086bf609c21dabccdf34fcd0687fd5542cf
charles_lee <lucky.goldragon@gmail.com>
2024-03-27 02:46:46 -0400

    change model

--- scripts/latent_search/generate_image_with_uniform_sampling.py
+++ scripts/latent_search/generate_image_with_uniform_sampling.py
@@ -7,7 +7,7 @@ import faiss
 base_directory = './'
 sys.path.insert(0, base_directory)
 
-from training_worker.sampling.models.sampling_regression_fc import SamplingFCRegressionNetwork
+from training_worker.sampling.models.uniform_sampling_regression_fc import SamplingFCRegressionNetwork
 from utility.minio import cmd
 from data_loader.utils import get_object
 import numpy as np
