c182fa4d54578c2e0f73f47a14c38be37b3003d8
rachid2198 <rachid9821@gmail.com>
2024-03-29 06:59:28 +0100

    testing training with small number of datapoints

--- data_loader/kandinsky_dataset_loader.py
+++ data_loader/kandinsky_dataset_loader.py
@@ -97,6 +97,7 @@ class KandinskyDatasetLoader:
         feature_vectors=[]
         scores=[]
         
+        index=0
         print("Loading input clip vectors and sigma scores for each job")
         for job in tqdm(jobs):
             try:
@@ -123,6 +124,9 @@ class KandinskyDatasetLoader:
 
             except:
                 print("An error occured")
+            
+            if index>50:
+                break
         
         return feature_vectors, scores
 
