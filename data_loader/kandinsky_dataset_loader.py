6330c310fae0db72cc4a19dd6352b71568dfe9bc
rachid2198 <rachid9821@gmail.com>
2024-03-29 07:00:54 +0100

    testing training with small number of datapoints

--- data_loader/kandinsky_dataset_loader.py
+++ data_loader/kandinsky_dataset_loader.py
@@ -125,6 +125,7 @@ class KandinskyDatasetLoader:
             except:
                 print("An error occured")
             
+            index+=1
             if index>50:
                 break
         
