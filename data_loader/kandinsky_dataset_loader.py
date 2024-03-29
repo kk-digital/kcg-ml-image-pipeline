e3dbba958ee0e9263e03b7bb075da8ce5e347847
rachid2198 <rachid9821@gmail.com>
2024-03-29 06:57:34 +0100

    reverting temporary changes

--- data_loader/kandinsky_dataset_loader.py
+++ data_loader/kandinsky_dataset_loader.py
@@ -96,8 +96,7 @@ class KandinskyDatasetLoader:
         jobs= self.load_kandinsky_jobs()
         feature_vectors=[]
         scores=[]
-
-        index=0
+        
         print("Loading input clip vectors and sigma scores for each job")
         for job in tqdm(jobs):
             try:
@@ -124,10 +123,6 @@ class KandinskyDatasetLoader:
 
             except:
                 print("An error occured")
-            
-            index+=1
-            if index>50:
-                break
         
         return feature_vectors, scores
 
