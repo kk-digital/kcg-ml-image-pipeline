cbcb773aa54e373a3186eef2bf390480d7d4f13b
rachid2198 <rachid9821@gmail.com>
2024-04-03 17:12:32 +0100

    testing float32

--- kandinsky/kandinsky_train.py
+++ kandinsky/kandinsky_train.py
@@ -44,7 +44,7 @@ def log_memory_usage(description, current_memory_usage):
 
     return total_memory
 
-weight_dtype = torch.float32
+weight_dtype = torch.float16
 
 device = torch.device(0)
 
@@ -102,158 +102,149 @@ image_encoder.requires_grad_(False)
 unet.enable_gradient_checkpointing()
 
 # Move image_encode and vae to gpu and cast to weight_dtype
-# image_encoder.to(device, dtype=weight_dtype)
-# total_memory= log_memory_usage("Loading the image encoder", total_memory)
-# vae.to(device, dtype=weight_dtype)
-# total_memory= log_memory_usage("Loading the vae", total_memory)
+image_encoder.to(device, dtype=weight_dtype)
+total_memory= log_memory_usage("Loading the image encoder", total_memory)
+vae.to(device, dtype=weight_dtype)
+total_memory= log_memory_usage("Loading the vae", total_memory)
 unet.to(device, dtype=weight_dtype)
 total_memory= log_memory_usage("Loading the Unet", total_memory)
-# optimizer = optimizer_cls(
-#     unet.parameters(),
-#     lr=learning_rate,
-#     betas=(adam_beta1, adam_beta2),
-#     weight_decay=adam_weight_decay,
-#     eps=adam_epsilon,
-# )
-# def center_crop(image):
-#     width, height = image.size
-#     new_size = min(width, height)
-#     left = (width - new_size) / 2
-#     top = (height - new_size) / 2
-#     right = (width + new_size) / 2
-#     bottom = (height + new_size) / 2
-#     return image.crop((left, top, right, bottom))
+optimizer = optimizer_cls(
+    unet.parameters(),
+    lr=learning_rate,
+    betas=(adam_beta1, adam_beta2),
+    weight_decay=adam_weight_decay,
+    eps=adam_epsilon,
+)
+def center_crop(image):
+    width, height = image.size
+    new_size = min(width, height)
+    left = (width - new_size) / 2
+    top = (height - new_size) / 2
+    right = (width + new_size) / 2
+    bottom = (height + new_size) / 2
+    return image.crop((left, top, right, bottom))
 
-# def train_transforms(img):
-#     img = center_crop(img)
-#     img = img.resize((resolution, resolution), resample=Image.BICUBIC, reducing_gap=1)
-#     img = np.array(img).astype(np.float32) / 127.5 - 1
-#     img = torch.from_numpy(np.transpose(img, [2, 0, 1]))
-#     return img
+def train_transforms(img):
+    img = center_crop(img)
+    img = img.resize((resolution, resolution), resample=Image.BICUBIC, reducing_gap=1)
+    img = np.array(img).astype(np.float32) / 127.5 - 1
+    img = torch.from_numpy(np.transpose(img, [2, 0, 1]))
+    return img
 
-# def preprocess_train(examples):
-#     images = [image.convert("RGB") for image in examples[image_column]]
-#     examples["pixel_values"] = [train_transforms(image) for image in images]
-#     examples["clip_pixel_values"] = image_processor(images, return_tensors="pt").pixel_values
-#     return examples
+def preprocess_train(examples):
+    images = [image.convert("RGB") for image in examples[image_column]]
+    examples["pixel_values"] = [train_transforms(image) for image in images]
+    examples["clip_pixel_values"] = image_processor(images, return_tensors="pt").pixel_values
+    return examples
 
-# if not os.path.exists("input/pokemon-blip-captions"):
-#     dataset = datasets.load_dataset('reach-vb/pokemon-blip-captions')
-#     dataset.save_to_disk('input/pokemon-blip-captions')
+if not os.path.exists("input/pokemon-blip-captions"):
+    dataset = datasets.load_dataset('reach-vb/pokemon-blip-captions')
+    dataset.save_to_disk('input/pokemon-blip-captions')
 
-# dataset = datasets.load_dataset('arrow', data_files={'train': 'input/pokemon-blip-captions/train/data-00000-of-00001.arrow'})
+dataset = datasets.load_dataset('arrow', data_files={'train': 'input/pokemon-blip-captions/train/data-00000-of-00001.arrow'})
 
-# # Set the training transforms
-# train_dataset = dataset["train"].with_transform(preprocess_train)
+# Set the training transforms
+train_dataset = dataset["train"].with_transform(preprocess_train)
 
-# def collate_fn(examples):
-#     pixel_values = torch.stack([example["pixel_values"] for example in examples])
-#     pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
-#     clip_pixel_values = torch.stack([example["clip_pixel_values"] for example in examples])
-#     clip_pixel_values = clip_pixel_values.to(memory_format=torch.contiguous_format).float()
-#     return {"pixel_values": pixel_values, "clip_pixel_values": clip_pixel_values}
+def collate_fn(examples):
+    pixel_values = torch.stack([example["pixel_values"] for example in examples])
+    pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
+    clip_pixel_values = torch.stack([example["clip_pixel_values"] for example in examples])
+    clip_pixel_values = clip_pixel_values.to(memory_format=torch.contiguous_format).float()
+    return {"pixel_values": pixel_values, "clip_pixel_values": clip_pixel_values}
 
-# train_dataloader = torch.utils.data.DataLoader(
-#     train_dataset,
-#     shuffle=True,
-#     collate_fn=collate_fn,
-#     batch_size=train_batch_size,
-#     num_workers=dataloader_num_workers,
-# )
-# lr_scheduler = get_scheduler(
-#     lr_scheduler,
-#     optimizer=optimizer,
-#     num_warmup_steps=lr_warmup_steps * gradient_accumulation_steps,
-#     num_training_steps=max_train_steps * gradient_accumulation_steps,
-# )
-# # progress_bar = tqdm(range(0, max_train_steps), desc="Steps")
+train_dataloader = torch.utils.data.DataLoader(
+    train_dataset,
+    shuffle=True,
+    collate_fn=collate_fn,
+    batch_size=train_batch_size,
+    num_workers=dataloader_num_workers,
+)
+lr_scheduler = get_scheduler(
+    lr_scheduler,
+    optimizer=optimizer,
+    num_warmup_steps=lr_warmup_steps * gradient_accumulation_steps,
+    num_training_steps=max_train_steps * gradient_accumulation_steps,
+)
+# progress_bar = tqdm(range(0, max_train_steps), desc="Steps")
 
-# epoch = 1
-# step = 0
-# data_iter = iter(train_dataloader)
+epoch = 1
+step = 0
+data_iter = iter(train_dataloader)
 
-# losses = list()
+losses = list()
 
-# # # Initialize the gradient scaler for mixed precision training
-# # scaler = torch.cuda.amp.GradScaler()
+# # Initialize the gradient scaler for mixed precision training
+# scaler = torch.cuda.amp.GradScaler()
 
-# while step < max_train_steps:
-#     try:
-#         batch = next(data_iter)
-#     except StopIteration:
-#         epoch += 1
-#         data_iter = iter(train_dataloader)
-#         batch = next(data_iter)
-#         print(f"Epoch: {epoch}, Step: {step}, Loss: {np.mean(losses)}")
-#         losses = list()
+while step < max_train_steps:
+    try:
+        batch = next(data_iter)
+    except StopIteration:
+        epoch += 1
+        data_iter = iter(train_dataloader)
+        batch = next(data_iter)
+        print(f"Epoch: {epoch}, Step: {step}, Loss: {np.mean(losses)}")
+        losses = list()
 
-#     # Set unet to trainable.
-#     unet.train()
+    # Set unet to trainable.
+    unet.train()
 
-#     # Convert images to latent space
-#     with torch.no_grad():
-#         images = batch["pixel_values"].to(device, weight_dtype)
-#         clip_images = batch["clip_pixel_values"].to(device, weight_dtype)
-#         latents = vae.encode(images).latents
-#         image_embeds = image_encoder(clip_images).image_embeds
+    # Convert images to latent space
+    with torch.no_grad():
+        images = batch["pixel_values"].to(device, weight_dtype)
+        clip_images = batch["clip_pixel_values"].to(device, weight_dtype)
+        latents = vae.encode(images).latents
+        image_embeds = image_encoder(clip_images).image_embeds
 
-#     # Sample noise that we'll add to the latents
-#     noise = torch.randn_like(latents)
-#     bsz = latents.shape[0]
-#     # Sample a random timestep for each image
-#     timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (bsz,), device=latents.device)
-#     timesteps = timesteps.long()
+    # Sample noise that we'll add to the latents
+    noise = torch.randn_like(latents)
+    bsz = latents.shape[0]
+    # Sample a random timestep for each image
+    timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (bsz,), device=latents.device)
+    timesteps = timesteps.long()
 
-#     noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
-#     target = noise
+    noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
+    target = noise
 
-#     with torch.cuda.amp.autocast(True):
-#         added_cond_kwargs = {"image_embeds": image_embeds}
-#         with profile(activities=[ProfilerActivity.CUDA], profile_memory=True, record_shapes=True) as inference_prof:
-#             model_pred = unet(noisy_latents, timesteps, None, added_cond_kwargs=added_cond_kwargs).sample[:, :4]
-#             if snr_gamma is None:
-#                 loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
-#             else:
-#                 snr = compute_snr(noise_scheduler, timesteps)
-#                 mse_loss_weights = torch.stack([snr, snr_gamma * torch.ones_like(timesteps)], dim=1).min(dim=1)[0]
-#                 if noise_scheduler.config.prediction_type == "epsilon":
-#                     mse_loss_weights = mse_loss_weights / snr
-#                 elif noise_scheduler.config.prediction_type == "v_prediction":
-#                     mse_loss_weights = mse_loss_weights / (snr + 1)
-#                 loss = F.mse_loss(model_pred.float(), target.float(), reduction="none")
-#                 loss = loss.mean(dim=list(range(1, len(loss.shape)))) * mse_loss_weights
-#                 loss = loss.mean()
-#         loss = loss / gradient_accumulation_steps  # Adjust loss for gradient accumulation
+    with torch.cuda.amp.autocast(True):
+        added_cond_kwargs = {"image_embeds": image_embeds}
+        model_pred = unet(noisy_latents, timesteps, None, added_cond_kwargs=added_cond_kwargs).sample[:, :4]
+        if snr_gamma is None:
+            loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
+            total_memory= log_memory_usage("Forward Pass", total_memory)
+        else:
+            snr = compute_snr(noise_scheduler, timesteps)
+            mse_loss_weights = torch.stack([snr, snr_gamma * torch.ones_like(timesteps)], dim=1).min(dim=1)[0]
+            if noise_scheduler.config.prediction_type == "epsilon":
+                mse_loss_weights = mse_loss_weights / snr
+            elif noise_scheduler.config.prediction_type == "v_prediction":
+                mse_loss_weights = mse_loss_weights / (snr + 1)
+            loss = F.mse_loss(model_pred.float(), target.float(), reduction="none")
+            loss = loss.mean(dim=list(range(1, len(loss.shape)))) * mse_loss_weights
+            loss = loss.mean()
+        loss = loss / gradient_accumulation_steps  # Adjust loss for gradient accumulation
 
-#     # Backward pass profiling
-#     with profile(activities=[ProfilerActivity.CUDA], profile_memory=True, record_shapes=True) as backward_pass_prof:
-#         loss.backward()
+    # Backward pass profiling
+    loss.backward()
+    total_memory= log_memory_usage("Backward Pass", total_memory)
+    step+=1 
+    # Optimizer step profiling
+    if step % gradient_accumulation_steps == 0:
+        optimizer.step()
+        total_memory= log_memory_usage("Optimizer Step", total_memory)
+        lr_scheduler.step()
+        optimizer.zero_grad()
 
-#     step+=1 
-#     # Optimizer step profiling
-#     if step % gradient_accumulation_steps == 0:
-#         with profile(activities=[ProfilerActivity.CUDA], profile_memory=True, record_shapes=True) as optimizer_prof:
-#             optimizer.step()
-#         lr_scheduler.step()
-#         optimizer.zero_grad()
+    losses.append(loss.detach().cpu().numpy())
 
-#     losses.append(loss.detach().cpu().numpy())
+    # Save model periodically or based on conditions
+    if step % checkpointing_steps == 0:
+        print(f"Epoch: {epoch}, Step: {step}, Loss: {np.mean(losses)}")
+        losses = list()
+        # torch.save(unet.state_dict(), f"unet.pth")
 
-#     # Save model periodically or based on conditions
-#     if step % checkpointing_steps == 0:
-#         print(f"Epoch: {epoch}, Step: {step}, Loss: {np.mean(losses)}")
-#         losses = list()
-#         # torch.save(unet.state_dict(), f"unet.pth")
-
-# # Analyze profiling results
-# inference_report= inference_prof.key_averages().table(sort_by="self_cuda_memory_usage")
-# optimizer_report= optimizer_prof.key_averages().table(sort_by="self_cuda_memory_usage")
-# backward_pass_report= backward_pass_prof.key_averages().table(sort_by="self_cuda_memory_usage")
-
-# log_file.write(f"Forward pass and Loss Calculation: \n\n {inference_report} \n\n")
-# log_file.write(f"Backward Pass: \n\n {backward_pass_report} \n\n")
-# log_file.write(f"Optimization step: \n\n {optimizer_report} \n\n")
-# log_file.close()  # Close the log file
+log_file.close()  # Close the log file
 
 # get minio client
 minio_client = cmd.get_minio_client(minio_access_key="v048BpXpWrsVIHUfdAix",
