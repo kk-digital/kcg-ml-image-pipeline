1168f3a376e86f426989f3506931c7cb2f84f82e
rachid2198 <rachid9821@gmail.com>
2024-04-03 23:27:55 +0100

    bug fix

--- kandinsky/kandinsky_train.py
+++ kandinsky/kandinsky_train.py
@@ -19,7 +19,6 @@ sys.path.insert(0, base_dir)
 sys.path.insert(0, os.getcwd())
 from kandinsky.model_paths import PRIOR_MODEL_PATH, DECODER_MODEL_PATH
 from utility.minio import cmd
-from torch.profiler import profile, record_function, ProfilerActivity
 
 # Set up file for logging
 log_file_path= "memory_usage_log.txt"
@@ -146,36 +145,45 @@ dataset = datasets.load_dataset('arrow', data_files={'train': 'input/pokemon-bli
 # Set the training transforms
 train_dataset = dataset["train"].with_transform(preprocess_train)
 
-def collate_fn(examples):
-    latents = torch.stack([example["latents"] for example in examples])
-    latents = latents.to(memory_format=torch.contiguous_format).float()
-    image_embeds = torch.stack([example["image_embeds"] for example in examples])
-    image_embeds = image_embeds.to(memory_format=torch.contiguous_format).float()
+def collate_fn(examples, vae, encoder, device, weight_dtype):
+    pixel_values = torch.stack([example["pixel_values"] for example in examples])
+    pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
+    clip_pixel_values = torch.stack([example["clip_pixel_values"] for example in examples])
+    clip_pixel_values = clip_pixel_values.to(memory_format=torch.contiguous_format).float()
+    
+    with torch.no_grad():
+        images = pixel_values.to(device, weight_dtype)
+        clip_images = clip_pixel_values.to(device, weight_dtype)
+        latents = vae.encode(images).latents
+        image_embeds = encoder(clip_images).image_embeds
+
     return {"latents": latents, "image_embeds": image_embeds}
 
 def encode_images(dataset, vae, image_encoder, device, weight_dtype):
     preprocessed_data = []
     for example in dataset:
-        images = example["pixel_values"].to(device, weight_dtype)
-        clip_images = example["clip_pixel_values"].to(device, weight_dtype)
-        latents = vae.encode(images).latents
-        image_embeds = image_encoder(clip_images).image_embeds
-        preprocessed_data.append({
-            "latents": latents,
-            "image_embeds": image_embeds
-        })
+        with torch.no_grad():
+            images = example["pixel_values"].to(device, weight_dtype)
+            clip_images = example["clip_pixel_values"].to(device, weight_dtype)
+            latents = vae.encode(images).latents
+            image_embeds = image_encoder(clip_images).image_embeds
+            preprocessed_data.append({
+                "latents": latents,
+                "image_embeds": image_embeds
+            })
     return preprocessed_data
 
 # Preprocess the entire dataset
-preprocessed_dataset = encode_images(train_dataset, vae, image_encoder, device, weight_dtype)
+# preprocessed_dataset = encode_images(train_dataset, vae, image_encoder, device, weight_dtype)
 
 train_dataloader = torch.utils.data.DataLoader(
-    preprocessed_dataset,
+    train_dataset,
     shuffle=True,
-    collate_fn=collate_fn,
+    collate_fn=lambda examples: collate_fn(examples, vae, image_encoder, device, weight_dtype),
     batch_size=train_batch_size,
     num_workers=dataloader_num_workers,
 )
+
 lr_scheduler = get_scheduler(
     lr_scheduler,
     optimizer=optimizer,
