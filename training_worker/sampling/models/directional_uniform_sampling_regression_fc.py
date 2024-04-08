6b826c81f6c8a9e9156ad044b7a4b8e8ae9e7024
rachid2198 <rachid9821@gmail.com>
2024-04-08 09:22:28 +0100

    adding an xgboost residual model

--- training_worker/sampling/models/directional_uniform_sampling_regression_fc.py
+++ training_worker/sampling/models/directional_uniform_sampling_regression_fc.py
@@ -5,6 +5,8 @@ import sys
 import tempfile
 from matplotlib import pyplot as plt
 import numpy as np
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
 import torch
 import time
 import torch.nn as nn
@@ -45,6 +47,348 @@ class DatasetLoader(Dataset):
         return sample_features, sample_label
 
 
+class DirectionalSamplingResidualXgboost(nn.Module):
+    def __init__(self, minio_client, input_size=2560, input_type="directional_uniform_sphere", 
+                 output_type="mean_sigma_score", dataset="environmental", dataloader=None):
+        
+        super(DirectionalSamplingResidualXgboost, self).__init__()
+        # set device
+        if torch.cuda.is_available():
+            device = 'cuda'
+        else:
+            device = 'cpu'
+        self._device = torch.device(device)
+
+        self.input_size= input_size
+        self.minio_client= minio_client
+        self.input_type= input_type
+        self.output_type= output_type
+        self.dataset=dataset
+
+        self.date = datetime.now().strftime("%Y_%m_%d")
+        self.local_path, self.minio_path =self.get_model_path()
+
+        # sphere dataloader
+        self.dataloader = dataloader
+
+    def get_model_path(self):
+        local_path=f"output/{self.output_type}_residual_xgboost_{self.input_type}.json"
+        minio_path=f"{self.dataset}/models/sampling/{self.date}_{self.output_type}_residual_xgboost_{self.input_type}.json"
+
+        return local_path, minio_path
+    
+    def train(self,
+              n_spheres, 
+              target_avg_points,
+              trained_model, 
+              validation_split, 
+              num_epochs, 
+              batch_size,
+              generate_every_epoch,
+              max_depth=7, 
+              min_child_weight=1,
+              gamma=0.01, 
+              subsample=1, 
+              colsample_bytree=1, 
+              eta=0.1,
+              early_stopping=50):
+
+        params = {
+            'objective': 'reg:absoluteerror',
+            "device": "cuda",
+            'max_depth': max_depth,
+            'min_child_weight': min_child_weight,
+            'gamma': gamma,
+            'subsample': subsample,
+            'colsample_bytree': colsample_bytree,
+            'eta': eta,
+            'eval_metric': 'mae'
+        }
+
+        # load datapoints from minio
+        if self.dataloader is None:
+            self.dataloader= DirectionalUniformSphereGenerator(self.minio_client, self.dataset)
+            self.dataloader.load_data()
+
+        # save loss for each epoch and features
+        train_loss=[]
+        val_loss=[]
+
+        best_val_loss = float('inf')  # Initialize best validation loss as infinity
+        best_train_loss = float('inf')  # Initialize best training loss as infinity
+        best_epoch= 0
+        start = time.time()        
+
+        # Training and Validation Loop
+        for epoch in range(num_epochs):
+
+            if(epoch==0 or generate_every_epoch):
+                # load inputs and targets
+                inputs, outputs = self.dataloader.generate_spheres(n_spheres, target_avg_points, self.output_type)
+
+                # calculate residuals
+                predicted_outputs= trained_model.predict(inputs, batch_size= batch_size).squeeze().cpu().numpy()
+                residuals= np.abs(np.array(outputs) - predicted_outputs)
+                outputs= residuals.tolist()
+
+            X_train, X_val, y_train, y_val = train_test_split(inputs, outputs, test_size=validation_split, shuffle=True)
+
+            dtrain = xgb.DMatrix(X_train, label=y_train)
+            dval = xgb.DMatrix(X_val, label=y_val)
+
+            evals_result = {}
+            
+            start = time.time()
+            self.model = xgb.train(params, dtrain, num_boost_round=100, evals=[(dval,'eval'), (dtrain,'train')], 
+                                    early_stopping_rounds=early_stopping, evals_result=evals_result)
+
+            end = time.time()
+
+            training_time= end - start
+    
+            #Extract MAE values and residuals
+            val_loss = evals_result['eval']['mae']
+            train_loss = evals_result['train']['mae']
+
+            # Update best model if current epoch's validation loss is the best
+            if val_loss[-1] < best_val_loss:
+                best_val_loss = val_loss[-1]
+                best_train_loss = train_loss[-1]
+                best_model_state = self.model
+                best_epoch= epoch + 1
+
+            print(f'Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss[-1]}, Val Loss: {val_loss[-1]}')
+        
+        self.model= best_model_state
+
+        start = time.time()
+        # Get predictions for validation set
+        val_preds = self.predict(X_val)
+
+        # Get predictions for training set
+        train_preds = self.predict(X_train)
+        end = time.time()
+        inference_speed=(len(X_train) + len(X_val))/(end - start)
+        print(f'Time taken for inference of {len(X_train) + len(X_val)} data points is: {end - start:.2f} seconds')
+
+        # Now you can calculate residuals using the predicted values
+        val_residuals = y_val - val_preds
+        train_residuals = y_train - train_preds
+        
+        self.save_graph_report(train_loss, val_loss, 
+                               best_epoch, generate_every_epoch,
+                               best_train_loss, best_val_loss, 
+                               val_residuals, train_residuals, 
+                               val_preds, y_val,
+                               len(X_train), len(X_val))
+        
+        self.save_model_report(num_training=len(X_train),
+                              num_validation=len(X_val),
+                              training_time=training_time, 
+                              train_loss=best_train_loss, 
+                              val_loss=best_val_loss,  
+                              inference_speed= inference_speed,
+                              model_params=params)
+
+    def save_model_report(self,num_training,
+                            num_validation,
+                            training_time, 
+                            train_loss, 
+                            val_loss, 
+                            inference_speed,
+                            model_params):
+        input_type="[input_clip_vector[1280], scaling_factors[1280]]"
+        output_type= f"{self.output_type}_residual"
+
+        report_text = (
+            "================ Model Report ==================\n"
+            f"Number of training datapoints: {num_training} \n"
+            f"Number of validation datapoints: {num_validation} \n"
+            f"Total training Time: {training_time:.2f} seconds\n"
+            "Loss Function: L1 \n"
+            f"Training Loss: {train_loss} \n"
+            f"Validation Loss: {val_loss} \n"
+            f"Inference Speed: {inference_speed:.2f} predictions per second\n\n"
+            "================ Input and output ==================\n"
+            f"Input: {input_type} \n"
+            f"Input Size: {self.input_size} \n" 
+            f"Output: {output_type} \n\n"
+        )
+
+        # Add model parameters to the report
+        for param, value in model_params.items():
+            report_text += f"{param}: {value}\n"
+
+        # Define the local file path for the report
+        local_report_path = 'output/model_report.txt'
+
+        # Save the report to a local file
+        with open(local_report_path, 'w') as report_file:
+            report_file.write(report_text)
+
+        # Read the contents of the local file
+        with open(local_report_path, 'rb') as file:
+            content = file.read()
+
+        # Upload the local file to MinIO
+        buffer = BytesIO(content)
+        buffer.seek(0)
+
+        cmd.upload_data(self.minio_client, 'datasets', self.minio_path.replace('.pth', '.txt'), buffer)
+
+        # Remove the temporary file
+        os.remove(local_report_path)
+
+    def save_graph_report(self, train_mae_per_round, val_mae_per_round,
+                          saved_at_epoch, generate_every_epoch,
+                          best_train_loss, best_val_loss,  
+                          val_residuals, train_residuals, 
+                          predicted_values, actual_values,
+                          training_size, validation_size):
+        fig, axs = plt.subplots(3, 2, figsize=(12, 10))
+        
+        output_type= f"{self.output_type}_residual"
+        #info text about the model
+        plt.figtext(0.02, 0.7, "Date = {}\n"
+                            "Dataset = {}\n"
+                            "Model type = {}\n"
+                            "Input type = {}\n"
+                            "Input shape = {}\n"
+                            "Output type= {}\n\n"
+                            ""
+                            "Training size = {}\n"
+                            "Validation size = {}\n"
+                            "Training loss = {:.4f}\n"
+                            "Validation loss = {:.4f}\n"
+                            "Saved at epoch = {}\n"
+                            "Generation policy = {}\n".format(self.date,
+                                                            self.dataset,
+                                                            'Fc_Network',
+                                                            self.input_type,
+                                                            self.input_size,
+                                                            output_type,
+                                                            training_size,
+                                                            validation_size,
+                                                            best_train_loss,
+                                                            best_val_loss,
+                                                            saved_at_epoch,
+                                                            "every epoch" if generate_every_epoch else "once"
+                                                            ))
+
+        # Plot validation and training Rmse vs. Rounds
+        axs[0][0].plot(range(1, len(train_mae_per_round) + 1), train_mae_per_round,'b', label='Training loss')
+        axs[0][0].plot(range(1, len(val_mae_per_round) + 1), val_mae_per_round,'r', label='Validation loss')
+        axs[0][0].set_title('MAE per Round')
+        axs[0][0].set_ylabel('Loss')
+        axs[0][0].set_xlabel('Rounds')
+        axs[0][0].legend(['Training loss', 'Validation loss'])
+
+        # Scatter Plot of actual values vs predicted values
+        axs[0][1].scatter(predicted_values, actual_values, color='green', alpha=0.5)
+        axs[0][1].set_title('Predicted values vs actual values')
+        axs[0][1].set_ylabel('True')
+        axs[0][1].set_xlabel('Predicted')
+
+        # plot histogram of training residuals
+        axs[1][0].hist(train_residuals, bins=30, color='blue', alpha=0.7)
+        axs[1][0].set_xlabel('Residuals')
+        axs[1][0].set_ylabel('Frequency')
+        axs[1][0].set_title('Training Residual Histogram')
+
+        # plot histogram of validation residuals
+        axs[1][1].hist(val_residuals, bins=30, color='blue', alpha=0.7)
+        axs[1][1].set_xlabel('Residuals')
+        axs[1][1].set_ylabel('Frequency')
+        axs[1][1].set_title('Validation Residual Histogram')
+        
+        # plot histogram of predicted values
+        axs[2][0].hist(predicted_values, bins=30, color='blue', alpha=0.7)
+        axs[2][0].set_xlabel('Predicted Values')
+        axs[2][0].set_ylabel('Frequency')
+        axs[2][0].set_title('Validation Predicted Values Histogram')
+        
+        # plot histogram of true values
+        axs[2][1].hist(actual_values, bins=30, color='blue', alpha=0.7)
+        axs[2][1].set_xlabel('Actual values')
+        axs[2][1].set_ylabel('Frequency')
+        axs[2][1].set_title('Validation True Values Histogram')
+
+        # Adjust spacing between subplots
+        plt.subplots_adjust(hspace=0.7, wspace=0.3, left=0.3)
+
+        plt.savefig(self.local_path.replace('.pth', '.png'))
+
+        # Save the figure to a file
+        buf = BytesIO()
+        plt.savefig(buf, format='png')
+        buf.seek(0)
+
+        # upload the graph report
+        cmd.upload_data(self.minio_client, 'datasets', self.minio_path.replace('.pth', '.png'), buf)  
+
+        # Clear the current figure
+        plt.clf()
+
+    def predict(self, data, batch_size=10000):
+        num_samples = len(data)
+        num_batches = int(np.ceil(num_samples / batch_size))
+
+        predictions = []
+
+        for i in range(num_batches):
+            start_idx = i * batch_size
+            end_idx = min((i + 1) * batch_size, num_samples)
+
+            batch_data = data[start_idx:end_idx]
+
+            # Get predictions for the current batch
+            batch_preds = self.model.predict(xgb.DMatrix(batch_data))
+
+            predictions.extend(batch_preds)
+
+        return np.array(predictions)
+
+    def load_model(self):
+        prefix= f"{self.dataset}/models/sampling/"
+        model_suffix= f"_{self.output_type}_residual_xgboost_{self.input_type}.json"
+        # get model file data from MinIO
+        model_files=cmd.get_list_of_objects_with_prefix(self.minio_client, 'datasets', prefix)
+        most_recent_model = None
+
+        for model_file in model_files:
+            if model_file.endswith(model_suffix):
+                most_recent_model = model_file
+
+        if most_recent_model:
+            model_file_data =cmd.get_file_from_minio(self.minio_client, 'datasets', most_recent_model)
+        else:
+            print("No .json files found in the list.")
+            return
+        
+        print(most_recent_model)
+
+        # Create a temporary file and write the downloaded content into it
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            for data in model_file_data.stream(amt=8192):
+                temp_file.write(data)
+
+        # Load the model from the downloaded bytes
+        self.model = xgb.Booster(model_file=temp_file.name)
+        self.model.set_param({"device": "cuda"})
+        
+        # Remove the temporary file
+        os.remove(temp_file.name)
+
+    def save_model(self):
+        self.model.save_model(self.local_path)
+        
+        #Read the contents of the saved model file
+        with open(self.local_path, "rb") as model_file:
+            model_bytes = model_file.read()
+
+        # Upload the model to MinIO
+        cmd.upload_data(self.minio_client, 'datasets', self.minio_path, BytesIO(model_bytes))
+    
 class DirectionalSamplingFCResidualNetwork(nn.Module):
     def __init__(self, minio_client, input_size=2560, hidden_sizes=[512], input_type="directional_uniform_sphere",
                  output_size=1, output_type="mean_sigma_score", dataset="environmental", dataloader=None):
@@ -645,7 +989,7 @@ class DirectionalSamplingFCRegressionNetwork(nn.Module):
 
         if(train_residual_model):
             print("training a residual model----------------")
-            residual_model= DirectionalSamplingFCResidualNetwork(minio_client=self.minio_client,
+            residual_model= DirectionalSamplingResidualXgboost(minio_client=self.minio_client,
                                                                  input_size= self.input_size,
                                                                  input_type= self.input_type,
                                                                  output_type= self.output_type,
@@ -654,7 +998,6 @@ class DirectionalSamplingFCRegressionNetwork(nn.Module):
             residual_model.train(n_spheres, 
                                 target_avg_points,
                                 self,
-                                learning_rate,
                                 validation_split, 
                                 num_epochs, 
                                 batch_size,
