fb5516270daf946e2bf893cff9f87fc34f1b8050
Amine-AH <md.amine.ah@gmail.com>
2024-04-10 16:34:16 +0100

    Combaring the EBM to ELM, checking some issues with the ELM classify function

--- scripts/train_ebm_on_vae_embeddings.py
+++ scripts/train_ebm_on_vae_embeddings.py
@@ -453,8 +453,10 @@ class Clip_NN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(Clip_NN, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, output_size)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         # Flatten the input tensor if it's not already flattened
@@ -462,6 +464,10 @@ class Clip_NN(nn.Module):
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        x = self.relu(x)
+        x = self.fc4(x)
         return x
 class DeepEnergyModel(pl.LightningModule):
 
