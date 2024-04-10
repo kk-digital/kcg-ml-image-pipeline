36f6662bca8a2803f3155b560f7596bd3d1afdc2
Amine-AH <md.amine.ah@gmail.com>
2024-04-10 20:22:20 +0100

    Combaring the EBM to ELM, checking some issues with the ELM classify function

--- scripts/train_ebm_on_vae_embeddings.py
+++ scripts/train_ebm_on_vae_embeddings.py
@@ -447,27 +447,20 @@ def get_dataset_from_id(id_class,data_augment_passes,label_value):
 # ------------------------------------------------- Neural Network Architecture ---------------------------------------
 # ---------------------------------------------------------------------------------------------------------------------
 class Clip_NN(nn.Module):
-    def __init__(self, input_size,hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size):
         super(Clip_NN, self).__init__()
-        self.conv1 = nn.Conv2d(input_size, 16, kernel_size=3, padding=1)
-        self.relu1 = nn.ReLU()
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.relu2 = nn.ReLU()
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc = nn.Linear(32 * 16 * 16, output_size)
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.pool2(x)
+        # Flatten the input tensor if it's not already flattened
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
         return x
-
+    
 
 # class Clip_NN(nn.Module):
 #     def __init__(self, input_size, hidden_size, output_size):
@@ -557,7 +550,7 @@ class DeepEnergyModel(pl.LightningModule):
         super().__init__()
         self.save_hyperparameters()
         #self.cnn = Clip_NN(input_size= 64 * 64 * 4, hidden_size = 512, output_size =1) 
-        self.cnn = Clip_NN(input_size=4, hidden_size=128, output_size=1)
+        self.cnn = Clip_NN(input_size=64 * 64 * 4, hidden_size=512, output_size=1)
         self.example_input_array = torch.zeros(1, *img_shape)
 
     def forward(self, x):
