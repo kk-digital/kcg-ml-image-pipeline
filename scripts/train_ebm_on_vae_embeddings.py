5fde79f9514e8bca96b3eaa99250866b14027e32
Amine-AH <md.amine.ah@gmail.com>
2024-04-10 19:54:11 +0100

    Combaring the EBM to ELM, checking some issues with the ELM classify function

--- scripts/train_ebm_on_vae_embeddings.py
+++ scripts/train_ebm_on_vae_embeddings.py
@@ -447,36 +447,14 @@ def get_dataset_from_id(id_class,data_augment_passes,label_value):
 # ------------------------------------------------- Neural Network Architecture ---------------------------------------
 # ---------------------------------------------------------------------------------------------------------------------
 
-class Clip_NN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(Clip_NN, self).__init__()
-        self.conv1 = nn.Conv3d(in_channels=input_size, out_channels=hidden_size, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool1 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
-        self.conv2 = nn.Conv3d(in_channels=hidden_size, out_channels=hidden_size, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
-        self.fc1 = nn.Linear(256, hidden_size)  # Adjusted for input shape (1, 64, 64, 4)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.pool2(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        return x
-
 # class Clip_NN(nn.Module):
-#     def __init__(self, input_size, hidden_size, output_size,):
+#     def __init__(self, input_size, hidden_size, output_size):
 #         super(Clip_NN, self).__init__()
-#         self.conv1 = nn.Conv2d(input_size, hidden_size, kernel_size=3, padding=1)
-#         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-#         self.conv2 = nn.Conv2d(hidden_size, hidden_size, kernel_size=3, padding=1)
-#         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-#         self.fc1 = nn.Linear(hidden_size * 16 * 16, hidden_size)
+#         self.conv1 = nn.Conv3d(in_channels=input_size, out_channels=hidden_size, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+#         self.pool1 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+#         self.conv2 = nn.Conv3d(in_channels=hidden_size, out_channels=hidden_size, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+#         self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+#         self.fc1 = nn.Linear(256, hidden_size)  # Adjusted for input shape (1, 64, 64, 4)
 #         self.relu = nn.ReLU()
 #         self.fc2 = nn.Linear(hidden_size, output_size)
 
@@ -490,6 +468,28 @@ class Clip_NN(nn.Module):
 #         x = self.relu(x)
 #         x = self.fc2(x)
 #         return x
+
+class Clip_NN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size,):
+        super(Clip_NN, self).__init__()
+        self.conv1 = nn.Conv2d(input_size, hidden_size, kernel_size=3, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(hidden_size, hidden_size, kernel_size=3, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(hidden_size * 16 * 16, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
     
 
 # class Clip_NN(nn.Module):
