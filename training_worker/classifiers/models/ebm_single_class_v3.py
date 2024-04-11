8828defbe061f8d286834892d8a10781817f2b3b
Amine-AH <md.amine.ah@gmail.com>
2024-04-11 13:56:53 +0100

    Combaring the EBM to ELM, checking some issues with the ELM classify function

--- training_worker/classifiers/models/ebm_single_class_v3.py
+++ training_worker/classifiers/models/ebm_single_class_v3.py
@@ -992,7 +992,7 @@ score = original_model.evalute_energy(clip_h_vector)
 from training_worker.classifiers.models.elm_regression import ELMRegression
 elm_model = ELMRegression()
 #def load_model(self, minio_client, model_dataset, tag_name, model_type, scoring_model, not_include, device=None):
-elm_model.load_model(minio_client = minio_client, model_dataset = "environmental",scoring_model = 'score' ,tag_name = "topic-desert", model_type = "regression-clip-h", not_include= 'batatatatatata', device= original_model.device)
+elm_model.load_model(minio_client = minio_client, model_dataset = "environmental",scoring_model = 'score' ,tag_name = "topic-desert", model_type = "elm-regression-clip-h", not_include= 'batatatatatata', device= original_model.device)
 
 
 
