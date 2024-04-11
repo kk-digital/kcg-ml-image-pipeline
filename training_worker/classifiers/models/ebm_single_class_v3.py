59ce053a48c38dd4d10096ab6b68421c3eef95dc
Amine-AH <md.amine.ah@gmail.com>
2024-04-11 16:40:30 +0100

    Combaring the EBM to ELM, checking some issues with the ELM classify function

--- training_worker/classifiers/models/ebm_single_class_v3.py
+++ training_worker/classifiers/models/ebm_single_class_v3.py
@@ -1158,7 +1158,7 @@ def process_and_sort_dataset(images_paths, model):
         
         # Compute the score by passing the image tensor through the model
         # Ensure the tensor is in the correct shape, device, etc.
-        score = model.cnn(embedding.unsqueeze(0).to(model.device)).cpu()
+        score = model.evalute_energy(embedding).cpu()
         
         # Append the path, embedding, and score as a tuple to the structure list
         structure.append((image_path, embedding, score.item(),image))  # Assuming score is a tensor, use .item() to get the value
@@ -1210,7 +1210,7 @@ def process_and_sort_dataset(images_paths, model):
         
         # Compute the score by passing the image tensor through the model
         # Ensure the tensor is in the correct shape, device, etc.
-        score = model.cnn(embedding.unsqueeze(0).to(model.device)).cpu()
+        score = model.evalute_energy(embedding).cpu()
         
         # Append the path, embedding, and score as a tuple to the structure list
         structure.append((image_path, embedding, score.item(),image))  # Assuming score is a tensor, use .item() to get the value
