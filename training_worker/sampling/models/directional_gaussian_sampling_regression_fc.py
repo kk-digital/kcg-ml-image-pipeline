9a174c96a5861ec0eadc199d54434cb1c849a4ea
charles_lee <lucky.goldragon@gmail.com>
2024-04-11 10:01:43 -0400

    fix bug

--- training_worker/sampling/models/directional_gaussian_sampling_regression_fc.py
+++ training_worker/sampling/models/directional_gaussian_sampling_regression_fc.py
@@ -706,8 +706,8 @@ class DirectionalSamplingResidualXGBoost(nn.Module):
         }
 
         if trained_model is None:
-            self.trained_model = DirectionalSamplingFCRegressionNetwork(minio_client=self.minio_client, dataset=self.dataset)
-            self.trained_model.load_model()
+            trained_model = DirectionalSamplingFCRegressionNetwork(minio_client=self.minio_client, dataset=self.dataset)
+            trained_model.load_model()
 
         # load datapoints from minio
         if self.dataloader is None:
