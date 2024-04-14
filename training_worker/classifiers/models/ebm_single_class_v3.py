1d21b205c820949c14ba3da1b8aeb7375eb67163
Amine-AH <md.amine.ah@gmail.com>
2024-04-14 15:17:53 +0100

    Combaring the EBM to ELM, checking some issues with the ELM classify function

--- training_worker/classifiers/models/ebm_single_class_v3.py
+++ training_worker/classifiers/models/ebm_single_class_v3.py
@@ -1301,7 +1301,7 @@ original_model=EBM_Single_Class(minio_access_key=args.minio_access_key,
 #original_model = EBM_Single_Class(train_loader = None,val_loader = None, adv_loader = None,img_shape=(1280,))
 # Load the last occult trained model
 
-tag_name_x = "perspective-isometric "#"topic-desert"
+tag_name_x = "defect-only-a-texture" #"topic-desert"
 original_model.load_model_from_minio(minio_client, dataset_name = "environmental", tag_name =tag_name_x, model_type = "energy-based-model")
 
 
