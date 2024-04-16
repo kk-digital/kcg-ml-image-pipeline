a4fd508143ad6a65fb8dcf5551d32d07d912cde8
rachid2198 <rachid9821@gmail.com>
2024-04-16 16:25:55 +0100

    testing

--- training_worker/scoring/scripts/calculate_clip_h_distribution.py
+++ training_worker/scoring/scripts/calculate_clip_h_distribution.py
@@ -48,6 +48,7 @@ def main():
         jobs_list= get_job_list(dataset=dataset)
         clip_vectors=[]
 
+        index=0
         print(f"fetching image clip files for {dataset}...........")
         for job in tqdm(jobs_list):
             try:
@@ -61,9 +62,16 @@ def main():
                 clip_vectors.append(clip_vector[0])
             except Exception as e:
                 print(f"an error occured: {e}")
+            
+            index+=1
 
+            if index>100:
+                break
+       
         # Convert list of vectors into a numpy array for easier computation
         clip_vectors_np = np.array(clip_vectors)
+
+        print(clip_vectors_np.shape)
 
         # Calculate mean and std for each feature
         mean_vector = np.mean(clip_vectors_np, axis=0)
@@ -84,16 +92,16 @@ def main():
             "cov_matrix": covariance_matrix.tolist()
         }
 
-        stats_msgpack = msgpack.packb(stats)
+        # stats_msgpack = msgpack.packb(stats)
 
-        data = BytesIO()
-        data.write(stats_msgpack)
-        data.seek(0)
+        # data = BytesIO()
+        # data.write(stats_msgpack)
+        # data.seek(0)
 
-        # Storing stats in MinIO
-        bucket_name = "datasets"
-        output_path = f"{dataset}/output/stats/clip_stats.msgpack"
-        cmd.upload_data(minio_client, bucket_name, output_path, data)
+        # # Storing stats in MinIO
+        # bucket_name = "datasets"
+        # output_path = f"{dataset}/output/stats/clip_stats.msgpack"
+        # cmd.upload_data(minio_client, bucket_name, output_path, data)
 
 if __name__ == '__main__':
     main()
