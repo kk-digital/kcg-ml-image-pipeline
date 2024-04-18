e0e9f4a53dc26c85a5989ac9cf69d9844581a5a2
rachid2198 <rachid9821@gmail.com>
2024-04-18 04:13:24 +0100

    bug fix

--- utility/http/request.py
+++ utility/http/request.py
@@ -475,9 +475,9 @@ def http_get_random_image_by_date(dataset, size, start_date=None, end_date=None)
         endpoint_url+= f"&end_date={end_date}"
 
     url = SERVER_ADDRESS + endpoint_url
+    print(url)
     try:
         response = requests.get(url)
-        print(response)
 
         if response.status_code == 200:
             data_json = response.json()
