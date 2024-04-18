43b10d128b5e0c9393dc5c25c9e8011e5873f6b9
Amine-AH <md.amine.ah@gmail.com>
2024-04-19 00:19:11 +0100

    Refactoring the tagging script, fixing a created time bug

--- training_worker/classifiers/models/ebm_single_class_v3.py
+++ training_worker/classifiers/models/ebm_single_class_v3.py
@@ -414,7 +414,7 @@ class EBM_Single_Class:
 
         # minimize dataset
 
-        target_paths = random.sample(target_paths, 120)
+        target_paths = random.sample(target_paths, 240)
         
         # minimize dataset
 
