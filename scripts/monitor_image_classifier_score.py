37b0dbf720e302a48b98076a848da70bf656bb74
charles_lee <lucky.goldragon@gmail.com>
2024-04-21 10:47:30 -0400

    fix bug

--- scripts/monitor_image_classifier_score.py
+++ scripts/monitor_image_classifier_score.py
@@ -432,7 +432,7 @@ def run_image_scorer(minio_client,
         return
     
     start_time = time.time()
-    paths = scorer.get_paths()
+    paths = scorer.get_paths_from_monodb()
     end_time = time.time()
     time_elapsed_to_get_paths = end_time - start_time
     print("Getting paths of clip vector is done! Time elapsed = ", time_elapsed_to_get_paths)
