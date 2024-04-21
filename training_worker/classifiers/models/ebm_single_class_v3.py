dec492c8fff17437bd969ecb305feb40d1a02486
Amine-AH <md.amine.ah@gmail.com>
2024-04-21 16:38:28 +0100

    Plotting Rank VS Score graphs elm vs ebm

--- training_worker/classifiers/models/ebm_single_class_v3.py
+++ training_worker/classifiers/models/ebm_single_class_v3.py
@@ -414,7 +414,7 @@ class EBM_Single_Class:
 
         # minimize dataset
 
-        target_paths = random.sample(target_paths, 240)
+        target_paths = random.sample(target_paths, 64)
         
         # minimize dataset
 
