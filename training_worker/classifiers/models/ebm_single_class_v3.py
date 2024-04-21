a5aa13b61bf72f94f7d21f12472e8b8554e311f4
Amine-AH <md.amine.ah@gmail.com>
2024-04-21 18:29:06 +0100

    Plotting Rank VS Score graphs elm vs ebm

--- training_worker/classifiers/models/ebm_single_class_v3.py
+++ training_worker/classifiers/models/ebm_single_class_v3.py
@@ -1599,7 +1599,7 @@ original_model=EBM_Single_Class(minio_access_key=args.minio_access_key,
 #original_model = EBM_Single_Class(train_loader = None,val_loader = None, adv_loader = None,img_shape=(1280,))
 # Load the last occult trained model
 
-tag_name_x = "defect-color-over-saturated" #"defect-color-mildly-over-saturated" # "defect-color-over-saturated"  #"topic-forest" #"concept-occult" #"concept-cybernetic"  #"defect-color-too-dark" #"content-has-waifu" #"concept-occult" #"topic-aquatic" #"topic-aquatic" #"topic-desert"
+tag_name_x =  "concept-nature" # "defect-color-over-saturated" #"defect-color-mildly-over-saturated" # "defect-color-over-saturated"  #"topic-forest" #"concept-occult" #"concept-cybernetic"  #"defect-color-too-dark" #"content-has-waifu" #"concept-occult" #"topic-aquatic" #"topic-aquatic" #"topic-desert"
 original_model.load_model_from_minio(minio_client, dataset_name = "environmental", tag_name =tag_name_x, model_type = "energy-based-model")
 
 
@@ -1627,30 +1627,36 @@ elm_model, _ = load_model_elm(device = original_model.device, minio_client = min
 #plot_samples_hashless_binning(loaded_model = elm_model, dataset_name = "environmental", number_of_samples = 32000,tag_name =tag_name_x)
 
 # Graphs
-#plot_samples_graph(loaded_model = original_model, dataset_name = "environmental", number_of_samples = 40000,tag_name =tag_name_x)
+plot_samples_graph(loaded_model = original_model, dataset_name = "environmental", number_of_samples = 40000,tag_name =tag_name_x)
 #plot_samples_graph(loaded_model = elm_model, dataset_name = "environmental", number_of_samples = 40000,tag_name =tag_name_x)
 ############################ Train ########################
 
 
-tag_name_x_2 = "concept-cybernetic" #"concept-nature"
-defect_test=EBM_Single_Class(minio_access_key="D6ybtPLyUrca5IdZfCIM",
-                            minio_secret_key= "2LZ6pqIGOiZGcjPTR6DZPlElWBkRTkaLkyLIBt4V",
-                            dataset= "environmental",
-                            class_name= tag_name_x_2,
-                            model = None,
-                            save_name = "bla",
-                            class_id =  get_tag_id_by_name("defect-color-over-saturated"),
-                            training_batch_size=64,
-                            num_samples= 32000,
-                            epochs= 20,
-                            learning_rate= 0.001)
+# tag_name_x_2 = "concept-cybernetic" #"concept-nature"
+# defect_test=EBM_Single_Class(minio_access_key="D6ybtPLyUrca5IdZfCIM",
+#                             minio_secret_key= "2LZ6pqIGOiZGcjPTR6DZPlElWBkRTkaLkyLIBt4V",
+#                             dataset= "environmental",
+#                             class_name= tag_name_x_2,
+#                             model = None,
+#                             save_name = "bla",
+#                             class_id =  get_tag_id_by_name("defect-color-over-saturated"),
+#                             training_batch_size=64,
+#                             num_samples= 32000,
+#                             epochs= 20,
+#                             learning_rate= 0.001)
 
 
-defect_test.train_v2()
+# defect_test.train_v2()
 
 
-defect_test.load_model_from_minio(minio_client , dataset_name = "environmental", tag_name =tag_name_x_2, model_type = tag_name_x_2)
-plot_samples_hashless(loaded_model = defect_test, dataset_name = "environmental", number_of_samples = 40000,tag_name =tag_name_x_2)
+# defect_test.load_model_from_minio(minio_client , dataset_name = "environmental", tag_name =tag_name_x_2, model_type = tag_name_x_2)
+# plot_samples_hashless(loaded_model = defect_test, dataset_name = "environmental", number_of_samples = 40000,tag_name =tag_name_x_2)
+
+
+
+
+
+
 
 
 
