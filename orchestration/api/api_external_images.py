2f3292a6ddb2a10b8630fe86c94ee96bace27adf
charles_lee <lucky.goldragon@gmail.com>
2024-04-23 14:53:25 -0400

    add get api to get image data list

--- orchestration/api/api_external_images.py
+++ orchestration/api/api_external_images.py
@@ -91,6 +91,64 @@ async def add_external_image_data_list(request: Request, image_data_list: List[E
             http_status_code=500
         )
     
+@router.get("/external-images/get-external-image-list", 
+            description="Delete an external image data",
+            tags=["external-images"],  
+            response_model=StandardSuccessResponseV1[List[ExternalImageData]],  
+            responses=ApiResponseHandlerV1.listErrors([404, 500]))
+async def get_external_image_data_list(request: Request, image_hash_list: List[str]):
+    api_response_handler = await ApiResponseHandlerV1.createInstance(request)
+    try:
+        list_external_images = []
+        for image_hash in image_hash_list:
+
+            result = request.app.external_images_collection.find_one({
+                "image_hash": image_hash
+            }, {"_id": 0})
+        
+            if result is not None:
+                list_external_images.append(result)
+
+        return api_response_handler.create_success_response_v1(
+            response_data={"data": list_external_images},
+            http_status_code=200  
+        )
+    
+    except Exception as e:
+        return api_response_handler.create_error_response_v1(
+            error_code=ErrorCode.OTHER_ERROR, 
+            error_string=str(e),
+            http_status_code=500
+        )
+    
+@router.get("/external-images/get-all-external-image-list", 
+            description="Delete an external image data",
+            tags=["external-images"],  
+            response_model=StandardSuccessResponseV1[List[ExternalImageData]],  
+            responses=ApiResponseHandlerV1.listErrors([404, 500]))
+async def get_all_external_image_data_list(request: Request):
+    api_response_handler = await ApiResponseHandlerV1.createInstance(request)
+    try:
+
+        all_image_data_list = list(request.app.external_images_collection.find_one({}, {"_id": 0}))
+    
+        list_external_images = []
+        for image_data in all_image_data_list:
+            list_external_images.append(image_data)
+
+        return api_response_handler.create_success_response_v1(
+            response_data={"data": list_external_images},
+            http_status_code=200  
+        )
+    
+    except Exception as e:
+        return api_response_handler.create_error_response_v1(
+            error_code=ErrorCode.OTHER_ERROR, 
+            error_string=str(e),
+            http_status_code=500
+        )
+
+
 @router.delete("/external-images/delete-external-image", 
             description="Delete an external image data",
             tags=["external-images"],  
