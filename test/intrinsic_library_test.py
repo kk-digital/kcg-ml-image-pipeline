ed992107a2db88ca2b0b065b739a3a9ad3dab836
charles_lee <lucky.goldragon@gmail.com>
2024-04-25 00:39:27 -0400

    fix bug

--- test/intrinsic_library_test.py
+++ test/intrinsic_library_test.py
@@ -7,9 +7,10 @@ base_dir = "./"
 sys.path.insert(0, base_dir)
 sys.path.insert(0, os.getcwd())
 
-from data_loader.kandinsky_dataset_loader import KandinskyDatasetLoader
 from utility.minio import cmd
 from utility.http import request
+from utility.path import separate_bucket_and_file_path
+from data_loader.utils import get_object
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -21,6 +22,41 @@ def parse_args():
     parser.add_argument('--num-vectors', type=int, default=1024, help="Number of vectors to get intrinsic dimensions")
     return parser.parse_args()
 
+def load_vectors(minio_client, dataset_names, vector_type="clip",  limit=1024):
+    num_loaded_vectors = 0
+
+    for dataset_name in dataset_names:
+
+        if num_loaded_vectors >= limit:
+            break
+
+        jobs = request.http_get_completed_job_by_dataset(dataset=dataset_name)
+        feature_vectors = []
+        for job in jobs:
+
+            if num_loaded_vectors >= limit:
+                break
+            
+            path = job.get("task_output_file_dict", {}).get("output_file_path")
+            if path is not None:
+                if vector_type == "clip":
+                    path = path.replace(".jpg", "_embedding.msgpack")
+                elif vector_type == "vae":
+                    path = path.replace(".jpg", "_vae_latent.msgpack")
+                bucket, features_vector_path = separate_bucket_and_file_path(path)
+                try:
+                    feature_vector = get_object(minio_client, features_vector_path)
+                    feature_vectors.append(feature_vector)
+
+                    num_loaded_vectors += 1
+                except Exception as e:
+                    print("Error in loading feature vector: {}, {}".format(path, e))
+
+    return feature_vectors
+        
+            
+
+
 def main():
     args = parse_args()
 
@@ -29,32 +65,19 @@ def main():
                                         minio_secret_key=args.minio_secret_key,
                                         minio_ip_addr=args.minio_addr)
     dataset_names = request.http_get_dataset_names()
+    
+    test_clip_vector_num_list = [args.num_vectors]
+    max_test_clip_vector_num = max(test_clip_vector_num_list)
+
     all_feature_vectors = []
+    
+    all_feature_vectors = load_vectors(minio_client=minio_client, dataset_names=dataset_names, vector_type=args.data_type, limit=max_test_clip_vector_num)
 
-    list_clip_vector_num = [args.num_vectors]
-
-    for dataset_name in dataset_names[0]:
-        try:
-            dataloader = KandinskyDatasetLoader(minio_client=minio_client, dataset=dataset_name)
-        except Exception as e:
-            print("Error in initializing kankinsky dataset loader:{}".format(e))
-            continue
-
-        if args.data_type == "clip":
-            feature_vectors, _= dataloader.load_clip_vector_data(limit=max(list_clip_vector_num))
-        elif args.data_type == "vae":
-            feature_vectors = dataloader.load_latents(limit=max(list_clip_vector_num))
-        else:
-            print("No support data type {}".format(args.data_type))
-            return None
-        all_feature_vectors.extend(feature_vectors)
-        if len(all_feature_vectors) >= max(list_clip_vector_num):
-            break
     result = []
 
     print("length", len(all_feature_vectors))
 
-    for clip_vector_num in list_clip_vector_num:
+    for clip_vector_num in test_clip_vector_num_list:
         data = torch.tensor(all_feature_vectors[:clip_vector_num])
         print("shape", data.size())
         d1 = mle_id(data, k=2)
