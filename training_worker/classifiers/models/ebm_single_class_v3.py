0ef1382dd44273394b7e31ef57ae19debb8b8a23
Amine-AH <md.amine.ah@gmail.com>
2024-04-28 21:18:11 +0100

    Reftraining the models that have been affected by the clamping

--- training_worker/classifiers/models/ebm_single_class_v3.py
+++ training_worker/classifiers/models/ebm_single_class_v3.py
@@ -1838,7 +1838,7 @@ elm_model, _ = load_model_elm(device = original_model.device, minio_client = min
 
 model_list = []
 
-model_1_name =  "concept-cybernetic"  #  "topic-forest" "topic-desert" "topic-aquatic" "concept-cybernetic" "concept-nature" 
+model_1_name =  "topic-medieval"  #  "topic-forest" "topic-desert" "topic-aquatic" "concept-cybernetic" "concept-nature" 
 model_1=EBM_Single_Class(minio_access_key=args.minio_access_key,
                             minio_secret_key=args.minio_secret_key,
                             dataset= args.dataset,
