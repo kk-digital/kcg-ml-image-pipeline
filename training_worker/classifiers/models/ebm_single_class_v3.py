29e5cefd957541c3218ac3d69391c58327375e65
Amine-AH <md.amine.ah@gmail.com>
2024-04-28 17:53:00 +0100

    Reftraining the models that have been affected by the clamping

--- training_worker/classifiers/models/ebm_single_class_v3.py
+++ training_worker/classifiers/models/ebm_single_class_v3.py
@@ -1810,7 +1810,7 @@ elm_model, _ = load_model_elm(device = original_model.device, minio_client = min
 ############################ Train ########################
 
 
-tag_name_x_2 =  "perspective-isometric" #"perspective-isometric"  # "perspective-3d"  #"concept-cybernetic" #"concept-nature"
+tag_name_x_2 =  "concept-cybernetic" #"perspective-isometric"  # "perspective-3d"  #"concept-cybernetic" #"concept-nature"
 defect_test=EBM_Single_Class(minio_access_key="D6ybtPLyUrca5IdZfCIM",
                             minio_secret_key= "2LZ6pqIGOiZGcjPTR6DZPlElWBkRTkaLkyLIBt4V",
                             dataset= "environmental",
