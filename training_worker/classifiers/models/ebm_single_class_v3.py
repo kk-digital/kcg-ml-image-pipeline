6b0ba1e3b7da7d904f8d581e74e6726bbb44aba2
Amine-AH <md.amine.ah@gmail.com>
2024-04-29 14:20:42 +0100

    Reftraining the models that have been affected by the clamping

--- training_worker/classifiers/models/ebm_single_class_v3.py
+++ training_worker/classifiers/models/ebm_single_class_v3.py
@@ -1709,6 +1709,7 @@ def plot_samples_graph_interpolation_plus_mapping(loaded_model,dataset_name, num
         bin_end = bins[bin_idx]
         # Map scores in each bin from +x to -x to 1 to -1
         mapping_function = lambda score: piecewise_linear(score, bin_start, 1, bin_end, -1)
+        print(f'the bin {bin_idx} values is {mapping_function}')
         mapping_functions.append(mapping_function)
 
     # Debugging prints
