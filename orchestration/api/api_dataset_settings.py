27e134437aef964a101cf8406d3861f3d00fd801
ssss <sdevdariani9@gmail.com>
2024-04-29 13:13:00 +0400

    update dataset apis

--- orchestration/api/api_dataset_settings.py
+++ orchestration/api/api_dataset_settings.py
@@ -180,6 +180,18 @@ async def set_dataset_config(request: Request, config: DatasetConfig):
     response_handler = await ApiResponseHandlerV1.createInstance(request)
 
     try:
+
+        dataset_exists = cmd.get_list_of_objects(request.app.minio_client, "datasets")
+        dataset_path = f"{config.dataset_name}/"
+
+        if not any(obj.object_name.startswith(dataset_path) for obj in dataset_exists):
+            # Return 422 error if dataset does not exist
+            return response_handler.create_error_response_v1(
+                error_code=ErrorCode.INVALID_PARAMS,
+                error_string=f"Dataset '{config.dataset_name}' does not exist.",
+                http_status_code=422,
+            )
+        
         # Ensure the dataset exists
         query = {"dataset_name": config.dataset_name}
         item = request.app.dataset_config_collection.find_one(query)
