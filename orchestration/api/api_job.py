e06aace7d95803545d8c14ca1176fc0d2b2049e3
charles_lee <lucky.goldragon@gmail.com>
2024-04-29 02:41:22 -0400

    test

--- orchestration/api/api_job.py
+++ orchestration/api/api_job.py
@@ -546,7 +546,7 @@ def update_completed_jobs_for_safe_delete(request: Request):
         image_hash = completed_job["task_output_file_dict"]["output_file_hash"]
         tag_list_response = get_tag_list_for_image_v1(request, image_hash)
         print(tag_list_response)
-        
+        return None
         ranking_list_response = get_image_rank_use_count_v1(request, image_hash)
         try:
             if tag_list_response["request_error_code"] == 0 and ranking_list_response["request_error_code"] == 0:
