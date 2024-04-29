4e516eb033ba4dac7341a39c7436e06839fcea6d
charles_lee <lucky.goldragon@gmail.com>
2024-04-29 02:35:07 -0400

    fix bug

--- orchestration/api/api_job.py
+++ orchestration/api/api_job.py
@@ -544,7 +544,8 @@ def update_completed_jobs_for_safe_delete(request: Request):
     for completed_job in completed_jobs:
         task_uuid = completed_job["uuid"]
         image_hash = completed_job["task_output_file_dict"]["output_file_hash"]
-        tag_list_response = get_tag_list_for_image_v1(request, image_hash).content
+        tag_list_response = json.loads(get_tag_list_for_image_v1(request, image_hash))
+        print(tag_list_response)
         
         ranking_list_response = get_image_rank_use_count_v1(request, image_hash).content
         try:
