857197de14c7984fd938773ee8e422c59dfad669
charles_lee <lucky.goldragon@gmail.com>
2024-04-29 16:51:13 -0400

    add test OMP

---
+++ test/test_omp/main.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+# feature selection library
+from sklearn.linear_model import OrthogonalMatchingPursuit
+
+# intrinsic library
+import skdim
+
+# clip vector loader which loads data from memory mapping of numpy
+from load_clip_vector_over_sigma_score.clip_vector_loader import get_clip_0_sigma
+
+# utils
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--count', type=int, default=10000, help='count of clip vectors')
+
+    return parser.parse_args()
+
+def main():
+    clip_vectors, scores = get_clip_0_sigma()
+
+    n_features = round(skdim.id.TwoNN().fit(clip_vectors).dimension_)
+
+    # Create the OMP feature selector
+    omp = OrthogonalMatchingPursuit(n_nonzero_coefs=n_features)
+
+    # Fit the OMP feature selector to the data
+    omp.fit(clip_vectors, scores)
+
+    clip_vectors[:, np.argsort(-np.abs(omp.coef_))[n_features:]] = 0
+
+    print(clip_vectors[:2])
+
+
+if __name__ == '__main__':
+    main()
