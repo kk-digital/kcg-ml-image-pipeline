55e11412a3d76c4c1151f02ede6e74d97c16330c
Amine-AH <md.amine.ah@gmail.com>
2024-05-01 22:17:13 +0100

    Reftraining the models that have been affected by the clamping

--- training_worker/classifiers/models/ebm_single_class_v3.py
+++ training_worker/classifiers/models/ebm_single_class_v3.py
@@ -2594,7 +2594,7 @@ tag_name = tag_name_combined
 sorted_images_and_hashes = plot_samples_graph_interpolation_plus_mapping_combined(loaded_model = model_1,
                                                         loaded_model_2 = model_2,
                                                           dataset_name = "environmental",
-                                                            number_of_samples = 2048 ,
+                                                            number_of_samples = 10000 ,
                                                             tag_name =tag_name_x,
                                                               model_type = "EBM Model" )
 # rank = 1
