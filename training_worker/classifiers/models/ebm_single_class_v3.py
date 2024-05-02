5fb51453cca646f6c513e0492afbf9af2c46196c
Amine-AH <md.amine.ah@gmail.com>
2024-05-02 20:03:16 +0100

    Reftraining the models that have been affected by the clamping

--- training_worker/classifiers/models/ebm_single_class_v3.py
+++ training_worker/classifiers/models/ebm_single_class_v3.py
@@ -2628,7 +2628,7 @@ elm_model, _ = load_model_elm(device = original_model.device, minio_client = min
 
 # graph interpol
 #plot_samples_graph_interpolation(loaded_model = original_model, dataset_name = "environmental", number_of_samples = 40000,tag_name =tag_name_x, model_type = "EBM Model" )
-sorted_images , new_scores = plot_samples_graph_interpolation_plus_mapping_v2(loaded_model = original_model, dataset_name = "environmental", number_of_samples = 2000 ,tag_name =tag_name_x, model_type = "EBM Model" )
+sorted_images , new_scores = plot_samples_graph_interpolation_plus_mapping_v2(loaded_model = original_model, dataset_name = "environmental", number_of_samples = 40000 ,tag_name =tag_name_x, model_type = "EBM Model" )
 
 sorted_images_x = []
 
