bf473785598ba0c61d70ec2df2823286979fc92b
charles_lee <lucky.goldragon@gmail.com>
2024-05-02 12:21:16 -0400

    test OMP

--- test/test_omp/script/main.py
+++ test/test_omp/script/main.py
@@ -5,6 +5,10 @@ import skdim
 
 # utils
 import argparse
+import json
+
+# os
+import os
 
 # feature selection library
 from sklearn.linear_model import OrthogonalMatchingPursuit
@@ -13,8 +17,12 @@ import sys
 base_dir = './'
 sys.path.insert(0, base_dir)
 
+# load minio utility
+from utility.minio import cmd
+
 # clip vector loader which loads data from memory mapping of numpy
-from test.load_clip_vector_over_sigma_score.clip_vector_loader import get_clip_0_sigma
+from test.load_clip_vector_over_sigma_score.clip_vector_loader import ClipVectorLoader
+from test.test_omp.model.scoring_fc import ScoringFCNetwork
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -25,14 +33,51 @@ def parse_args():
                         default=-1000, 
                         help='Min sigma score, default is -1000, it means loadding all data')
     parser.add_argument('--dataset', type=str, default='all', help='Dataset name')
+    parser.add_argument('--epochs', type=int, default=10, help='epochs')
+    parser.add_argument('--training-batch-size', type=int, default=64)
+    parser.add_argument('--learning-rate', type=float, default=0.001)
 
     return parser.parse_args()
+
+def save_sparse_index(sparse_index, dataset="environmental"):
+
+    file_path = 'output/sparse_index.json'
+
+    # Check if the file exists
+    if os.path.exists(file_path):
+        # If the file exists, read the contents
+        with open(file_path, mode='r+') as file:
+            try:
+                existed_data = json.load(file)
+            except json.JSONDecodeError:
+                print("Error")
+                # If the file is empty or contains invalid JSON, initialize an empty dictionary
+                existed_data = {}
+    else:
+        # If the file doesn't exist, initialize an empty dictionary
+        existed_data = {}
+
+    # Update or create the dataset in the dictionary
+    existed_data[dataset] = sparse_index
+
+    # Write the updated dictionary to the file
+    with open(file_path, mode='w') as file:
+        json.dump(existed_data, file)
 
 def main():
 
     args = parse_args()
+    
+    minio_client = cmd.get_minio_client(minio_access_key=args.minio_access_key,
+                                    minio_secret_key=args.minio_secret_key,
+                                    minio_ip_addr=args.minio_addr)
 
-    clip_vectors, scores = get_clip_0_sigma(0, args.count)
+    clip_vector_loader = ClipVectorLoader(min_sigma_score=-1000, dataset=args.dataset)
+
+    # load data
+    clip_vectors, scores = clip_vector_loader.get_all_clip_vector()
+
+    # remove nan, inf value which causes error
     clip_vectors = np.array(clip_vectors)
     scores = np.array(scores).reshape(-1, 1)
     clip_vectors = clip_vectors[~np.isnan(clip_vectors).any(axis=1)]
@@ -47,9 +92,14 @@ def main():
         # Fit the OMP feature selector to the data
         omp.fit(clip_vectors, scores)
 
-        clip_vectors[:, np.argsort(-np.abs(omp.coef_))[n_features:]] = 0
+        save_sparse_index(sparse_index=np.argsort(-np.abs(omp.coef_))[:n_features].tolist(), dataset=args.dataset)
+        
+        sparse_clip_vectors = clip_vectors[:, np.argsort(-np.abs(omp.coef_))[:n_features]]
+        
+        model = ScoringFCNetwork(minio_client=minio_client, dataset="test-generations", input_size=n_features, input_type="clip-h")
+        loss = model.train(clip_vectors, scores, num_epochs= args.epochs, batch_size=args.training_batch_size, learning_rate=args.learning_rate)
+        model.save_model()
 
-        print(clip_vectors[:2].tolist())
     except Exception as e:
         print('Error occured! ', e)
 if __name__ == '__main__':
