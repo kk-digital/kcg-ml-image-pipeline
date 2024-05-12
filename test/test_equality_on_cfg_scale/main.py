40d1eddc299d57adbfe2bf3cd40bb1d951a981a9
charles_lee <lucky.goldragon@gmail.com>
2024-05-12 16:38:28 -0400

    test equality on diff cfg scales

--- test/test_equality_on_cfg_scale/main.py
+++ test/test_equality_on_cfg_scale/main.py
@@ -1,13 +1,25 @@
 import sys
 import msgpack
 import torch
+import argparse
 
 base_dir = './'
 sys.path.insert(base_dir)
 
 from data_loader.utils import get_object
 from kandinsky_worker.image_generation.img2img_generator import generate_img2img_generation_jobs_with_kandinsky
+from utility.minio import cmd
 
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--minio-access-key', type=str, help='Minio access key')
+    parser.add_argument('--minio-secret-key', type=str, help='Minio secret key')
+    parser.add_argument('--dataset', type=str, default='environmental')
+    parser.add_argument('--cfg-scale', type=int, default=0)
+
+    return parser.parse_args()
 
 def get_clip_distribution(minio_client, dataset):
     
@@ -24,7 +36,12 @@ def get_clip_distribution(minio_client, dataset):
     return mean_vector, std_vector, max_vector, min_vector
 
 def main():
-    mean_vector = get_clip_distribution()
+
+    args = parse_args()
+    minio_client = cmd.get_minio_client(minio_access_key=args.minio_access_key, 
+                                        minio_secret_key=args.minio_secret_key)
+
+    mean_vector, _, _, _ = get_clip_distribution(minio_client=minio_client, dataset=args.dataset)
 
     response= generate_img2img_generation_jobs_with_kandinsky(
         image_embedding=mean_vector.unsqueeze(0),
@@ -34,6 +51,8 @@ def main():
         decoder_guidance_scale=0,
         self_training=True
     )
+
+    print('Successfully generated jobs')
 
 if __name__ == '__main__':
     main()
