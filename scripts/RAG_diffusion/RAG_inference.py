8d511de6a87fbf4c14e2d7217959045b929a0625
rachid2198 <rachid9821@gmail.com>
2024-05-22 17:50:25 +0100

    update

--- scripts/RAG_diffusion/RAG_inference.py
+++ scripts/RAG_diffusion/RAG_inference.py
@@ -71,12 +71,12 @@ def load_image_latents(minio_client, file_path:str):
 
 def load_clip_vae_latents(minio_client, dataset):
     print(f"Fetching clip and vae vectors for all images in the {dataset} dataset")
-    end_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
-    response = requests.get(f'{API_URL}/image/list-image-metadata-by-dataset?dataset={dataset}&end_date={end_date}&limit=40000')
+
+    response = requests.get(f'{API_URL}/image/get_random_image_with_time?dataset={dataset}&size=10000')
     # get the list of jobs
-    jobs = json.loads(response.content)
+    jobs = json.loads(response.content)["images"]
     # get the list of file paths to each image
-    image_paths=[job["image_path"] for job in jobs]
+    image_paths=[job['task_output_file_dict'].get('output_file_path') for job in jobs]
      
     image_latents=[] 
     # load vae and clip vectors for each one
