707cfa563a6aff487a58526add13144a2e7d013c
charles_lee <lucky.goldragon@gmail.com>
2024-05-22 12:06:21 -0400

    fix bug

--- test/test_equality_on_cfg_scale/show_graph.py
+++ test/test_equality_on_cfg_scale/show_graph.py
@@ -1,7 +1,12 @@
 
 import matplotlib.pyplot as plt
 import argparse
+import numpy as np
 import pandas as pd
+
+import sys
+base_dir = './'
+sys.path.insert(0, base_dir)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -22,8 +27,8 @@ def main():
         print(e)
         return
 
-    cfg_scales = downloaded_image_data['task_cfg_scale'].unique()
-    seeds = downloaded_image_data['task_seed'].unique()
+    cfg_scales = np.sort(downloaded_image_data['task_cfg_scale'].unique())
+    seeds = np.sort(downloaded_image_data['task_seed'].unique())
 
     len_cfg_scales = len(cfg_scales)
     len_seeds = len(seeds)
@@ -36,27 +41,29 @@ def main():
     for index, seed in enumerate(seeds):
         seed_to_index[seed] = index
 
-    fig, ax = plt.subplots(len_cfg_scales, len_seeds, figsize=(len_cfg_scales * len_seeds, len_cfg_scales * len_seeds))
+    print(cfg_scale_to_index, seed_to_index)
 
-    for i in range(len_cfg_scales):
-        for j in range(len_seeds):
+    fig, ax = plt.subplots(len_seeds, len_cfg_scales, figsize=(len_cfg_scales * len_seeds, len_cfg_scales * len_seeds))
+
+    for i in range(len_seeds):
+        for j in range(len_cfg_scales):
             ax[i, j].axis('off')
 
     for i in range(len(downloaded_image_data)):
-        image_path = downloaded_image_data.loc(i)['downloaded_image_path']
+        image_path = downloaded_image_data.loc[i, 'downloaded_image_path']
 
-        cfg_scale = downloaded_image_data.loc(i)['task_cfg_scale']
-        seed = downloaded_image_data.loc(i)['task_seed']
+        cfg_scale = downloaded_image_data.loc[i, 'task_cfg_scale']
+        seed = downloaded_image_data.loc[i, 'task_seed']
         
-        col = cfg_scale_to_index[downloaded_image_data.loc(i)['task_cfg_scale']]
-        row = seed_to_index[downloaded_image_data.loc(i)['task_seed']]
+        col = cfg_scale_to_index[downloaded_image_data.loc[i, 'task_cfg_scale']]
+        row = seed_to_index[downloaded_image_data.loc[i, 'task_seed']]
         if image_path is not None:
             image = plt.imread(image_path, format='jpg')
             ax[row, col].imshow(image)
         else:
-            ax[row, col].text(0.5, 0.5, 'Failed', ha='center', va='center', fontsize=20)
+            ax[row, col].text(0.5, 0.5, 'Failed', ha='center', va='center', fontsize=8)
         ax[row, col].axis('off')
-        ax[row, col].set_title(f'cfg_scale: {cfg_scale_to_index} seed: {seed}', fontSize=8)
+        ax[row, col].set_title(f'cfg_scale: {cfg_scale_to_index} seed: {seed}', fontsize=8)
 
     plt.savefig('test/output/cfg_scale/result_on_cfg_scale.png', format='png')
     plt.show()
