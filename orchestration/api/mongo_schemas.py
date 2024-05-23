d9cdf972187c56995b0bb6b8fbb284b276abdfbf
charles_lee <lucky.goldragon@gmail.com>
2024-05-23 11:53:57 -0400

    fix bug endpoint for  ranking image-pair

--- orchestration/api/mongo_schemas.py
+++ orchestration/api/mongo_schemas.py
@@ -63,12 +63,17 @@ class Task(BaseModel):
 class ABRankTrainingImage(Task):
     classifier_score: float
 
-class ABRankImagePairResponse(BaseModel):
+class ABRankImagePairResponse_v1(BaseModel):
     image_1: ABRankTrainingImage
     image_2: ABRankTrainingImage
     num_images_above_min_score: float
     num_image_pair_within_max_diff: float
 
+class ABRankImagePairResponse(BaseModel):
+    image_pair: List[Task]
+    image_score_pair: List[int]
+    num_images_above_min_score: float
+    num_image_pair_within_max_diff: float
 
 class ListTask(BaseModel):
     jobs: List[Task]
