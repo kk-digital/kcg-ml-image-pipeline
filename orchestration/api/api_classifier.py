498b0b01d2ec51a14e3041533f5733c80704d1c7
ssss <sdevdariani9@gmail.com>
2024-06-12 13:46:20 +0400

    rever commit

--- orchestration/api/api_classifier.py
+++ orchestration/api/api_classifier.py
@@ -30,6 +30,95 @@ def get_next_classifier_id_sequence(request: Request):
 
     return counter_seq
 
+@router.post("/classifier/register-tag-classifier", 
+             tags=["deprecated2"],
+             description="deprecated, replaced with /pseudotag-classifiers/register-tag-classifier",
+             responses=ApiResponseHandlerV1.listErrors([400, 500]))
+async def add_update_classifier(request: Request, classifier_data: Classifier):
+    response_handler = await ApiResponseHandlerV1.createInstance(request)
+
+    try:
+        # Check if the tag_id exists in tag_definitions_collection
+        tag_definition_exists = request.app.tag_definitions_collection.find_one({"tag_id": classifier_data.tag_id})
+        if not tag_definition_exists:
+            return response_handler.create_error_response_v1(
+                error_code=ErrorCode.INVALID_PARAMS, 
+                error_string=f"Tag ID {classifier_data.tag_id} not found in tag definitions.",
+                http_status_code=400
+            )
+
+        # Check if an existing classifier can be updated
+        existing_classifier = request.app.classifier_models_collection.find_one(
+            {"tag_id": classifier_data.tag_id, "classifier_name": classifier_data.classifier_name}
+        )
+        
+        if existing_classifier:
+            new_seq_number = existing_classifier.get("model_sequence_number", 0) + 1
+            classifier_id = existing_classifier.get("classifier_id")
+            update_result = request.app.classifier_models_collection.update_one(
+                {"classifier_id": classifier_id},
+                {"$set": {
+                    "classifier_id": classifier_id,
+                    "classifier_name": classifier_data.classifier_name,
+                    "tag_id": classifier_data.tag_id,
+                    "model_sequence_number": new_seq_number,
+                    "latest_model": classifier_data.latest_model,
+                    "model_path": classifier_data.model_path
+                }}
+            )
+            if update_result.modified_count == 0:
+                return response_handler.create_error_response_v1(
+                    error_code=ErrorCode.INVALID_PARAMS, 
+                    error_string="Classifier update failed.",
+                    http_status_code=500
+                )
+            return response_handler.create_success_response_v1(
+                response_data={"message": "Classifier updated successfully.", "classifier_id": classifier_id, "new_model_sequence_number": new_seq_number},
+                http_status_code=200
+            )
+        else:
+            classifier_data.model_sequence_number = 1
+            classifier_data.classifier_id= get_next_classifier_id_sequence(request)
+            request.app.classifier_models_collection.insert_one(classifier_data.to_dict())
+            return response_handler.create_success_response_v1(
+                response_data={"message": "New classifier added successfully.", "classifier_id": classifier_data.classifier_id, "model_sequence_number": 1},
+                http_status_code=201
+            )
+
+    except Exception as e:
+        return response_handler.create_error_response_v1(
+            error_code=ErrorCode.OTHER_ERROR, 
+            error_string=f"Failed to add/update classifier: {str(e)}",
+            http_status_code=500
+        )
+    
+    
+@router.get("/classifier/list-classifiers", 
+            response_model=StandardSuccessResponseV1[List[Classifier]],
+            description="deprecated, replaced with /pseudotag-classifiers/list-classifiers",
+            tags=["deprecated2"],
+            status_code=200,
+            responses=ApiResponseHandlerV1.listErrors([500]))
+async def list_classifiers(request: Request):
+    response_handler = await ApiResponseHandlerV1.createInstance(request)
+    try:
+        classifier_cursor = request.app.classifier_models_collection.find({})
+        classifier_list = list(classifier_cursor)
+
+        result = []
+        for classifier in classifier_list:
+            # Convert MongoDB's ObjectId to string if needed, otherwise prepare as is
+            classifier['_id'] = str(classifier['_id'])
+            result.append(classifier)
+
+        return response_handler.create_success_response_v1(response_data=result, http_status_code=200)
+
+    except Exception as e:
+        # Implement appropriate error handling
+        print(f"Error: {str(e)}")
+        return response_handler.create_error_response_v1(error_code=ErrorCode.OTHER_ERROR, error_string="Internal server error", http_status_code=500)
+
+
 
 # Updated apis
 
