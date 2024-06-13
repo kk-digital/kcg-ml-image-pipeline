17e537a738d8c19a3ad7808deb17a843acfa776e
ssss <sdevdariani9@gmail.com>
2024-06-13 13:16:13 +0400

    rever deleted apis

--- orchestration/api/api_classifier_score.py
+++ orchestration/api/api_classifier_score.py
@@ -13,6 +13,106 @@ from typing import List
 router = APIRouter()
 
 
+@router.get("/classifier-score/get-scores-by-classifier-id-and-tag-id",
+            description="deprecated, replaced with /pseudotag-classifier-scores/list-images-by-scores-v1",
+            status_code=200,
+            tags=["deprecated2"],
+            response_model=StandardSuccessResponseV1[ClassifierScore],
+            responses=ApiResponseHandlerV1.listErrors([400, 422]))
+def get_scores_by_classifier_id_and_tag_id(request: Request, 
+                                                  tag_id: int, 
+                                                 classifier_id: int, 
+                                                 sort: int = -1):
+    api_response_handler = ApiResponseHandlerV1(request)
+
+    query = {"classifier_id": classifier_id, "tag_id": tag_id}
+    items = request.app.image_classifier_scores_collection.find(query).sort("score", sort)
+
+    if not items:
+        # If no items found, use ApiResponseHandler to return a standardized error response
+        return api_response_handler.create_error_response_v1(
+            error_code=ErrorCode.INVALID_PARAMS,
+            error_string="No scores found for specified tag_id.",
+            http_status_code=400
+        )
+    
+    score_data = []
+    for item in items:
+        # remove the auto generated '_id' field
+        item.pop('_id', None)
+        score_data.append(item)
+        print(item)
+    print(len(score_data))
+    # Return a standardized success response with the score data
+    return api_response_handler.create_success_response_v1(
+        response_data=score_data,
+        http_status_code=200
+    )    
+
+
+@router.get("/classifier-score/get-image-classifier-score-by-hash", 
+            description="deprecated, replaced with /pseudotag-classifier-scores/get-image-classifier-score-by-hash-and-classifier-id ",
+            status_code=200,
+            tags=["deprecated2"],  
+            response_model=StandardSuccessResponseV1[ClassifierScore],  # Specify the expected response model, adjust as needed
+            responses=ApiResponseHandlerV1.listErrors([400,422]))
+def get_image_classifier_score_by_hash(request: Request, image_hash: str, tag_id: int, classifier_id: int):
+    api_response_handler = ApiResponseHandlerV1(request)
+
+    # check if exists
+    query = {"image_hash": image_hash, "tag_id": tag_id, "classifier_id": classifier_id}
+
+    item = request.app.image_classifier_scores_collection.find_one(query)
+
+    if item is None:
+        # Return a standardized error response if not found
+        return api_response_handler.create_error_response_v1(
+            error_code=ErrorCode.INVALID_PARAMS,
+            error_string="Score for specified classifier_id, tag_id and image_hash does not exist.",
+            http_status_code=404
+        )
+
+    # Remove the auto generated '_id' field before returning
+    item.pop('_id', None)
+
+    # Return a standardized success response
+    return api_response_handler.create_success_response_v1(
+        response_data=item,
+        http_status_code=200
+    )
+
+
+
+@router.get("/classifier-score/get-image-classifier-score-by-uuid", 
+            description="deprecated, replaced with /pseudotag-classifier-scores/get-image-classifier-score-by-uuid-and-classifier-id",
+            status_code=200,
+            tags=["deprecated2"],  
+            response_model=StandardSuccessResponseV1[ClassifierScore],  # Specify the expected response model, adjust as needed
+            responses=ApiResponseHandlerV1.listErrors([400,422]))
+def get_image_classifier_score_by_uuid(request: Request, classifier_score_uuid: str):
+    api_response_handler = ApiResponseHandlerV1(request)
+
+    # check if exists
+    query = {"uuid": classifier_score_uuid}
+
+    item = request.app.image_classifier_scores_collection.find_one(query)
+
+    if item is None:
+        # Return a standardized error response if not found
+        return api_response_handler.create_error_response_v1(
+            error_code=ErrorCode.INVALID_PARAMS,
+            error_string="Score for specified uuid does not exist.",
+            http_status_code=404
+        )
+
+    # Remove the auto generated '_id' field before returning
+    item.pop('_id', None)
+
+    # Return a standardized success response
+    return api_response_handler.create_success_response_v1(
+        response_data=item,
+        http_status_code=200
+    )
 
 
 @router.put("/classifier-score/update-image-classifier-score-by-uuid",
@@ -105,6 +205,78 @@ async def set_image_classifier_score(request: Request, classifier_score: Classif
         response_data=classifier_score.to_dict(),
         http_status_code=200  
     )
+
+
+@router.delete("/classifier-score/delete-image-classifier-score-by-uuid", 
+               description="deprecated,replaced with /pseudotag-classifier-scores/delete-image-classifier-score-by-uuid-and-classifier-id",
+               status_code=200,
+               tags = ['deprecated2'],
+               response_model=StandardSuccessResponseV1[WasPresentResponse],
+               responses=ApiResponseHandlerV1.listErrors([422]))
+def delete_image_classifier_score_by_uuid(
+    request: Request,
+    classifier_score_uuid: str):
+
+    api_response_handler = ApiResponseHandlerV1(request)
+    
+    query = {"uuid": classifier_score_uuid}
+    res = request.app.image_classifier_scores_collection.delete_one(query)
+    
+    was_present = res.deleted_count > 0
+    
+    # Use ApiResponseHandler to return the standardized response
+    return api_response_handler.create_success_response_v1(
+        response_data={"wasPresent": was_present},
+        http_status_code=200
+    )
+
+
+@router.get("/classifier-score/list-by-scores", 
+            description="deprecated, replaced with /pseudotag-classifier-scores/list-images-by-scores-v1",
+            tags=["deprecated2"],  
+            response_model=StandardSuccessResponseV1[ListClassifierScore],  # Adjust the response model as needed
+            responses=ApiResponseHandlerV1.listErrors([400, 422]))
+async def list_images_by_classifier_scores(
+    request: Request,
+    classifier_id: Optional[int] = Query(None, description="Filter by classifier ID"),
+    min_score: Optional[float] = Query(None, description="Minimum score"),
+    max_score: Optional[float] = Query(None, description="Maximum score"),
+    limit: int = Query(10, alias="limit")
+):
+    response_handler = await ApiResponseHandlerV1.createInstance(request)
+
+    # Build the query based on provided filters
+    query = {}
+    if classifier_id is not None:
+        query["classifier_id"] = classifier_id
+    if min_score is not None and max_score is not None:
+        query["score"] = {"$gte": min_score, "$lte": max_score}
+    elif min_score is not None:
+        query["score"] = {"$gte": min_score}
+    elif max_score is not None:
+        query["score"] = {"$lte": max_score}
+
+    # Fetch data from MongoDB with a limit
+    cursor = request.app.image_classifier_scores_collection.find(query).limit(limit)
+    scores_data = list(cursor)
+
+    # Remove _id in response data
+    for score in scores_data:
+        score.pop('_id', None)
+
+    # Prepare the data for the response
+    images_data = ListClassifierScore(images=[ClassifierScore(**doc).to_dict() for doc in scores_data]).dict()
+
+    # Return the fetched data with a success response
+    return response_handler.create_success_response_v1(
+        response_data=images_data, 
+        http_status_code=200
+    )
+
+
+
+
+
 
 
 # Updated apis
