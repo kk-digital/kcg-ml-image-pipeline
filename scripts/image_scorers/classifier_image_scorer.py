7da8f92cf100ab84ed7646ec24b2c06d655774ba
rachid2198 <rachid9821@gmail.com>
2024-07-18 14:58:48 +0100

    updating the classifier scorer

--- scripts/image_scorers/classifier_image_scorer.py
+++ scripts/image_scorers/classifier_image_scorer.py
@@ -234,7 +234,7 @@ def main():
         print(f"Load the {bucket_name}/{dataset_name} dataset")
         dataset_loader = ImageDatasetLoader(minio_client, bucket_name, dataset_name)
         image_dataset = dataset_loader.load_dataset()
-        print(image_dataset[0])
+        print(image_dataset[0]["image_hash"])
 
         world_size = torch.cuda.device_count()
         mp.spawn(calculate_and_upload_scores, args=(world_size, image_dataset, image_source, classifier_models, batch_size), nprocs=world_size, join=True)
