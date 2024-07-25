473d6a1ccbffd4939c4e2753aaac2a1f74a1170b
ssss <sdevdariani9@gmail.com>
2024-07-25 16:14:41 +0400

    spit script

---
+++ scripts/update_ranking_datapoints_minio.py
@@ -0,0 +1,72 @@
+import os
+import json
+from pymongo import MongoClient
+from minio import Minio
+from datetime import datetime
+from io import BytesIO
+
+# MongoDB connection setup
+MONGO_URI = "mongodb://192.168.3.1:32017/"
+DATABASE_NAME = "orchestration-job-db"
+mongo_client = MongoClient(MONGO_URI)
+db = mongo_client[DATABASE_NAME]
+ranking_datapoints_collection = db['ranking_datapoints']
+
+# MinIO connection setup
+minio_client = Minio(
+    '192.168.3.5:9000',
+    access_key='v048BpXpWrsVIHUfdAix',
+    secret_key='4TFS20qkxVuX2HaC8ezAgG7GaDlVI1TqSPs0BKyu',
+    secure=False  # Set to True if using HTTPS
+)
+
+BATCH_SIZE = 100  # Adjust the batch size as needed
+
+def update_minio():
+    cursor = ranking_datapoints_collection.find(no_cursor_timeout=True).batch_size(BATCH_SIZE)
+    processed_count = 0
+
+    try:
+        while True:
+            batch = []
+            try:
+                for _ in range(BATCH_SIZE):
+                    batch.append(next(cursor))
+            except StopIteration:
+                pass
+
+            if not batch:
+                break
+
+            for doc in batch:
+                # Prepare data for MinIO upload (excluding the '_id' field)
+                minio_data = doc.copy()
+                minio_data.pop("_id")
+
+                formatted_rank_model_id = f"{doc['rank_model_id']:05d}"
+                path = f"ranks/{formatted_rank_model_id}/data/ranking/aggregate"
+
+                # Fetch the corresponding filenames from MinIO
+                objects = minio_client.list_objects("datasets", prefix=path, recursive=True)
+                for obj in objects:
+                    file_name = obj.object_name.split('/')[-1]
+                    full_path = obj.object_name
+                    json_data = json.dumps(minio_data, indent=4).encode('utf-8')
+                    data = BytesIO(json_data)
+
+                    # Upload data to MinIO
+                    try:
+                        minio_client.put_object("datasets", full_path, data, len(json_data), content_type='application/json')
+                        print(f"Uploaded successfully to MinIO: {full_path}")
+                    except Exception as e:
+                        print(f"Error uploading to MinIO: {str(e)}")
+
+            processed_count += len(batch)
+            print(f"Processed {len(batch)} documents for MinIO upload.")
+
+    finally:
+        cursor.close()
+        print(f"Total documents processed for MinIO upload: {processed_count}")
+
+if __name__ == "__main__":
+    update_minio()
