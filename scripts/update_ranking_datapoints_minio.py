cc5b3b09468ed1836d72ba620a757c8fc33cd5b8
sandrodevdariani <sdevdariani9@gmail.com>
2024-07-25 19:20:15 +0400

    update script

--- scripts/update_ranking_datapoints_minio.py
+++ scripts/update_ranking_datapoints_minio.py
@@ -11,6 +11,9 @@ DATABASE_NAME = "orchestration-job-db"
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client[DATABASE_NAME]
 ranking_datapoints_collection = db['ranking_datapoints']
+completed_jobs_collection = db['completed-jobs']
+extracts_collection = db['extracts']
+external_images_collection = db['external_images']
 
 # MinIO connection setup
 minio_client = Minio(
@@ -20,49 +23,63 @@ minio_client = Minio(
     secure=False  # Set to True if using HTTPS
 )
 
-BATCH_SIZE = 100  # Adjust the batch size as needed
+def determine_image_source(image_hash):
+    if completed_jobs_collection.find_one({"task_output_file_dict.output_file_hash": image_hash}):
+        return "generated_image"
+    elif extracts_collection.find_one({"image_hash": image_hash}):
+        return "extract_image"
+    elif external_images_collection.find_one({"image_hash": image_hash}):
+        return "external_image"
+    else:
+        return None
+
+def update_image_source(doc):
+    if 'image_source' in doc:
+        doc.pop('image_source')
+    
+    if 'image_1_metadata' in doc and doc['image_1_metadata'].get('file_hash'):
+        image_source_1 = determine_image_source(doc['image_1_metadata']['file_hash'])
+        doc['image_1_metadata']['image_source'] = image_source_1
+    
+    if 'image_2_metadata' in doc and doc['image_2_metadata'].get('file_hash'):
+        image_source_2 = determine_image_source(doc['image_2_metadata']['file_hash'])
+        doc['image_2_metadata']['image_source'] = image_source_2
+    
+    return doc
 
 def update_minio():
-    cursor = ranking_datapoints_collection.find(no_cursor_timeout=True).batch_size(BATCH_SIZE)
+    cursor = ranking_datapoints_collection.find(no_cursor_timeout=True)
     processed_count = 0
 
     try:
-        while True:
-            batch = []
-            try:
-                for _ in range(BATCH_SIZE):
-                    batch.append(next(cursor))
-            except StopIteration:
-                pass
+        for doc in cursor:
+            print(f"Processing document with ID: {doc['_id']}")
+            updated_doc = update_image_source(doc)
 
-            if not batch:
-                break
+            # Prepare data for MinIO upload (excluding the '_id' field)
+            minio_data = updated_doc.copy()
+            minio_data.pop("_id")
 
-            for doc in batch:
-                # Prepare data for MinIO upload (excluding the '_id' field)
-                minio_data = doc.copy()
-                minio_data.pop("_id")
+            formatted_rank_model_id = f"{doc['rank_model_id']:05d}"
+            path = f"ranks/{formatted_rank_model_id}/data/ranking/aggregate"
 
-                formatted_rank_model_id = f"{doc['rank_model_id']:05d}"
-                path = f"ranks/{formatted_rank_model_id}/data/ranking/aggregate"
+            # Fetch the corresponding filenames from MinIO
+            objects = minio_client.list_objects("datasets", prefix=path, recursive=True)
+            for obj in objects:
+                file_name = obj.object_name.split('/')[-1]
+                full_path = obj.object_name
+                json_data = json.dumps(minio_data, indent=4).encode('utf-8')
+                data = BytesIO(json_data)
 
-                # Fetch the corresponding filenames from MinIO
-                objects = minio_client.list_objects("datasets", prefix=path, recursive=True)
-                for obj in objects:
-                    file_name = obj.object_name.split('/')[-1]
-                    full_path = obj.object_name
-                    json_data = json.dumps(minio_data, indent=4).encode('utf-8')
-                    data = BytesIO(json_data)
+                # Upload data to MinIO
+                try:
+                    minio_client.put_object("datasets", full_path, data, len(json_data), content_type='application/json')
+                    print(f"Uploaded successfully to MinIO: {full_path}")
+                except Exception as e:
+                    print(f"Error uploading to MinIO: {str(e)}")
 
-                    # Upload data to MinIO
-                    try:
-                        minio_client.put_object("datasets", full_path, data, len(json_data), content_type='application/json')
-                        print(f"Uploaded successfully to MinIO: {full_path}")
-                    except Exception as e:
-                        print(f"Error uploading to MinIO: {str(e)}")
-
-            processed_count += len(batch)
-            print(f"Processed {len(batch)} documents for MinIO upload.")
+            processed_count += 1
+            print(f"Processed document {processed_count} for MinIO upload.")
 
     finally:
         cursor.close()
