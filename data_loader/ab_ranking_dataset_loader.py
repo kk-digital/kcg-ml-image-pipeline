5cdbc6c52a6acdd55b68cc2169ab19e918c31bb9
rachid2198 <rachid9821@gmail.com>
2024-08-04 08:50:52 +0100

    bug fix

--- data_loader/ab_ranking_dataset_loader.py
+++ data_loader/ab_ranking_dataset_loader.py
@@ -180,7 +180,7 @@ class ABRankingDatasetLoader:
         features_path_img_2 = file_path_img_2.replace(".jpg", input_type_extension)
         bucket_img_2, features_path_img_2 = separate_bucket_and_file_path(features_path_img_2)
 
-        features_img_1_data = cmd.get_file_from_minio(self.minio_client, bucket_img_1, features_path_img_1)
+        features_img_1_data = get_object_with_bucket(self.minio_client, bucket_img_1, features_path_img_1)
         features_img_1_data = msgpack.unpackb(features_img_1_data)
         features_vector_img_1 = []
 
@@ -193,7 +193,7 @@ class ABRankingDatasetLoader:
 
         features_vector_img_1 = np.array(features_vector_img_1)
         
-        features_img_2_data = cmd.get_file_from_minio(self.minio_client, bucket_img_2, features_path_img_2)
+        features_img_2_data = get_object_with_bucket(self.minio_client, bucket_img_2, features_path_img_2)
         features_img_2_data = msgpack.unpackb(features_img_2_data)
         features_vector_img_2 = []
 
